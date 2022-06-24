89ea6d513e3ab4642c5359bad5f52fa3a081a397
dlopes14 <donylopes@gmail.com>
2022-06-24 12:02:43 -0300

    file downloader

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Layout from "../componentes/Layout";
 import { Footer } from "../componentes/Footer/Footer.jsx";
 import { HomeBanner } from "../componentes/HomeBanner/HomeBanner.jsx";
 import { NavBar } from "../componentes/NavBar/NavBar.jsx";
+import { FileDownloader } from "../componentes/FileDownloader/Filedownloader";
 
 const Index = () => {
   return (
@@ -20,6 +21,7 @@ const Index = () => {
         tituloDestaque = ""
         texto = ""
       />
+      <FileDownloader />
       <Footer
         address={{
             first: "Rua Aracaju 100 - Vila Madalena",
