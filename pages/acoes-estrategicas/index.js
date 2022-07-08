79edd04375d0f6caada87d161ff6ce31112287cc
Gabrielle Arruda <gabriellearrudac@gmail.com>
2022-07-08 15:30:12 -0300

    Create index.js

---
+++ pages/acoes-estrategicas/index.js
@@ -0,0 +1,15 @@
+import Layout from "../../componentes/Layout";
+import { IFrame } from "../../componentes/IFrame/IFrame.jsx";
+
+const Index = () => {
+  return (
+    <Layout pageTitle="Previne Brasil | Ações Estratégicas">
+      <IFrame
+        height="450"
+        link="https://datastudio.google.com/embed/reporting/d28c7264-d346-4b64-9042-4455f0235f35/page/p_mdgdcavlwc"
+      />
+    </Layout>
+  )
+}
+
+export default Index;
