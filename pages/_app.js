6abb6b090cf7d461276ad93cce592934b98a8a4f
dlopes14 <donylopes@gmail.com>
2022-07-14 19:26:11 -0300

    fix ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../componentes/Analytics/lib/gtag'
+import Analytics from '../componentes/Analytics/Analytics'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
