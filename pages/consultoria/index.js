5dbc1c0af21c7d88836a7977c4f5b24558071c97
Gabrielle Arruda <gabriellearrudac@gmail.com>
2022-07-19 18:01:08 -0300

    Update index.js

--- pages/consultoria/index.js
+++ pages/consultoria/index.js
@@ -38,7 +38,7 @@ const Index = () => {
       />
     <FormConsultoria
         title="Se interessou pela nossa consultoria? Preencha o formulário abaixo para fazer sua inscrição.Em caso de dúvidas, escreva para "
-        mail="contato@impulsogov.org"
+        mail="previne@impulsogov.org"
         link="https://docs.google.com/forms/d/e/1FAIpQLSce3dYZO3tdRmNq-Oy8Z_0IFu5RXtwDDsSw6BXLaWx7BBfv_Q/viewform?embedded=true"
         button="enviar"
     />  
