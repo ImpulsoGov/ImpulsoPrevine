5ed9ad5ca74d2aab893530a0d43b6cf0e8fff5da
dlopes14 <donylopes@gmail.com>
2022-09-05 17:41:29 -0300

    fix font

--- pages/_app.js
+++ pages/_app.js
@@ -59,6 +59,12 @@ function MyApp(props) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Context.Provider value={[cidade, setCidade]}>
         {isLoading && 
