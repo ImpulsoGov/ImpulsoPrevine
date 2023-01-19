0d8c9c6085a1b4a576bcb4d47e6156371b9be5e8
dlopes14 <donylopes@gmail.com>
2023-01-19 19:31:36 -0300

    criar senha - primeiro acesso

---
+++ services/primeiroAcesso.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { API_URL } from "../constants/API_URL";
+import FormData from "form-data";
+
+const primeiroAcessoClient = async(mail)=>{
+    const data = new FormData();
+    data.append('mail', mail);
+    
+    let config = {
+      method: 'post',
+      url: API_URL + 'suporte/ger_usuarios/primeiro-acesso',
+      data : data
+    };
+    
+    const res = await axios(config)
+    .then(function (response) {
+        return response.data;
+    })
+    .catch(function (error) {
+        return error.response.data
+    });
+    return res
+}    
+
+const primeiroAcesso = async(mail)=>{
+    const res = await primeiroAcessoClient(mail)
+      if (res?.success == true) return true
+      return false
+  }
+  
+  const criarSenhaClient = async(mail,codigo,nova_senha)=>{
+    let data = new FormData();
+    data.append('mail', mail);
+    data.append('codigo', codigo);
+    data.append('nova_senha', nova_senha);
+  
+    let config = {
+      method: 'post',
+      url: API_URL + 'suporte/ger_usuarios/criar-senha',
+      data : data
+    };
+  
+    const res = await axios(config)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      return error.response.data;
+    });
+  
+    return res
+  
+  }
+  const criarSenha = async(mail,codigo,nova_senha)=>{
+    const res = await criarSenhaClient(mail,codigo,nova_senha)
+      if (res?.success == true) return true
+      return false
+  }
+  
+export {primeiroAcesso,criarSenha}
