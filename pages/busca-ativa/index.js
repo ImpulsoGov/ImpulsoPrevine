25c9f9e029c66a850dc9fa53bd4d468b8582005d
dlopes14 <donylopes@gmail.com>
2023-01-26 15:27:29 -0300

    alterar links

--- pages/busca-ativa/index.js
+++ pages/busca-ativa/index.js
@@ -1,6 +1,7 @@
 import { PanelSelector} from "@impulsogov/design-system";
 import { useSession,signOut } from "next-auth/react"
 import React, { useState,useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { getData } from '../../utils/cms'
 import { LAYOUT } from '../../utils/QUERYS'
 import { DATA_STUDIO_URL_EQUIPE, DATA_STUDIO_URL_COORDENACAO_APS, DATA_STUDIO_URL_CADASTROS_EQUIPE, DATA_STUDIO_URL_CADASTROS_COORDENACAO_APS } from "../../constants/dataStudio";
@@ -76,6 +77,9 @@ const urlGenBuscaAtivaCoordenacaoAPS = (data_studio,token,municipio_uf,cargo)=>{
 const Index = ({res}) => {
   const { data: session,status } = useSession()
   const [tokenValido, setTokenValido] = useState();
+  const router = useRouter()
+  const panel = router.query?.painel
+  const initialTitle = router.query?.initialTitle
   useEffect(()=>{
     if(session){
       validatetoken(session?.user?.access_token)
@@ -96,32 +100,24 @@ const Index = ({res}) => {
     {
       label: "Indicadores Gestantes",
     },
-    {
-      label: "Cadastros Gestantes"
-    },
   ]
 
   if(session){
     const labelsBuscaAtiva = [[],[]]
     if(session.user?.cargo == "Coordenação APS" || session.user?.cargo == "Impulser")  labelsBuscaAtiva[0].push({label: "Coordenação APS"})
     if(session.user?.cargo == "Coordenação de Equipe" || session.user?.cargo == "Impulser")  labelsBuscaAtiva[0].push({label: "Coordenação de Equipe"})
-    if(titlesBuscaAtiva.length > 1){
-      labelsBuscaAtiva.push([])
-      if(session.user?.cargo == "Coordenação APS" || session.user?.cargo == "Impulser")  labelsBuscaAtiva[1].push({label: "Duplicados por Município"})
-      if(session.user?.cargo == "Coordenação de Equipe" || session.user?.cargo == "Impulser")  labelsBuscaAtiva[1].push({label: "Duplicados por Equipe"})
-  }
     const links = [[],[]]
     if (session.user?.cargo == "Coordenação APS" || session.user?.cargo == "Impulser") links[0].push(urlGenBuscaAtivaCoordenacaoAPS(DATA_STUDIO_URL_COORDENACAO_APS,session?.user?.access_token,session?.user?.municipio,session?.user?.cargo))
     if (session.user?.cargo == "Coordenação de Equipe" || session.user?.cargo == "Impulser") links[0].push(urlGenBuscaAtivaEquipe(DATA_STUDIO_URL_EQUIPE,session?.user?.access_token,session?.user?.municipio,session?.user?.equipe,session?.user?.cargo))
-    if (session.user?.cargo == "Coordenação APS" || session.user?.cargo == "Impulser") links[1].push(urlGenBuscaAtivaCoordenacaoAPS(DATA_STUDIO_URL_CADASTROS_COORDENACAO_APS,session?.user?.access_token,session?.user?.municipio,session?.user?.cargo))
-    if (session.user?.cargo == "Coordenação de Equipe" || session.user?.cargo == "Impulser") links[1].push(urlGenBuscaAtivaEquipe(DATA_STUDIO_URL_CADASTROS_EQUIPE,session?.user?.access_token,session?.user?.municipio,session?.user?.equipe,session?.user?.cargo))
     return (
       <>
-      <PanelSelector
-        links = {links}
-        list={labelsBuscaAtiva}
-        titles={titlesBuscaAtiva}
-      />
+        <PanelSelector
+          links = {links}
+          list={labelsBuscaAtiva}
+          titles={titlesBuscaAtiva}
+          panel={Number(panel)}
+          initialTitle={Number(initialTitle)}
+        />
       </>
     )
   }
---
+++ pages/inicio/index.js
@@ -0,0 +1,87 @@
+import { getData } from '../../utils/cms'
+import { LAYOUT } from '../../utils/QUERYS'
+import { useSession } from "next-auth/react"
+import { Greeting, CardAlert, CardLargeGrid} from '@impulsogov/design-system'
+
+
+export async function getServerSideProps({req}) {
+    let redirect 
+    const userIsActive = req.cookies['next-auth.session-token']
+    const userIsActiveSecure = req.cookies['__Secure-next-auth.session-token']
+    if(userIsActive){
+      redirect=true
+    }else{
+        if(userIsActiveSecure){redirect=true}else{redirect=false}
+    }
+    if(!redirect) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false, // make this true if you want the redirect to be cached by the search engines and clients forever
+        }, 
+      }
+    }
+    const res = [
+        await getData(LAYOUT),
+    ]
+    return {
+        props: {
+        res : res
+        }
+    }
+}
+
+
+const Index = ({res}) => {
+    const { data: session,status } = useSession()
+    const cargo = (usuario)=>{
+        if (usuario.cargo == "Coordenação APS") return "coordenador(a) da APS"
+        if (usuario.cargo == "Coordenação de Equipe") return "coordenador(a) de equipe"
+        if (usuario.cargo == "Impulser") return usuario.cargo
+    }
+    if (session){
+        return (
+            <>
+                <Greeting
+                    cargo = {cargo(session.user)}
+                    greeting = "Bem vindo(a)"
+                    municipio_uf = {session.user.municipio}
+                    nome_usuario = {session.user.nome}
+                    texto = "Você está na área logada da Coordenação da APS do seu município. Aqui você vai encontrar um painel com uma lista de possíveis cadastros duplicados de gestantes e listas nominais para monitoramento, referentes aos indicadores de gestantes, hipertensão e diabetes, do Previne Brasil."
+                />
+                <CardAlert
+                    destaque="IMPORTANTE: "
+                    msg="Os dados exibidos nesta plataforma refletem a base de dados local do município e podem divergir dos divulgados quadrimestralmente pelo SISAB. O Ministério da Saúde aplica regras de vinculação e validações cadastrais do usuário, profissional e estabelecimento que não são replicadas nesta ferramenta."
+                />   
+                <CardLargeGrid
+                    cards={[
+                        {
+                        icon: 'https://media.graphassets.com/6cOfkxeyT7245Fn19kgU',
+                        links: [
+                            {
+                            label: 'Pré-Natal',
+                            link: '/busca-ativa?initialTitle=0&painel=0'
+                            },
+                        ],
+                        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidi dunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercita tion ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+                        titulo: 'Listas Nominais'
+                        },
+                        {
+                        icon: 'https://media.graphassets.com/6cOfkxeyT7245Fn19kgU',
+                        links: [
+                            {
+                            label: 'Gestantes',
+                            link: '/cadastros-duplicados?initialTitle=0&painel=0'
+                            },
+                        ],
+                        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidi dunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercita tion ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+                        titulo: 'Cadastros Duplicados'
+                        }
+                    ]}
+                />         
+            </>
+        )
+    }
+}
+
+export default Index;
