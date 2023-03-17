2894beb365a62f4a57b8daf4bb06d217b0ecacdb
dlopes14 <donylopes@gmail.com>
2023-03-17 14:39:29 -0300

    atualiza link paineis cronicos

--- constants/dataStudio.js
+++ constants/dataStudio.js
@@ -4,8 +4,8 @@ export const DATA_STUDIO_URL_COORDENACAO_APS ='https://datastudio.google.com/emb
 export const DATA_STUDIO_URL_CADASTROS_EQUIPE = 'https://datastudio.google.com/embed/reporting/411de6f5-372b-4e2a-8117-4b87b9eeec97/page/NvkxC?params='
 export const DATA_STUDIO_URL_CADASTROS_COORDENACAO_APS ='https://datastudio.google.com/embed/reporting/cc8a1d39-7d65-4211-a0c4-e0b65c976e28/page/NvkxC?params='
 
-export const DATA_STUDIO_URL_DIABETICOS_EQUIPE = 'https://lookerstudio.google.com/embed/reporting/49196ab9-3333-4c49-8d8f-2de035216cf4/page/NvkxC?params='
-export const DATA_STUDIO_URL_DIABETICOS_COORDENACAO_APS ='https://lookerstudio.google.com/embed/reporting/6387d2ab-238b-4528-afc6-61d0274a020e/page/p_kl3q988fyc?params='
+export const DATA_STUDIO_URL_DIABETICOS_EQUIPE = 'https://lookerstudio.google.com/embed/reporting/0dc14b39-8eb0-48f4-8796-602ec42378c8/page/NvkxC?params='
+export const DATA_STUDIO_URL_DIABETICOS_COORDENACAO_APS ='https://lookerstudio.google.com/embed/reporting/000245eb-3e7a-4af4-99b7-983524e727ca/page/p_kl3q988fyc?params='
 
-export const DATA_STUDIO_URL_HIPERTENSOS_EQUIPE = 'https://lookerstudio.google.com/embed/reporting/5824b1c2-80db-4cc4-82d4-0d023953d4a3/page/NvkxC?params='
-export const DATA_STUDIO_URL_HIPERTENSOS_COORDENACAO_APS ='https://lookerstudio.google.com/embed/reporting/7c3d2bb8-5707-4901-b90e-6965ff928ea3/page/p_kl3q988fyc?params='
+export const DATA_STUDIO_URL_HIPERTENSOS_EQUIPE = 'https://lookerstudio.google.com/embed/reporting/ca39b909-688c-42f5-8aaa-71cbf7525dae/page/NvkxC?params='
+export const DATA_STUDIO_URL_HIPERTENSOS_COORDENACAO_APS ='https://lookerstudio.google.com/embed/reporting/b972c385-d867-4b4a-aacd-b72c52104b6a/page/p_kl3q988fyc?params='
