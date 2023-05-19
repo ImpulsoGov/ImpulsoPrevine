510287896b7be218f4e75169450a8c97893cb312
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-05-18 23:34:38 -0300

    ajusta blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -10,7 +10,6 @@ export async function getStaticProps() {
     await getData(LAYOUT),
     await getData(POSTS),
     await getData(LISTA_ARTIGOS),
-
   ]
   return {
     props: {
@@ -22,7 +21,7 @@ export async function getStaticProps() {
 
 const Index = ({res}) => {
     const artigos = res[1].blogArtigos.map((artigo)=>{
-        return (
+    return (
           {
             id: artigo.id,
             tag : artigo.tag,
@@ -33,29 +32,12 @@ const Index = ({res}) => {
             data:  artigo.createdAt,
             imagem : artigo.capa.url,
           }
-    )})
-    const artigoDestaque = artigos.sort((a,b) => new Date(b.data) - new Date(a.data))[0]
+    )});
+    const artigoDestaque = artigos.sort((a,b) => new Date(b.data) - new Date(a.data))[0];
+    
+    
     return (
-    <Layout 
-      pageTitle="Previne Brasil | Blog"
-      logoIPColor={res[0].logoIps[0].logo[0].url}
-      logoIPWhite = {res[0].logoIps[0].logo[1].url}
-      menus = {res[0].menus}
-      footer = {res[0].footers}
-      logoImpulso = {res[0].logoImpulsos[0].logo[0].url}
-      socialMedia = { [
-        { url: res[0].socialMedias[0].url, logo: res[0].socialMedias[0].logo[0].url},
-        { url: res[0].socialMedias[1].url, logo: res[0].socialMedias[1].logo[0].url},
-        { url: res[0].socialMedias[2].url, logo: res[0].socialMedias[2].logo[0].url},
-      ]}
-      copyright = {{
-        label: res[0].copyrights[0].copyright,
-        contato : res[0].copyrights[0].contato
-      }}
-      NavBarIconBranco =  {res[0].logoMenuMoblies[0].logo.url}
-      NavBarIconDark =  {res[0].logoMenuMoblies[1].logo.url}
-
-    >
+      <>
         <PreviewArtigoDestaque
             id = {artigoDestaque.id}
             tag = {artigoDestaque.tag}
@@ -70,9 +52,8 @@ const Index = ({res}) => {
             titulo = {res[2].listaArtigos[0].titulo}
             btn = {{label : res[2].listaArtigos[0].buttonLabel , link: res[2].listaArtigos[0].buttonLink}}
         />
-    </Layout>
-    
-  )
+      </>
+    );
 }
 
 export default Index;
