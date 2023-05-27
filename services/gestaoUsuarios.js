35b01a61bbfe91365222da8f69a97026d6c00912
Taina Santana <tainnaps@gmail.com>
2023-05-26 22:10:42 -0300

    add requisições gestão de usuários

---
+++ services/gestaoUsuarios.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import FormData from "form-data";
+import { API_URL } from "../constants/API_URL";
+
+const instance = axios.create({
+  baseURL: `${API_URL}suporte/ger_usuarios`,
+});
+
+export const listarPerfis = async () => {
+  try {
+    const response = await instance.get('/perfis');
+
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+  }
+};
+
+export const listarUsuarios = async () => {
+  try {
+    const response = await instance.get('/usuarios-ip');
+
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+  }
+};
+
+export const atualizarUsuario = async (id, dados) => {
+  try {
+    const requestData = new FormData();
+
+    requestData.append('nome', dados.nome);
+    requestData.append('mail', dados.mail);
+    requestData.append('cpf', dados.cpf);
+    requestData.append('municipio', dados.municipio);
+    requestData.append('equipe', dados.equipe);
+    requestData.append('cargo', dados.cargo);
+    requestData.append('telefone', dados.telefone);
+
+    const response = await instance.put(`/usuarios-ip/${id}`, requestData);
+
+    return response.data;
+  } catch (error) {
+    throw new Error(JSON.stringify(error.response.data.detail));
+  }
+};
+
+export const atualizarAutorizacoes = async (usuarioId, autorizacoesIds) => {
+  try {
+    const requestData = { perfis_ids: autorizacoesIds };
+    const response = await instance.put(
+      `/perfil-usuario/${usuarioId}`,
+      requestData
+    );
+
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+  }
+};
