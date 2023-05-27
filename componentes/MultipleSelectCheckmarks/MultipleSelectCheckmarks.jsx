b36678c2b22092166e19f74a952ca0ead337a49c
Taina Santana <tainnaps@gmail.com>
2023-05-26 22:11:33 -0300

    add componente MultiSelect

---
+++ componentes/MultipleSelectCheckmarks/MultipleSelectCheckmarks.jsx
@@ -0,0 +1,53 @@
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import ListItemText from '@mui/material/ListItemText';
+import MenuItem from '@mui/material/MenuItem';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Select from '@mui/material/Select';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+
+function MultipleSelectCheckmarks({
+  label, options, handleChange, selectedOptions
+}) {
+
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  return (
+    <FormControl sx={ { m: 1, width: '75%', height: '20vh' } }>
+      <InputLabel id='demo-multiple-checkbox-label'>
+        { label }
+      </InputLabel>
+
+      <Select
+        labelId='demo-multiple-checkbox-label'
+        id='demo-multiple-checkbox'
+        multiple
+        value={ selectedOptions }
+        onChange={ handleChange }
+        input={ <OutlinedInput label={ label } /> }
+        renderValue={ (selected) => selected.join(', ') }
+        MenuProps={ MenuProps }
+        sx={ { width: '100%' } }
+      >
+        { options.map((autorizacao) => (
+          <MenuItem key={ autorizacao.id } value={ autorizacao.descricao }>
+            <Checkbox checked={ selectedOptions.includes(autorizacao.descricao) } />
+            <ListItemText primary={ autorizacao.descricao } />
+          </MenuItem>
+        )) }
+      </Select>
+    </FormControl>
+  );
+}
+
+export default MultipleSelectCheckmarks;
