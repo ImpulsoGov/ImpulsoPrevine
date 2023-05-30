516d75eb28e31cbdb15cc61b67ce2942f086b557
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-05-30 18:56:01 -0300

    corrige build

--- pages/blog/[post].js
+++ pages/blog/[post].js
@@ -1,7 +1,7 @@
-import Layout from "../../../componentes/Layout"
+import Layout from "../../componentes/Layout"
 import { ConteudoBlog } from '@impulsogov/design-system'
-import { getData } from '../../../services/cms'
-import { LAYOUT,POST, POSTID } from '../../../utils/QUERYS'
+import { getData } from '../../services/cms'
+import { LAYOUT,POST, POSTID } from '../../utils/QUERYS'
 
 export async function getStaticPaths() {
     const res = await getData(POSTID)
