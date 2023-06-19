27f0a85869c703300c65ee4beea3ab3d7ae87769
dlopes14 <donylopes@gmail.com>
2023-06-19 15:48:45 -0300

    type : module

--- pages/busca-ativa/hipertensos/index.js
+++ pages/busca-ativa/hipertensos/index.js
@@ -1,4 +1,4 @@
-import { PanelSelector, CardAlert, TituloTexto, ButtonLight} from "@impulsogov/design-system";
+import { PanelSelector, CardAlert, TituloTexto, ButtonLight, PainelBuscaAtiva} from "@impulsogov/design-system";
 import { useSession,signOut, getSession } from "next-auth/react"
 import React, { useState,useEffect } from 'react';
 import { useRouter } from 'next/router';
@@ -68,6 +68,294 @@ const Index = ({res}) => {
                 destaque="IMPORTANTE: "
                 msg="Os dados exibidos nesta plataforma refletem a base de dados local do município e podem divergir dos divulgados quadrimestralmente pelo SISAB. O Ministério da Saúde aplica regras de vinculação e validações cadastrais do usuário, profissional e estabelecimento que não são replicadas nesta ferramenta."
             />  
+            <PainelBuscaAtiva
+  cards={[
+    {
+      descricao: 'Total de pessoas com Hipertensão',
+      valor: 102
+    },
+    {
+      descricao: 'Total de pessoas com Hipertensão',
+      valor: 102
+    },
+    {
+      descricao: 'Total de pessoas com Hipertensão',
+      valor: 102
+    },
+    {
+      descricao: 'Total de pessoas com Hipertensão',
+      valor: 102
+    }
+  ]}
+  dadosFiltros={[
+    {
+      data: [
+        'equipe sol',
+        'equipe lua',
+        'equipe estrela'
+      ],
+      filtro: 'nome_equipe',
+      rotulo: 'Filtrar por nome da equipe'
+    },
+    {
+      data: [
+        '12345678',
+        '87654321',
+        '18273645'
+      ],
+      filtro: 'ine',
+      rotulo: 'Filtrar por INE da equipe'
+    },
+    {
+      data: [
+        'Luana',
+        'Soraia',
+        'Estela'
+      ],
+      filtro: 'acs_nome_cadastro',
+      rotulo: 'Filtrar por nome do ACS'
+    },
+    {
+      data: [
+        'Diagnostico Clinico',
+        'Autorreferido'
+      ],
+      filtro: 'identificacao_condicao',
+      rotulo: 'Filtrar por tipo de diagnóstico'
+    },
+    {
+      data: [
+        'apenas em dia',
+        'consulta e aferição de PA em dia',
+        'apenas aferição de PA em dia',
+        'nada em dia'
+      ],
+      filtro: '',
+      rotulo: 'Filtrar por faixa etária'
+    }
+  ]}
+  painel="hipertensao"
+  tabela={{
+    colunas: [
+      {
+        align: 'left',
+        field: 'nome',
+        headerAlign: 'center',
+        headerName: 'NOME',
+        width: 240
+      },
+      {
+        align: 'center',
+        field: 'cpf',
+        headerAlign: 'center',
+        headerName: 'CPF',
+        width: 130
+      },
+      {
+        align: 'center',
+        field: 'identificacao_condicao',
+        headerAlign: 'center',
+        headerName: 'IDENTIFICAÇÃO DA CONDIÇÃO',
+        width: 150
+      },
+      {
+        align: 'center',
+        field: 'dt_consulta_mais_recente',
+        headerAlign: 'center',
+        headerName: 'DATA DA CONSULTA MAIS RECENTE',
+        width: 120
+      },
+      {
+        align: 'center',
+        field: 'prazo_proxima_consulta',
+        headerAlign: 'center',
+        headerName: 'PRAZO PARA PRÓXIMA CONSULTA',
+        renderCell: () => {},
+        width: 130
+      },
+      {
+        align: 'center',
+        field: 'dt_afericao_pressao_mais_recente',
+        headerAlign: 'center',
+        headerName: 'DATA DA AFERIÇÃO DE PA MAIS RECENTE',
+        width: 130
+      },
+      {
+        align: 'center',
+        field: 'prazo_proxima_afericao_pa',
+        headerAlign: 'center',
+        headerName: 'PRAZO PARA PRÓXIMA AFERIÇÃO DE PA',
+        renderCell: function noRefCheck() {},
+        width: 130
+      },
+      {
+        align: 'center',
+        field: 'acs_nome_cadastro',
+        headerAlign: 'center',
+        headerName: 'ACS RESPONSÁVEL',
+        width: 250
+      }
+    ],
+    data: [
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '06385182604',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'Diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'até 30/04/23'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '06385182214',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '01/01/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Danilo Lopes Neves',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '06385172614',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '06385582514',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '06385122814',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '06185182914',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '06185182014',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '06385182114',
+        dt_afericao_pressao_mais_recente: '01/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '06885182319',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '04385182415',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '16385182514',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '06385182559',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '46385182664',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      },
+      {
+        acs_nome_cadastro: 'Angelica Souza Toledo Andrade',
+        cpf: '56385182674',
+        dt_afericao_pressao_mais_recente: '22/02/21',
+        dt_consulta_mais_recente: '22/09/21',
+        identificacao_condicao: 'Diagnostico Clinico',
+        mail: 'aangelicasouza24@gmail.com',
+        nome: 'Angelica Souza Toledo Andrade',
+        prazo_proxima_afericao_pa: 'em dia',
+        prazo_proxima_consulta: 'em dia'
+      }
+    ]
+  }}
+  visualizacao="equipe"
+/>
+
         </>
       )
   }}
