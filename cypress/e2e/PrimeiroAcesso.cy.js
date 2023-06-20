13f433a080daaadcb80caf6aa6c38c87ad568cd9
Taina Santana <tainnaps@gmail.com>
2023-06-20 15:20:10 -0300

    testa visibilidade componentes de primeiro acesso

---
+++ cypress/e2e/PrimeiroAcesso.cy.js
@@ -0,0 +1,54 @@
+/// <reference types="Cypress" />
+
+describe('Primeiro acesso', () => {
+  context('resolução 1440 x 900', () => {
+    beforeEach(() => {
+      cy.viewport(1440, 900);
+      cy.visit('/');
+
+      cy.contains(/acesso restrito/i).click();
+
+      cy.get('.NavBar_NavBarModalContainer__tePj9').as('desktopModal');
+    });
+
+    it('O modal desktop é exibido ao clicar no campo ACESSO RESTRITO da navbar', () => {
+      cy.get('@desktopModal').should('be.visible');
+    });
+
+    describe('Dentro do modal desktop', () => {
+      beforeEach(() => {
+        cy.get('@desktopModal')
+          .find('.ButtonLight_ButtonLightContainer__w0rNI')
+          .as('primeiroAcessoButtonModal');
+      });
+
+      it('É exibido o botão de PRIMEIRO ACESSO', () => {
+        cy.get('@primeiroAcessoButtonModal')
+          .should('be.visible')
+          .and('have.text', 'PRIMEIRO ACESSO');
+      });
+
+      describe('Ao clicar no botão de PRIMEIRO ACESSO do modal desktop', () => {
+        beforeEach(() => {
+          cy.get('@primeiroAcessoButtonModal').click();
+        });
+
+        it('É exibida a input de email', () => {
+          cy.get('input[placeholder*="E-mail"]').should('be.visible');
+        });
+
+        it('É exibido o botão VOLTAR', () => {
+          cy.get('@desktopModal')
+            .contains(/voltar/i)
+            .should('be.visible');
+        });
+
+        it('É exibido o botão PRÓXIMO', () => {
+          cy.get('@desktopModal')
+            .contains(/próximo/i)
+            .should('be.visible');
+        });
+      });
+    });
+  });
+});
