92ddb58e85e83dc9e0167be1f4a609e0863a66ca
dlopes14 <donylopes@gmail.com>
2023-06-21 11:34:15 -0300

    add visualicao APS para hipertensao

--- pages/busca-ativa/hipertensos/index.js
+++ pages/busca-ativa/hipertensos/index.js
@@ -1,14 +1,11 @@
 import { PanelSelector, CardAlert, TituloTexto, ButtonLight, PainelBuscaAtiva , ScoreCardGrid } from "@impulsogov/design-system";
 import { useSession,signOut, getSession } from "next-auth/react"
 import React, { useState,useEffect } from 'react';
-import { useRouter } from 'next/router';
 import { getData } from '../../../services/cms'
 import { LAYOUT } from '../../../utils/QUERYS'
-import { DATA_STUDIO_URL_HIPERTENSOS_EQUIPE, DATA_STUDIO_URL_HIPERTENSOS_COORDENACAO_APS,DATA_STUDIO_URL_HIPERTENSOS_COORDENACAO_APS_GRAFICOS } from "../../../constants/dataStudio";
 import { validatetoken} from "../../../services/validateToken"
 import { redirectHome } from "../../../helpers/redirectHome";
 import style from "../../duvidas/Duvidas.module.css"
-import { urlGenBuscaAtivaCoordenacaoAPS,urlGenBuscaAtivaEquipe, urlGenBuscaAtivaCoordenacaoAPSGraficos } from "../../../helpers/urlGenBuscaAtiva";
 import { colunasHipertensao } from "../../../helpers/colunasHipertensao";
 import { tabelaHipertensao } from "../../../services/busca_ativa/Hipertensao";
 
@@ -30,7 +27,7 @@ const Index = ({res}) => {
   const { data: session,status } = useSession()
   const [tokenValido, setTokenValido] = useState();
   const [tabelaData, setTabelaData] = useState();
-  const HipertensaoTabelaData = async()=> await tabelaHipertensao('355060','0001540971',session?.user?.access_token)
+  const HipertensaoTabelaData = async()=> await tabelaHipertensao(session?.user?.municipio,session?.user?.equipe,session?.user?.access_token)
   useEffect(()=>{
       session &&  
       HipertensaoTabelaData().then((response)=>{
@@ -58,7 +55,7 @@ const Index = ({res}) => {
   ]
   if(session){
     console.log(session.user)
-    if(session.user.perfis.includes(5) || session.user.perfis.includes(9)){
+    if(session.user.perfis.includes(9)){
       return (
         <>
           <div className={style.BotaoVoltar}>
@@ -112,63 +109,90 @@ const Index = ({res}) => {
               <PainelBuscaAtiva
                 dadosFiltros={[
                   {
-                    data: [
-                      'equipe sol',
-                      'equipe lua',
-                      'equipe estrela'
-                    ],
-                    filtro: 'nome_equipe',
+                    data: [...new Set(tabelaData.map(item => item.equipe_nome_cadastro))],
+                    filtro: 'equipe_nome_cadastro',
                     rotulo: 'Filtrar por nome da equipe'
                   },
                   {
-                    data: [
-                      '12345678',
-                      '87654321',
-                      '18273645'
-                    ],
-                    filtro: 'ine',
+                    data: [...new Set(tabelaData.map(item => item.equipe_ine_cadastro))],
+                    filtro: 'equipe_ine_cadastro',
                     rotulo: 'Filtrar por INE da equipe'
                   },
                   {
-                    data: [
-                      'Luana',
-                      'Soraia',
-                      'Estela'
-                    ],
+                    data: [...new Set(tabelaData.map(item => item.acs_nome_cadastro))],
                     filtro: 'acs_nome_cadastro',
                     rotulo: 'Filtrar por nome do ACS'
                   },
                   {
-                    data: [
-                      'Diagnostico Clinico',
-                      'Autorreferido'
-                    ],
-                    filtro: 'identificacao_condicao',
+                    data: [...new Set(tabelaData.map(item => item.identificacao_condicao_hipertensao))],
+                    filtro: 'identificacao_condicao_hipertensao',
                     rotulo: 'Filtrar por tipo de diagnóstico'
                   },
-                  {
-                    data: [
-                      'apenas em dia',
-                      'consulta e aferição de PA em dia',
-                      'apenas aferição de PA em dia',
-                      'nada em dia'
-                    ],
-                    filtro: '',
-                    rotulo: 'Filtrar por faixa etária'
-                  }
                 ]}
                 painel="hipertensao"
                 tabela={{
                   colunas: colunasHipertensao,
                   data:tabelaData
                 }}
-                visualizacao="equipe"
               />
             }
         </>
       )
-  }}
-  return(
+  }
+  if(session.user.perfis.includes(5) || session.user.perfis.includes(8)){
+    return (
+      <>
+        <div className={style.BotaoVoltar}>
+        <ButtonLight icone={{posicao: 'right',
+          url: 'https://media.graphassets.com/8NbkQQkyRSiouNfFpLOG'}} 
+          label="VOLTAR" link="/inicio"/>
+        </div>
+        <TituloTexto
+                titulo="Lista Nominal Hipertensão"
+                texto="Oferecemos três listas nominais para monitoramento dos seguintes grupos: gestantes, pessoas com hipertensão e pessoas com diabetes. As listas auxiliam no acompanhamento dos indicadores do Previne Brasil relacionados a esses grupos."
+                imagem = {{posicao: null,url: ''}}
+            />
+        <CardAlert
+              destaque="IMPORTANTE: "
+              msg="Os dados exibidos nesta plataforma refletem a base de dados local do município e podem divergir dos divulgados quadrimestralmente pelo SISAB. O Ministério da Saúde aplica regras de vinculação e validações cadastrais do usuário, profissional e estabelecimento que não são replicadas nesta ferramenta."
+          />  
+          {
+            tabelaData &&
+            <PainelBuscaAtiva
+              dadosFiltros={[
+                {
+                  data: [...new Set(tabelaData.map(item => item.equipe_nome_cadastro))],
+                  filtro: 'equipe_nome_cadastro',
+                  rotulo: 'Filtrar por nome da equipe'
+                },
+                {
+                  data: [...new Set(tabelaData.map(item => item.equipe_ine_cadastro))],
+                  filtro: 'equipe_ine_cadastro',
+                  rotulo: 'Filtrar por INE da equipe'
+                },
+                {
+                  data: [...new Set(tabelaData.map(item => item.acs_nome_cadastro))],
+                  filtro: 'acs_nome_cadastro',
+                  rotulo: 'Filtrar por nome do ACS'
+                },
+                {
+                  data: [...new Set(tabelaData.map(item => item.identificacao_condicao_hipertensao))],
+                  filtro: 'identificacao_condicao_hipertensao',
+                  rotulo: 'Filtrar por tipo de diagnóstico'
+                },
+              ]}
+              painel="hipertensao"
+              tabela={{
+                colunas: colunasHipertensao,
+                data:tabelaData
+              }}
+            />
+          }
+      </>
+    )
+}
+}
+return(
     <p>{status}</p>
   )
 }
--- services/busca_ativa/Hipertensao.js
+++ services/busca_ativa/Hipertensao.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { API_URL } from "../../constants/API_URL";
 
-const tabelaHipertensao = async(municipio_id_sus,equipe,token)=>{
+const tabelaHipertensao = async(municipio_uf,equipe,token)=>{
     let config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: API_URL + `impulsoprevine/busca-ativa/hipertensao-por-equipe?municipio_id_sus=${municipio_id_sus}&equipe=${equipe}`,
+        url: API_URL + `impulsoprevine/busca-ativa/hipertensao-por-equipe?municipio_uf=${municipio_uf}&equipe=${equipe}`,
         headers: { 
           'Authorization': 'Bearer ' + token
         }
