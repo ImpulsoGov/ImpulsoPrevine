2b93468bf54a74010b538c5c53f5640e65163d62
dlopes14 <donylopes@gmail.com>
2023-06-26 15:06:58 -0300

    fix build

--- pages/busca-ativa/hipertensos/index.js
+++ pages/busca-ativa/hipertensos/index.js
@@ -43,6 +43,14 @@ const Index = ({res}) => {
       setTabelaDataAPS(response)
   })},[session]) 
 
+  const [tabelaDataEquipe, setTabelaDataEquipe] = useState();
+  const HipertensaoTabelaDataEquipe = async()=> await tabelaHipertensaoEquipe(session?.user?.municipio,session?.user?.equipe,session?.user?.access_token)
+  useEffect(()=>{
+    session &&  session.user.perfis.includes(9) &&
+    HipertensaoTabelaDataEquipe().then((response)=>{
+      setTabelaDataEquipe(response)
+  })},[session]) 
+
   useEffect(()=>{
       if(session){
         validatetoken(session?.user?.access_token)
@@ -65,13 +73,6 @@ const Index = ({res}) => {
   ]
   if(session){  
     if(session.user.perfis.includes(9)){
-      const [tabelaDataEquipe, setTabelaDataEquipe] = useState();
-      const HipertensaoTabelaDataEquipe = async()=> await tabelaHipertensaoEquipe(session?.user?.municipio,session?.user?.equipe,session?.user?.access_token)
-      useEffect(()=>{
-        session &&  session.user.perfis.includes(9) &&
-        HipertensaoTabelaDataEquipe().then((response)=>{
-          setTabelaDataEquipe(response)
-      })},[session]) 
         return (
         <>
           <div className={style.BotaoVoltar}>
@@ -158,7 +159,6 @@ const Index = ({res}) => {
   if(session.user.perfis.includes(5) || session.user.perfis.includes(8)){
     return (
       <>
-        
         <div className={style.BotaoVoltar}>
         <ButtonLight icone={{posicao: 'right',
           url: 'https://media.graphassets.com/8NbkQQkyRSiouNfFpLOG'}} 
@@ -172,217 +172,230 @@ const Index = ({res}) => {
         <CardAlert
               destaque="IMPORTANTE: "
               msg="Os dados exibidos nesta plataforma refletem a base de dados local do município e podem divergir dos divulgados quadrimestralmente pelo SISAB. O Ministério da Saúde aplica regras de vinculação e validações cadastrais do usuário, profissional e estabelecimento que não são replicadas nesta ferramenta."
-          />  
-            {
-              tabelaDataAPS &&
-              <ScoreCardGrid
-                valores={[
+        />  
+        <div 
+          style={{
+            marginLeft : "80px",
+            marginTop : "30px",
+            color: "#1F1F1F",
+            fontSize: "22px",
+            fontFamily: "Inter",
+            fontWeight: 500,
+            lineHeight: "130%",
+          }}
+        >
+          {session.user.municipio} - Q1/23
+        </div>
+        {
+          tabelaDataAPS &&
+          <ScoreCardGrid
+            valores={[
+              {
+                descricao: 'Total de pessoas com hipertensão',
+                valor: tabelaDataAPS.length
+              },
+              {
+                descricao: 'Total de pessoas com consulta e aferição de PA em dia',
+                valor: tabelaDataAPS.reduce((acumulador,item)=>{ 
+                  return (item.prazo_proxima_consulta == "Em dia" && item.prazo_proxima_afericao_pa == "Em dia") ?
+                  acumulador + 1 : acumulador;
+                },0)
+              },
+              {
+                descricao: 'Total de pessoas com diagnóstico autorreferido',
+                valor: tabelaDataAPS.reduce((acumulador,item)=>{ 
+                  return (item.identificacao_condicao_hipertensao == "Autorreferida") ?
+                  acumulador + 1 : acumulador;
+                },0)
+              },
+              {
+                descricao: 'Total de pessoas com diagnóstico clínico',
+                valor: tabelaDataAPS.reduce((acumulador,item)=>{ 
+                  return (item.identificacao_condicao_hipertensao == "Diagnóstico Clínico") ?
+                  acumulador + 1 : acumulador;
+                },0)
+              }
+            ]}
+          />
+        }
+        {
+          tabelaDataAPS &&
+          <GraficoBuscaAtiva
+          dataBarra={{
+            color: [
+              '#1D856C',
+              '#2EB280',
+              '#55D499',
+              '#9DEECD'
+            ],
+            grid: {
+              containLabel: true,
+              top: '20%'
+            },
+            legend: {
+              data: [
+                'Apenas consulta em dia',
+                'consulta e aferição de PA em dia',
+                'Apenas aferição de PA em dia',
+                'Nada em dia'
+              ],
+              top: 'top'
+            },
+            series: [
+              {
+                data: Object.values(tabelaDataAPS.reduce((acumulador,item)=>{ 
+                  if(item.prazo_proxima_consulta == "Em dia") acumulador[item.equipe_ine_cadastro] = (acumulador[item.equipe_ine_cadastro] || 0) + 1
+                  return acumulador
+                },{})),
+                name: 'Apenas consulta em dia',
+                stack: 'stack',
+                type: 'bar'
+              },
+              {
+                data: Object.values(tabelaDataAPS.reduce((acumulador,item)=>{ 
+                  if(item.prazo_proxima_consulta == "Em dia" && item.prazo_proxima_afericao_pa == "Em dia") acumulador[item.equipe_ine_cadastro] = (acumulador[item.equipe_ine_cadastro] || 0) + 1
+                  return acumulador
+                },{})),
+                name: 'consulta e aferição de PA em dia',
+                stack: 'stack',
+                type: 'bar'
+              },
+              {
+                data: Object.values(tabelaDataAPS.reduce((acumulador,item)=>{ 
+                  if(item.prazo_proxima_afericao_pa == "Em dia") acumulador[item.equipe_ine_cadastro] = (acumulador[item.equipe_ine_cadastro] || 0) + 1
+                  return acumulador
+                },{})),
+                name: 'Apenas aferição de PA em dia',
+                stack: 'stack',
+                type: 'bar'
+              },
+              {
+                data: Object.values(tabelaDataAPS.reduce((acumulador,item)=>{ 
+                  if(item.prazo_proxima_consulta != "Em dia" && item.prazo_proxima_afericao_pa != "Em dia") acumulador[item.equipe_ine_cadastro] = (acumulador[item.equipe_ine_cadastro] || 0) + 1
+                  return acumulador
+                },{})),
+                name: 'Nada em dia',
+                stack: 'stack',
+                type: 'bar'
+              }
+            ],
+            tooltip: {
+              trigger: 'axis'
+            },
+            xAxis: {
+              data: [...new Set(tabelaDataAPS.map(item => item.equipe_nome_cadastro))],
+              type: 'category'
+            },
+            yAxis: {
+              type: 'value'
+            }
+          }}
+          dataRosca={{
+            color: [
+              '#1D856C',
+              '#2EB280',
+              '#55D499',
+              '#9DEECD'
+            ],
+            series: [
+              {
+                avoidLabelOverlap: false,
+                data: [
                   {
-                    descricao: 'Total de pessoas com hipertensão',
-                    valor: tabelaDataAPS.length
+                    name: 'Apenas consulta em dia',
+                    value: Math.round((tabelaDataAPS.reduce((acumulador,item)=>{ 
+                      return (item.prazo_proxima_consulta == "Em dia") ?
+                      acumulador + 1 : acumulador;
+                    },0)*100)/tabelaDataAPS.length)
                   },
                   {
-                    descricao: 'Total de pessoas com consulta e aferição de PA em dia',
-                    valor: tabelaDataAPS.reduce((acumulador,item)=>{ 
+                    name: 'Consulta e Aferição em dia',
+                    value: Math.round((tabelaDataAPS.reduce((acumulador,item)=>{ 
                       return (item.prazo_proxima_consulta == "Em dia" && item.prazo_proxima_afericao_pa == "Em dia") ?
                       acumulador + 1 : acumulador;
-                    },0)
+                    },0)*100)/tabelaDataAPS.length)
                   },
                   {
-                    descricao: 'Total de pessoas com diagnóstico autorreferido',
-                    valor: tabelaDataAPS.reduce((acumulador,item)=>{ 
-                      return (item.identificacao_condicao_hipertensao == "Autorreferida") ?
+                    name: 'Apenas Aferição de PA em dia',
+                    value: Math.round((tabelaDataAPS.reduce((acumulador,item)=>{ 
+                      return (item.prazo_proxima_afericao_pa == "Em dia") ?
                       acumulador + 1 : acumulador;
-                    },0)
+                    },0)*100)/tabelaDataAPS.length)
                   },
                   {
-                    descricao: 'Total de pessoas com diagnóstico clínico',
-                    valor: tabelaDataAPS.reduce((acumulador,item)=>{ 
-                      return (item.identificacao_condicao_hipertensao == "Diagnóstico Clínico") ?
-                      acumulador + 1 : acumulador;
-                    },0)
-                  }
-                ]}
-             />
-            }
-            {
-              tabelaDataAPS &&
-              <GraficoBuscaAtiva
-              dataBarra={{
-                color: [
-                  '#1D856C',
-                  '#2EB280',
-                  '#55D499',
-                  '#9DEECD'
-                ],
-                grid: {
-                  containLabel: true,
-                  top: '20%'
-                },
-                legend: {
-                  data: [
-                    'Apenas consulta em dia',
-                    'consulta e aferição de PA em dia',
-                    'Apenas aferição de PA em dia',
-                    'Nada em dia'
-                  ],
-                  top: 'top'
-                },
-                series: [
-                  {
-                    data: Object.values(tabelaDataAPS.reduce((acumulador,item)=>{ 
-                      if(item.prazo_proxima_consulta == "Em dia") acumulador[item.equipe_ine_cadastro] = (acumulador[item.equipe_ine_cadastro] || 0) + 1
-                      return acumulador
-                    },{})),
-                    name: 'Apenas consulta em dia',
-                    stack: 'stack',
-                    type: 'bar'
-                  },
-                  {
-                    data: Object.values(tabelaDataAPS.reduce((acumulador,item)=>{ 
-                      if(item.prazo_proxima_consulta == "Em dia" && item.prazo_proxima_afericao_pa == "Em dia") acumulador[item.equipe_ine_cadastro] = (acumulador[item.equipe_ine_cadastro] || 0) + 1
-                      return acumulador
-                    },{})),
-                    name: 'consulta e aferição de PA em dia',
-                    stack: 'stack',
-                    type: 'bar'
-                  },
-                  {
-                    data: Object.values(tabelaDataAPS.reduce((acumulador,item)=>{ 
-                      if(item.prazo_proxima_afericao_pa == "Em dia") acumulador[item.equipe_ine_cadastro] = (acumulador[item.equipe_ine_cadastro] || 0) + 1
-                      return acumulador
-                    },{})),
-                    name: 'Apenas aferição de PA em dia',
-                    stack: 'stack',
-                    type: 'bar'
-                  },
-                  {
-                    data: Object.values(tabelaDataAPS.reduce((acumulador,item)=>{ 
-                      if(item.prazo_proxima_consulta != "Em dia" && item.prazo_proxima_afericao_pa != "Em dia") acumulador[item.equipe_ine_cadastro] = (acumulador[item.equipe_ine_cadastro] || 0) + 1
-                      return acumulador
-                    },{})),
                     name: 'Nada em dia',
-                    stack: 'stack',
-                    type: 'bar'
+                    value: Math.round((tabelaDataAPS.reduce((acumulador,item)=>{ 
+                      return (item.prazo_proxima_consulta != "Em dia" && item.prazo_proxima_afericao_pa != "Em dia") ?
+                      acumulador + 1 : acumulador;
+                    },0)*100)/tabelaDataAPS.length)
                   }
                 ],
-                tooltip: {
-                  trigger: 'axis'
-                },
-                xAxis: {
-                  data: [...new Set(tabelaDataAPS.map(item => item.equipe_nome_cadastro))],
-                  type: 'category'
-                },
-                yAxis: {
-                  type: 'value'
-                }
-              }}
-              dataRosca={{
-                color: [
-                  '#1D856C',
-                  '#2EB280',
-                  '#55D499',
-                  '#9DEECD'
-                ],
-                series: [
-                  {
-                    avoidLabelOverlap: false,
-                    data: [
-                      {
-                        name: 'Apenas consulta em dia',
-                        value: Math.round((tabelaDataAPS.reduce((acumulador,item)=>{ 
-                          return (item.prazo_proxima_consulta == "Em dia") ?
-                          acumulador + 1 : acumulador;
-                        },0)*100)/tabelaDataAPS.length)
-                      },
-                      {
-                        name: 'Consulta e Aferição em dia',
-                        value: Math.round((tabelaDataAPS.reduce((acumulador,item)=>{ 
-                          return (item.prazo_proxima_consulta == "Em dia" && item.prazo_proxima_afericao_pa == "Em dia") ?
-                          acumulador + 1 : acumulador;
-                        },0)*100)/tabelaDataAPS.length)
-                      },
-                      {
-                        name: 'Apenas Aferição de PA em dia',
-                        value: Math.round((tabelaDataAPS.reduce((acumulador,item)=>{ 
-                          return (item.prazo_proxima_afericao_pa == "Em dia") ?
-                          acumulador + 1 : acumulador;
-                        },0)*100)/tabelaDataAPS.length)
-                      },
-                      {
-                        name: 'Nada em dia',
-                        value: Math.round((tabelaDataAPS.reduce((acumulador,item)=>{ 
-                          return (item.prazo_proxima_consulta != "Em dia" && item.prazo_proxima_afericao_pa != "Em dia") ?
-                          acumulador + 1 : acumulador;
-                        },0)*100)/tabelaDataAPS.length)
-                      }
-                    ],
-                    emphasis: {
-                      label: {
-                        fontSize: '20',
-                        fontWeight: 'bold',
-                        show: true
-                      }
-                    },
-                    label: {
-                      formatter: '{c}%',
-                      position: 'inside',
-                      show: true,
-                      textStyle: {
-                        color: 'white',
-                        fontSize: 12
-                      }
-                    },
-                    labelLine: {
-                      show: false
-                    },
-                    name: 'Gráfico de rosca',
-                    radius: [
-                      '35%',
-                      '70%'
-                    ],
-                    type: 'pie'
+                emphasis: {
+                  label: {
+                    fontSize: '20',
+                    fontWeight: 'bold',
+                    show: true
                   }
+                },
+                label: {
+                  formatter: '{c}%',
+                  position: 'inside',
+                  show: true,
+                  textStyle: {
+                    color: 'white',
+                    fontSize: 12
+                  }
+                },
+                labelLine: {
+                  show: false
+                },
+                name: 'Gráfico de rosca',
+                radius: [
+                  '35%',
+                  '70%'
                 ],
-                tooltip: {
-                  formatter: '{b}',
-                  trigger: 'item'
-                }
-              }}
-            />
+                type: 'pie'
+              }
+            ],
+            tooltip: {
+              formatter: '{b}',
+              trigger: 'item'
             }
-          {
-            tabelaDataAPS ?
-            <PainelBuscaAtiva
-              dadosFiltros={[
-                {
-                  data: [...new Set(tabelaDataAPS.map(item => item.equipe_nome_cadastro))],
-                  filtro: 'equipe_nome_cadastro',
-                  rotulo: 'Filtrar por nome da equipe'
-                },
-                {
-                  data: [...new Set(tabelaDataAPS.map(item => item.equipe_ine_cadastro))],
-                  filtro: 'equipe_ine_cadastro',
-                  rotulo: 'Filtrar por INE da equipe'
-                },
-                {
-                  data: [...new Set(tabelaDataAPS.map(item => item.acs_nome_cadastro))],
-                  filtro: 'acs_nome_cadastro',
-                  rotulo: 'Filtrar por nome do ACS'
-                },
-                {
-                  data: [...new Set(tabelaDataAPS.map(item => item.identificacao_condicao_hipertensao))],
-                  filtro: 'identificacao_condicao_hipertensao',
-                  rotulo: 'Filtrar por tipo de diagnóstico'
-                },
-              ]}
-              painel="hipertensao"
-              tabela={{
-                colunas: colunasHipertensao,
-                data:tabelaDataAPS
-              }}
-            /> : <Spinner/>
-          }
+          }}
+          />
+        }
+        {
+          tabelaDataAPS ?
+          <PainelBuscaAtiva
+            dadosFiltros={[
+              {
+                data: [...new Set(tabelaDataAPS.map(item => item.equipe_nome_cadastro))],
+                filtro: 'equipe_nome_cadastro',
+                rotulo: 'Filtrar por nome da equipe'
+              },
+              {
+                data: [...new Set(tabelaDataAPS.map(item => item.equipe_ine_cadastro))],
+                filtro: 'equipe_ine_cadastro',
+                rotulo: 'Filtrar por INE da equipe'
+              },
+              {
+                data: [...new Set(tabelaDataAPS.map(item => item.acs_nome_cadastro))],
+                filtro: 'acs_nome_cadastro',
+                rotulo: 'Filtrar por nome do ACS'
+              },
+              {
+                data: [...new Set(tabelaDataAPS.map(item => item.identificacao_condicao_hipertensao))],
+                filtro: 'identificacao_condicao_hipertensao',
+                rotulo: 'Filtrar por tipo de diagnóstico'
+              },
+            ]}
+            painel="hipertensao"
+            tabela={{
+              colunas: colunasHipertensao,
+              data:tabelaDataAPS
+            }}
+          /> : <Spinner/>
+        }
       </>
     )
 }
