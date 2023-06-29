a070a26312e129646223fb93b0a0c816967b0f97
NicoliSte <nickstefan4@gmail.com>
2023-06-29 16:16:08 -0300

    atualizacao textos

--- pages/analise/indicadores/index.js
+++ pages/analise/indicadores/index.js
@@ -1,10 +1,10 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { getData } from '../../../services/cms';
 import { LAYOUT } from '../../../utils/QUERYS';
 import { redirectHomeNotLooged } from "../../../helpers/redirectHome";
 import { v1 as uuidv1 } from 'uuid';
-import { ButtonLight, PanelSelectorSM, TituloTexto, GraficoInfo , Grid12Col, TituloSmallTexto} from "@impulsogov/design-system"
+import { ButtonLight, PanelSelectorSM, TituloTexto, GraficoInfo, Grid12Col, TituloSmallTexto } from "@impulsogov/design-system"
 
 export async function getServerSideProps() {
   const res = [
@@ -12,12 +12,12 @@ export async function getServerSideProps() {
   ]
   return {
     props: {
-      res : res
+      res: res
     }
   }
 }
 
-const Indicadores = ({res}) => {
+const Indicadores = ({ res }) => {
   const router = useRouter();
   const [activeTabIndex, setActiveTabIndex] = useState(Number(router.query?.painel));
   const [activeTitleTabIndex, setActiveTitleTabIndex] = useState(0);
@@ -37,37 +37,38 @@ const Indicadores = ({res}) => {
   return (
     <div>
       <TituloSmallTexto
-          imagem={{
-            posicao: null,
-            url: ''
-          }}
-          texto="Nessa página você vai encontrar o histórico de desempenho do seu município, os resultados dos indicadores na competência atual e nos quadrimestres anteriores, além de informações sobre como bater a meta em cada um deles e recomendações."
-          titulo="<b> Indicadores de desempenho </b>"
-        />
-                
-        <Grid12Col
-          items={[
-            <GraficoInfo descricao="Verifique como foi o desempenho do seu município em relação a meta preconizada pelo Ministério da Saúde para as <b> equipes avaliadas** no quadrimestre selecionado. </b> Veja também o desempenho nos quadrimestres passados. " destaque="" fonte="" link={{   label: '',   url: '/' }} titulo="Desempenho do Município" tooltip=""/> ,
-            <GraficoInfo descricao="Verifique como foi o desempenho do seu município em relação a meta preconizada pelo Ministério da Saúde para as <b> equipes avaliadas** no quadrimestre selecionado. </b> Veja também o desempenho nos quadrimestres passados. " destaque="" fonte="" link={{   label: '',   url: '/' }} titulo="Desempenho do Município" tooltip=""/>
-          ]}
-        />
+        imagem={{
+          posicao: null,
+          url: ''
+        }}
+        texto="Nessa página você vai encontrar o histórico de desempenho do seu município, os resultados dos indicadores na competência atual e nos quadrimestres anteriores, além de informações sobre como bater a meta em cada um deles e recomendações."
+        titulo="<b> Indicadores de desempenho </b>"
+      />
 
-          <GraficoInfo
-            descricao="<b> Fonte: SISAB </b>
-          <br>
-          *As regras aplicadas no cálculo dos indicadores seguem a NOTA TÉCNICA Nº 12/2022 (SAPS/MS) referente à metodologia de cálculo vigente.
-          O resultado do Indicador “Cobertura vacinal de Poliomielite inativada e de Pentavalente” considera o alcance de 100% devido à correção da divergência no método de cálculo em 2021, para fins de cálculo do ISF. Os demais resultados dos indicadores estão com o percentual de alcance real.
-          <br></br>
-          **Equipes avaliadas: Equipes eSF, eAP, eCR, eAPP e eSFR ativas e credenciadas pelo Ministério da Saúde, cadastradas no SCNES pela gestão municipal, distrital ou estadual, homologadas pelo Ministério da Saúde e com ausência de irregularidades que justifiquem a suspensão de 100% (cem por cento) dos incentivos financeiros. "
-            destaque=""
-            fonte=""
-            link={{
-              label: '',
-              url: '/'
-            }}
-            titulo=""
-            tooltip=""
-          />
+      <Grid12Col
+        items={[
+          <GraficoInfo descricao="Verifique como foi o desempenho do seu município em relação a meta preconizada pelo Ministério da Saúde para as <b> equipes avaliadas** no quadrimestre selecionado. </b> Veja também o desempenho nos quadrimestres passados. " destaque="" fonte="" link={{ label: '', url: '/' }} titulo="Desempenho do Município" tooltip="" />,
+          <GraficoInfo descricao="Veja o <b> histórico de desempenho (%) </b> geral do seu município ao longo do tempo ou selecione um (ou mais) indicador(es) por vez. " destaque="" fonte="" link={{ label: '', url: '/' }} titulo="Histórico de Desempenho" tooltip="" />
+        ]}
+      />
+
+      <GraficoInfo
+        descricao="<b> Fonte: SISAB </b>
+        <br>
+        *As regras aplicadas no cálculo dos indicadores seguem a <a href='https://www.conasems.org.br/wp-content/uploads/2022/07/SEI_MS-0027964163-Nota-Tecnica-12.pdf' target='_blank' rel='noreferrer nofollow noopener'><u> NOTA TÉCNICA Nº 12/2022 (SAPS/MS) </u></a> referente à metodologia de cálculo vigente.
+        O resultado do Indicador “<b> Cobertura vacinal de Poliomielite inativada e de Pentavalente </b>” considera o alcance de 100% devido à correção da divergência no método de cálculo em 2021, para fins de cálculo do ISF. Os demais resultados dos indicadores estão com o percentual de alcance real.
+        <br></br>
+        **Equipes avaliadas: Equipes eSF, eAP, eCR, eAPP e eSFR ativas e credenciadas pelo Ministério da Saúde, cadastradas no SCNES pela gestão municipal, distrital ou estadual, homologadas pelo Ministério da Saúde e com ausência de irregularidades que justifiquem a suspensão de 100% (cem por cento) dos incentivos financeiros. "
+        destaque=""
+        fonte=""
+        link={{
+          label: '',
+          url: '/'
+          
+        }}
+        titulo=""
+        tooltip=""
+      />
     </div>
   )
 }
