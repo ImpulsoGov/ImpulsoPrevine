7f59a9a4b18885ea33051ed7ee1b2c29b76bd83b
NicoliSte <nickstefan4@gmail.com>
2023-07-05 01:45:44 -0300

    Modificação na tabela de indicadores

--- componentes/TabelaIndicadores/TabelaIndicadores.jsx
+++ componentes/TabelaIndicadores/TabelaIndicadores.jsx
@@ -1,4 +1,5 @@
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbarContainer, GridToolbarColumnsButton, GridToolbarFilterButton, GridToolbarExport } from '@mui/x-data-grid';
+import Tooltip from '@mui/material/Tooltip';
 import React, { useCallback, useMemo } from 'react';
 import { v4 as uuidV4 } from 'uuid';
 import styles from './Tabelas.module.css';
@@ -9,7 +10,11 @@ const TabelaIndicadores = ({ TabIndicadores }) => {
     {
       field: 'ordenacao',
       headerName: 'Ordenação',
-      flex: 75
+      flex: 75,
+      description:
+        'A ordenação dos indicadores é uma sugestão feita, levando em consideração o peso do indicador, duração, nota do indicador e quanto falta para alcançar a meta.',
+      headerClassName: styles.cabecalho,
+      
     },
     {
       field: 'indicador',
@@ -17,6 +22,9 @@ const TabelaIndicadores = ({ TabIndicadores }) => {
       flex: 70,
       align: 'center',
       headerAlign: 'center',
+      description: 
+      '',
+      headerClassName: styles.cabecalho,      
     },
     {
       field: 'dominadorInformado',
@@ -24,6 +32,11 @@ const TabelaIndicadores = ({ TabIndicadores }) => {
       flex: 140,
       align: 'center',
       headerAlign: 'center',
+      description:'O denominador utilizado pode ser o informado (identificado) ou o estimado.'+
+      'O denominador informado - é o número de pessoas cadastradas informado pelo município, que impactam o indicador (ex.: o número de gestantes cadastradas é utilizado como referência para o indicador de pré-natal).' +
+      'O denominador estimado -  é uma estimativa criada pelo ministério da saúde de quantas pessoas o município deveria ter cadastradas naquela categoria. O denominador utilizado será o informado caso o valor seja igual ou maior que 85% do denominador estimado. Caso ele seja menor, o denominador estimado será utilizado para cálculo da meta.'+
+      'Nesse campo, é possível comparar o denominador utilizado e o informado, para saber quão próximo dos 85% está, caso o utilizado seja o estimado.',
+      headerClassName: styles.cabecalho,
     },
     {
       field: 'N_totaldePessoas_baterMeta',
@@ -31,6 +44,9 @@ const TabelaIndicadores = ({ TabIndicadores }) => {
       flex: 85,
       align: 'center',
       headerAlign: 'center',
+      description: 
+      'A partir de dados do SISAB, fazemos um cálculo de quantas pessoas devem ser atendidas dentro de cada indicador, para que o município atinja a meta estabelecida. Se o número indicado for igual a zero, isso significa que o município já atingiu ou ultrapassou a meta estabelecida pelo Previne Brasil.',
+      headerClassName: styles.cabecalho,
     },
     {
       field: 'PessoasNaoCadastradas',
@@ -38,6 +54,9 @@ const TabelaIndicadores = ({ TabIndicadores }) => {
       flex: 280,
       align: 'center',
       headerAlign: 'center',
+      description: 
+      'A partir dos denominadores informado e estimado, calculamos dentro do número total de pessoas para bater a meta e quantas o município ainda não cadastrou (de acordo com o denominador estimado) e ainda precisam ser cadastradas, acompanhadas e atendidas, para que a meta seja cumprida.',
+      headerClassName: styles.cabecalho,
     },
     {
       field: 'Nota',
@@ -45,6 +64,9 @@ const TabelaIndicadores = ({ TabIndicadores }) => {
       flex: 60,
       align: 'center',
       headerAlign: 'center',
+      description: 
+      '',
+      headerClassName: styles.cabecalho,
     },
     {
       field: 'VariacaoDesempenho',
@@ -52,6 +74,9 @@ const TabelaIndicadores = ({ TabIndicadores }) => {
       flex: 95,
       align: 'center',
       headerAlign: 'center',
+      description: 
+      'Calculamos a variação percentual entre o desempenho da competência atual e da competência anterior (como mostrado no gráfico de histórico de desempenho), para que o município saiba quão grande foi o crescimento ou a queda de um quadrimestre para o outro.',
+      headerClassName: styles.cabecalho,
     },
     {
       field: 'Recomendacoes',
@@ -59,6 +84,9 @@ const TabelaIndicadores = ({ TabIndicadores }) => {
       flex: 100,
       align: 'center',
       headerAlign: 'center',
+      description: 
+      '',
+      headerClassName: styles.cabecalho,
     },
   ]);
 
@@ -88,20 +116,46 @@ const TabelaIndicadores = ({ TabIndicadores }) => {
     }));
   }, [TabIndicadores]);
 
+  const CustomHeader = () => {
+    return (
+      <div className={styles.customHeader}>
+        <GridToolbarContainer>
+          <GridToolbarColumnsButton />
+          <GridToolbarFilterButton />
+          <GridToolbarExport />
+        </GridToolbarContainer>
+      </div>
+    );
+  };
+
   return (
-    <div>
+
+    <div style={{ height: '100%', width: '100%' }}>
       <DataGrid
         sx={{
           '& .MuiDataGrid-columnHeaderTitle': {
             fontWeight: 'bold',
             fontSize: '16px',
-            lineHeight: '1.2rem',
+            lineHeight: '1.1rem',
             whiteSpace: 'normal',
-            textAlign: 'center'
+            textAlign: 'center',
+          },
+          '& .MuiDataGrid-toolbarContainer': {
+            backgroundColor: 'black', // cor de fundo da barra de ferramentas
+          },
+          '& .MuiButton-root': {
+            color: '#D4DBE7', // cor do texto dos botões
+          },
+          '& .MuiButton-outlined': {
+            borderColor: '#D4DBE7', //  cor da borda dos botões
           },
         }}
         rows={linhas}
         columns={colunas}
+        density="comfortable"
+        components={{
+          Toolbar: CustomHeader,
+        }}
         autoHeight
         hideFooter
         disableColumnMenu
@@ -110,6 +164,7 @@ const TabelaIndicadores = ({ TabIndicadores }) => {
             sortModel: [{ field: 'ordenacao', sort: 'asc' }],
           },
         }}
+        rowHeight={30} // altura das linhas
       />
     </div>
   );
