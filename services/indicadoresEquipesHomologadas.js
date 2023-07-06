96e78f0c6787f600dc5c657993338647a817aa5f
NicoliSte <nickstefan4@gmail.com>
2023-07-06 13:24:37 -0300

    services indicadores IP

---
+++ services/indicadoresEquipesHomologadas.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { API_URL } from "../constants/API_URL";
+
+const indicadoresEquipesHomologadas = async (municipio_uf) => {
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: API_URL + `/impulsoprevine/indicadores/municipios_equipes_homologadas'=${municipio_uf}`,
+    headers: {},
+  };
+  const res = await axios(config)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      return error.response.data;
+    });
+  return res
+}
+
+  export {indicadoresEquipesHomologadas}
