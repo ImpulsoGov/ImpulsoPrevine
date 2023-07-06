7bf5bc4442f6686316d7167ef95294f52ba92218
NicoliSte <nickstefan4@gmail.com>
2023-07-06 13:31:46 -0300

    Atualizaçao services

--- services/indicadoresEquipesHomologadas.js
+++ services/indicadoresEquipesHomologadas.js
@@ -5,7 +5,7 @@ const indicadoresEquipesHomologadas = async (municipio_uf) => {
   let config = {
     method: 'get',
     maxBodyLength: Infinity,
-    url: API_URL + `/impulsoprevine/indicadores/municipios_equipes_homologadas'=${municipio_uf}`,
+    url: API_URL + `/impulsoprevine/indicadores/municipios_equipes_homologadas=${municipio_uf}`,
     headers: {},
   };
   const res = await axios(config)
