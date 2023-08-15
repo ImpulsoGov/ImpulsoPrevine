a7530ac2cabd94ebf8bc5a3d20ccd209fc80d136
gabriellearruda <gabriellearrudac@gmail.com>
2023-08-15 11:36:42 -0300

    atualiza texto e imagens

--- pages/index.js
+++ pages/index.js
@@ -59,10 +59,10 @@ const Index = ({res}) => {
             <div style={{paddingTop:80}}></div>
             <NovoTituloTexto
               titulo="Ajudamos profissionais do SUS na gestão da atenção primária"
-              texto="Somos parte de uma organização não governamental financiada por entidades filantrópicas para desenvolver a saúde pública <b>de forma totalmente gratuita.</b>"
+              texto="O Impulso Previne é uma iniciativa realizada pela ImpulsoGov, uma organização não governamental sem fins lucrativos que apoia municípios na gestão da saúde pública <b>de forma totalmente gratuita.</b>"
             />
             <div style={{paddingTop:75}}></div>
-            <ImagensFull2 imagem="https://media.graphassets.com/zSjt74JRTda4QD5B6kRJ" />
+            <ImagensFull2 imagem="https://media.graphassets.com/xGxFkzz9Q5eTik6An7uJ" />
             <div style={{paddingTop:75}}></div>
           </>
         } 
@@ -81,12 +81,12 @@ const Index = ({res}) => {
                 imagem={{posicao: null,url: ''}}
                 supertitulo=""
                 titulo="<b>Suporte para coordenação e assistência da APS</b>"
-                texto="Melhore a atuação da sua unidade e do seu município com:"
+                texto="Melhore a atuação da sua unidade de saúde e do seu município com:"
               />
             <Grid12Col
             proporcao="7-5"
             items={ [
-              <ImagensFull2 key={ uuidv1() } imagem="https://media.graphassets.com/fBWSb6X9QbyS6G1wX0I9" />,
+              <ImagensFull2 key={ uuidv1() } imagem="https://media.graphassets.com/55Ovc152SoahfSNlqFIZ" />,
               <><div style={{alignContent:"center"}}>
                   <TituloSmallTexto
                     key={ uuidv1() }
@@ -94,7 +94,7 @@ const Index = ({res}) => {
                     imagem={{posicao: null,url: ''}}
                     supertitulo="<b>Análises do Previne</b>"
                     titulo=""
-                    texto="Veja o desempenho de qualquer município nas categorias do Previne Brasil e sugestões para alcançar as metas definidas.<br><br>"
+                    texto="Veja o desempenho de qualquer município nos componentes do Previne Brasil e sugestões para alcançar as metas definidas.<br><br>"
                   />
                 </div></>,
             ] }
@@ -108,22 +108,22 @@ const Index = ({res}) => {
                 imagem={{posicao: null,url: ''}}
                 supertitulo="<b>Mentorias exclusivas</b>"
                 titulo=""
-                texto="Seja nosso parceiro para receber apoio especializado da nossa equipe e treinamentos focados nos seus desafios.<br/><br/>"
+                texto="Informações sempre atualizadas sobre atenção primária à saúde diretamente por e-mail, no nosso blog e em nossas capacitações.<br/><br/>"
               />,
-              <ImagensFull2 key={ uuidv1() } imagem="https://media.graphassets.com/WX9nahBsT2qFcm0luXeI" />
+              <ImagensFull2 key={ uuidv1() } imagem="https://media.graphassets.com/myRTKSALRUChuOhlEnQT" />
             ] }
           />
           <Grid12Col
             proporcao="7-5"
             items={ [
-              <ImagensFull2 key={ uuidv1() } imagem="https://media.graphassets.com/WJpXZINRS16gcFMCysFe" />,
+              <ImagensFull2 key={ uuidv1() } imagem="https://media.graphassets.com/kuiOKdlR5iR2nFrnAjsk" />,
               <TituloSmallTexto
                 key={ uuidv1() }
                 botao={{label: 'VER MATERIAIS',url: '/blog'}}
                 imagem={{posicao: null,url: ''}}
                 supertitulo="<b>Conteúdos gratuitos</b>"
                 titulo=""
-                texto="Confira informações atualizadas sobre atenção primária à saúde nos artigos do nosso blog e em nossas capacitações.<br><br>"
+                texto="Seja nosso(a) parceiro(a) para receber apoio especializado da nossa equipe e treinamentos focados nos desafios do seu município.<br><br>"
               />
             ] }
           />
@@ -153,7 +153,7 @@ const Index = ({res}) => {
                 <CardIP
                   key={ uuidv1() }
                   titulo=""
-                  indicador="+12 mil profissionais"
+                  indicador="+12 mil profissionais do SUS"
                   descricao="recebem dicas semanais sobre APS"
                 />,
                 <CardIP
@@ -179,7 +179,7 @@ const Index = ({res}) => {
                     botao={{label: '',url: ''}}
                     imagem={{posicao: null,url: ''}}
                     supertitulo=""
-                    titulo="<b>Resultados na prática</b>"
+                    titulo="<b>Estamos fazendo a diferença nos municípios</b>"
                     texto=""
                   />,
                   <></>,
@@ -187,13 +187,13 @@ const Index = ({res}) => {
                     key={ uuidv1() }
                     titulo="PARCEIRO DESDE 2022"
                     indicador="Brejo de Areia/MA"
-                    descricao="O uso das ferramentas nas reuniões de gestão ajudou os coordenadores a identificar pendências e rendeu no 1º quadrimestre de 2023 um resultado 35% melhor que seus 4 municípios vizinhos."
+                    descricao="O uso das ferramentas nas reuniões de gestão ajudou os coordenadores a identificar pendências e gerou  um resultado 35% melhor que seus 4 municípios vizinhos no 1º quadrimestre de 2023."
                   />,
                   <CardIP
                     key={ uuidv1() }
                     titulo="PARCEIRO DESDE 2022"
                     indicador="Minaçu/GO"
-                    descricao="Com apoio da nossa equipe de especialistas em saúde, no 3º quadrimestre de 2022, o município obteve o primeiro lugar na Regional Norte de Goiás no ranking do Previne Brasil!"
+                    descricao="Com apoio da nossa equipe de especialistas em saúde, o município obteve o primeiro lugar na Regional Norte de Goiás no ranking do Previne Brasil no 3º quadrimestre de 2022!"
                   />
                 ] }
               />
@@ -214,8 +214,9 @@ const Index = ({res}) => {
                   botao={{label: 'CONHECER MAIS DA IMPULSOGOV',url: 'https://impulsogov.org/'}}
                   imagem={{posicao: null,url: ''}}
                   supertitulo=""
-                  titulo="<b>Impulsionamos o SUS com tecnologia</b>"
-                  texto="Na ImpulsoGov, acreditamos que tecnologia deve facilitar o dia a dia da profissional do SUS e permitir que ela foque naquilo que importa: o cuidado oferecido às pessoas.<br/><br/>O Impulso Previne é um de seus projetos e foca nos critérios do Previne Brasil, o atual modelo de financiamento da atenção primária à saúde no Brasil.<br/><br/>"
+                  titulo="<b>Impulsionamos o SUS com dados e tecnologia</b>"
+                  texto="Na ImpulsoGov, acreditamos que os dados são necessários para facilitar a tomada de decisões e a tecnologia deve facilitar o dia a dia da profissional do SUS e permitir que ela foque naquilo que importa: o cuidado oferecido às pessoas.<br/><br/>
+                  O Impulso Previne é um projeto da ImpulsoGov focado em promover o uso de dados e indicadores na gestão da atenção primária, com destaque para o Previne Brasil, atual modelo de financiamento federal da APS no Brasil <br/><br/>"
                 />
               ] }
             />
