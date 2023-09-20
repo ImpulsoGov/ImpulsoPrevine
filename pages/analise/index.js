56014d9f5c47aa5efd424551d033a59e93381586
NicoliSte <nickstefan4@gmail.com>
2023-09-19 21:16:25 -0300

    ajuste de responsividade na pre tela

--- pages/analise/index.js
+++ pages/analise/index.js
@@ -1,36 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { v1 as uuidv1 } from 'uuid';
-import { Margem, NovoTituloTexto, Grid12Col, CardIP, ButtonColor } from "@impulsogov/design-system"
-import { getData } from '../../services/cms'
-import { LAYOUT, HOME } from '../../utils/QUERYS'
-
-export async function getServerSideProps(ctx) {
-  const userIsActive = ctx.req.cookies['next-auth.session-token']
-  const userIsActiveSecure = ctx.req.cookies['__Secure-next-auth.session-token']
-  let redirect = !userIsActive && !userIsActiveSecure
-  if (!redirect) {
-    return {
-      redirect: {
-        destination: "/analise",
-        permanent: false, // make this true if you want the redirect to be cached by the search engines and clients forever
-      },
-    }
-  }
-  const res = [
-    await getData(LAYOUT),
-  ]
-  return {
-    props: {
-      res: res
-    }
-  }
-}
+import { Margem, NovoTituloTexto, CardIP, ButtonColor } from "@impulsogov/design-system"
 
 const Index = ({ res }) => {
   const router = useRouter();
   const [activeTabIndex, setActiveTabIndex] = useState(Number(router.query?.painel));
-  const [activeTitleTabIndex, setActiveTitleTabIndex] = useState(0);
+
   useEffect(() => {
     setActiveTabIndex(Number(router.query?.painel));
   }, [router.query?.painel]);
@@ -40,9 +16,31 @@ const Index = ({ res }) => {
       pathname: router.pathname,
       query: { painel: activeTabIndex }
     },
-      undefined, { shallow: true }
-    );
+    undefined,
+    { shallow: true });
   }, [activeTabIndex]);
+
+  const cardsData = [
+    {
+      title: " Indicadores de Desempenho",
+      description: "Compare resultados dos 7 indicadores entre um quadrimestre e outro.",
+      buttonText: "INDICADORES DE DESEMPENHO",
+      link: "/dadoPublicos"
+    },
+    {
+      title: "Captação Ponderada - Cadastros",
+      description: "Acompanhe a evolução nos cadastros de cada equipe do seu município.",
+      buttonText: "CAPTAÇÃO PONDERADA",
+      link: "/dadoPublicos?painel=1"
+    },
+    {
+      title: "Incentivos a Ações Estratégicas",
+      description: "Confira o histórico de repasses e as ações que se enquadram no seu perfil.",
+      buttonText: "AÇÕES ESTRATÉGICAS",
+      link: "/dadoPublicos?painel=2"
+    }
+  ];
+
   return (
     <div style={{ backgroundColor: "#E6ECF0" }}>
       <Margem
@@ -57,95 +55,72 @@ const Index = ({ res }) => {
                     texto="Confira gratuitamente uma nova maneira de acompanhar o desempenho de qualquer município no Previne Brasil com informações comparativas a partir dos dados públicos do SISAB.</br>"
                   />
                 </>
-
               }
-
-            />
-            <div style={{ paddingTop: 75 }}></div>
-          </>
-
-        }
-      />
-
-      <Margem
-        componente={
-          <>
-            <Grid12Col
-              proporcao="4-4-4"
-              items={[
-                <div key={uuidv1()} style={{ position: 'relative' }}>
-                  <CardIP
-                    titulo=""
-                    indicador={<span style={{ color: '#1F1F1F', fontSize: '29px' }}> <br></br>Indicadores de Desempenho </span>}
-                    descricao={
-                      <span style={{ fontSize: '18px' }}>
-                        Compare resultados dos 7 indicadores entre um quadrimestre e outro.
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                      </span>
-                    }
-                  />
-                  <div style={{display: 'flex', justifyContent: 'center', marginTop: '-110px', marginLeft: '-25px' }}>
-                    <ButtonColor
-                      label="INDICADORES DE DESEMPENHO"
-                      link="/dadoPublicos"
-                    />
-                  </div>
-                </div>,
-                 <div key={uuidv1()} style={{ position: 'relative' }}>
-                 <CardIP
-                   titulo=""
-                   indicador={<span style={{ color: '#1F1F1F', fontSize: '27.2px' }}><br></br> Capitação Ponderada - cadastros</span>}
-                   descricao={
-                     <span style={{ fontSize: '18px' }}>
-                       Acompanhe a evolução nos cadastros de cada equipe do seu município.
-                       <br></br>
-                       <br></br>
-                       <br></br>
-                       <br></br>
-                       <br></br>
-                     </span>
-                   }
-                 />
-                 <div style={{display: 'flex', justifyContent: 'center', marginTop: '-110px', marginLeft: '-75px' }}>
-                   <ButtonColor
-                     label="CAPITAÇÃO PONDERADA"
-                     link="/dadoPublicos?painel=1"
-                   />
-                 </div>
-               </div>,
-                <div key={uuidv1()} style={{ position: 'relative' }}>
-                <CardIP
-                  titulo=""
-                  indicador={<span style={{ color: '#1F1F1F', fontSize: '28px' }}><br></br> Incentivos a Ações Estratégicas</span>}
-                  descricao={
-                    <span style={{ fontSize: '18px' }}>
-                      Confira o histórico de repasses e as ações que se enquadram no seu perfil.
-                      <br></br>
-                      <br></br>
-                      <br></br>
-                      <br></br>
-                      <br></br>
-                    </span>
-                  }
-                />
-                <div style={{display: 'flex', justifyContent: 'center', marginTop: '-110px', marginLeft: '-85px' }}>
-                  <ButtonColor
-                    label="AÇÕES ESTRATÉGICAS"
-                    link="/dadoPublicos?painel=2"
-                  />
-                </div>
-              </div>
-
-              ]}
             />
             <div style={{ paddingTop: 75 }}></div>
           </>
         }
       />
+      
+      <div className="cardGrid">
+        {cardsData.map((card, index) => (
+          <div key={uuidv1()} className="cardContainer">
+            <CardIP
+              titulo=""
+              indicador={<span style={{ color: '#1F1F1F', fontSize: '32px' }}> <br></br>{card.title} </span>}
+              descricao={
+                <span style={{ fontSize: '18px' }}>
+                  {card.description}
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                </span>
+              }
+            />
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '-110px', marginLeft: '-25px' }}>
+              <ButtonColor
+                label={card.buttonText}
+                link={card.link}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      
+
+      <style jsx>{`
+        .cardGrid {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          padding: 40px;
+         
+        }
+
+        .cardContainer {
+          width: calc(33.33% - 20px);
+          position: relative;
+          margin-bottom: 20px;
+        }
+
+        @media (max-width: 1050px) {
+          .cardGrid {
+            flex-direction: column;
+            align-items: center;
+          }
+
+          .cardContainer {
+            width: 100%;
+            padding: 20px;
+          }
+        }
+      `}</style>
+      <div style={{ paddingBottom: '105px' }}></div>
     </div>
   )
 }
