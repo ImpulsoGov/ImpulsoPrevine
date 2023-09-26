6418baabfe6187e7272a6fbfef0ba4289ebcc870
gabriellearruda <gabriellearrudac@gmail.com>
2023-09-26 12:24:59 -0300

    tira o seletor de municipios da pagina de analise

--- pages/_app.js
+++ pages/_app.js
@@ -81,7 +81,6 @@ function MyApp(props) {
                   cor: (path == '/' || path == '/apoio' || path == '/analise') ? "Cinza" : "White",
                   logoLink: props.ses ? '/inicio' : '/'
                 } }
-                seletorMunicipios={ path == '/analise' }
                 showMenuMobile={ {
                   states: {
                     active: active,
