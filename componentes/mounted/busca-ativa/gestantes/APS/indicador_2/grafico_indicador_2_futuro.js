f7d02b5c5b295febb321f067578d3c9759f2ad8c
dlopes14 <donylopes@gmail.com>
2023-12-18 11:10:07 -0300

    corrige filtro cards futuro

--- componentes/mounted/busca-ativa/gestantes/APS/indicador_2/grafico_indicador_2_futuro.js
+++ componentes/mounted/busca-ativa/gestantes/APS/indicador_2/grafico_indicador_2_futuro.js
@@ -1,6 +1,6 @@
 import { GraficoBuscaAtiva, ScoreCardGrid, Spinner } from "@impulsogov/design-system";
 const CardsGraficoIndicadorDoisQuadriFuturo = ({tabelaDataAPS}) =>{
-    const dataQuadriFuturo = tabelaDataAPS.filter(item => item.gestacao_quadrimestre == '2024.Q1' || item.gestante_quadrimestre == '2024.Q2')
+    const dataQuadriFuturo = tabelaDataAPS.filter(item => item.gestacao_quadrimestre == '2024.Q1' || item.gestacao_quadrimestre == '2024.Q2')
     return dataQuadriFuturo ? 
     <>
         <h2 style={{
