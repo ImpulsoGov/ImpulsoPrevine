1a266425667ecdb52bd175204e00555faeb219ee
Taina Santana <tainnaps@gmail.com>
2024-01-13 00:55:42 -0300

    Add sentry wrapper na rota de exemplo da api

--- pages/api/sentry-example-api.js
+++ pages/api/sentry-example-api.js
@@ -1,5 +1,8 @@
+import { wrapApiHandlerWithSentry } from "@sentry/nextjs";
 // A faulty API route to test Sentry's error monitoring
-export default function handler(_req, res) {
+function handler(_req, res) {
   throw new Error("Sentry Example API Route Error");
   res.status(200).json({ name: "John Doe" });
 }
+
+export default wrapApiHandlerWithSentry(handler, "/sentry-example-api");
