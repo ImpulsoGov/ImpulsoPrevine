4aafbb426cf4ef7566040986352854b389c84399
Taina Santana <tainnaps@gmail.com>
2024-01-13 01:01:21 -0300

    Revert "Add sentry wrapper na rota de exemplo da api"
    This reverts commit 1a266425667ecdb52bd175204e00555faeb219ee.

--- pages/api/sentry-example-api.js
+++ pages/api/sentry-example-api.js
@@ -1,8 +1,5 @@
-import { wrapApiHandlerWithSentry } from "@sentry/nextjs";
 // A faulty API route to test Sentry's error monitoring
-function handler(_req, res) {
+export default function handler(_req, res) {
   throw new Error("Sentry Example API Route Error");
   res.status(200).json({ name: "John Doe" });
 }
-
-export default wrapApiHandlerWithSentry(handler, "/sentry-example-api");
