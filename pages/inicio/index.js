b24e78835d41f97df33c66133e288626268fce28
dlopes14 <donylopes@gmail.com>
2024-02-15 12:43:41 -0300

    add banner webnar area logada

--- pages/inicio/index.js
+++ pages/inicio/index.js
@@ -1,7 +1,7 @@
 import { getData, getDataCapacitacao } from '../../services/cms'
 import { LAYOUT, CONTEUDOS_TRILHAS } from '../../utils/QUERYS'
 import { useSession,signOut, getSession } from "next-auth/react"
-import { Greeting, CardTrilha, ButtonColorSubmit, CardLarge } from '@impulsogov/design-system'
+import { Greeting, CardTrilha, ButtonColorSubmit, CardLarge, ModalAlert, Alert_v2 } from '@impulsogov/design-system'
 import { progresso } from '../../helpers/modulosDataTransform'
 import { acessoTrilhasClient } from '../../services/acessoTrilha'
 import { useEffect, useState, useRef } from 'react'
@@ -125,6 +125,40 @@ const Index = ({ res }) => {
                         submit = {NPSAvaliacao}
                     />                    
                 }  */}
+                      <ModalAlert
+                        Child = {Alert_v2}
+                        childProps ={ {
+                            titulos : {
+                                Titulo : "Impulso Previne:",
+                                SubTitulo : "Uso das listas nominais na rotina das equipes"
+                            },
+                            Info : [
+                                {
+                                    icon : "https://media.graphassets.com/mK0XWA2qSyK3tSPER5SM",
+                                    info : "22 de feverreiro"
+                                },
+                                {
+                                    icon : "https://media.graphassets.com/Bsq3OasQQxWNFuN0Ldhs",
+                                    info : "14h"
+                                },
+                                {
+                                    icon : "https://media.graphassets.com/Ui2qHF9IR9WyqEQv8H1v",
+                                    info : "Certificado para os participantes"
+                                },
+                            ],
+                            cardProfissional : {
+                                profissional : "https://media.graphassets.com/CL8x2D45RxKZCGJZSXLa",
+                                logo : "https://media.graphassets.com/et6MBNobT9OA39JxsjNi",
+                                nome : "Isabela Santos",
+                                cargo : "Especialista em Saúde Coletiva"
+                            },
+                            botao : {
+                                label : "ACESSAR EVENTO",
+                                url : "https://bit.ly/encontro-listas-nominais"
+                            }
+                        }}
+                    />
+
                 <Greeting
                     cargo = {cargo}
                     greeting = "Bem-vindo(a)"
