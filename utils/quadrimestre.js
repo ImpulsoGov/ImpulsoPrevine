76ba5ed2d95ba297117dc3b5e4b80724563fbbcb
Taina Santana <tainnaps@gmail.com>
2024-02-15 14:34:08 -0300

    Add funções últeis para atualizar quadrimestre

---
+++ utils/quadrimestre.js
@@ -0,0 +1,45 @@
+const PRIMEIRO_QUADRIMESTRE_DO_ANO = 1;
+const ULTIMO_QUADRIMESTRE_DO_ANO = 3;
+
+export const obterDadosQuadrimestre = (data) => {
+  const objetoDeData = new Date(data);
+  const mes = objetoDeData.getUTCMonth() + 1; // ? +1 pra ficar de 1 a 12
+  const ano = String(objetoDeData.getUTCFullYear());
+  const quadrimestre = String(Math.ceil(mes / 4));
+  return {
+    quadrimestre,
+    ano,
+  };
+};
+
+export const obterDadosProximosQuadrimestres = (dataBase, quantidade) => {
+  let iteracao = quantidade;
+  const proximosQuadrimestres = [];
+
+  while (iteracao > 0) {
+    const dadosUltimoQuadrimestre = proximosQuadrimestres.length === 0
+      ? obterDadosQuadrimestre(dataBase)
+      : proximosQuadrimestres.at(-1);
+
+    const proximoQuadrimestre = Number(dadosUltimoQuadrimestre.quadrimestre) + 1;
+
+    const anoDoProximoQuadrimestre = String(
+      proximoQuadrimestre > ULTIMO_QUADRIMESTRE_DO_ANO
+        ? Number(dadosUltimoQuadrimestre.ano) + 1
+        : dadosUltimoQuadrimestre.ano
+    );
+
+    proximosQuadrimestres.push({
+      quadrimestre: String(
+        proximoQuadrimestre > ULTIMO_QUADRIMESTRE_DO_ANO
+          ? PRIMEIRO_QUADRIMESTRE_DO_ANO
+          : proximoQuadrimestre
+      ),
+      ano: anoDoProximoQuadrimestre,
+    });
+
+    iteracao -= 1;
+  }
+
+  return proximosQuadrimestres;
+};
