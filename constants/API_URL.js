325851fbb533313c6ec7d48ec542b098d980ad50
Taina Santana <tainnaps@gmail.com>
2024-02-20 18:39:13 -0300

    Retorna URL da api de usuários

--- constants/API_URL.js
+++ constants/API_URL.js
@@ -4,7 +4,6 @@ export const API_URL_DADOS_PUBLICOS = 'https://api-ip-publico.impulsogov.org/'
 
 export const API_LOCAL = 'http://127.0.0.1:8000/'
 
-export const API_URL_USUARIOS = 'https://api-ip-nomin-feat-munic-ns9er3.herokuapp.com/'
+export const API_URL_USUARIOS = 'https://api-usuarios.impulsogov.org/'
 
 export const API_URL_DADOS_NOMINAIS = 'https://api-ip-nominal.impulsogov.org/'
-// export const API_URL_DADOS_NOMINAIS = 'http://127.0.0.1:8000/'
