b9fb3dcd35e4d3300f5adddcc99f9b7485029f4a
dlopes14 <donylopes@gmail.com>
2024-04-26 14:32:06 -0300

    remove toastbar impressao

--- helpers/imprimir.js
+++ helpers/imprimir.js
@@ -6,8 +6,6 @@ export const Imprimir = (
     lista,
     aba,
     sub_aba="",
-    filtros_aplicados,
-    setShowSnackBar
 )=>{
     mixpanel.track('button_click', {
         'button_action': "imprimir_lista",
@@ -15,16 +13,6 @@ export const Imprimir = (
         'aba_lista_nominal' : aba,
         'sub_aba_lista_nominal' : sub_aba
       });
-    const toastBar = ()=>{
-    if(!filtros_aplicados){
-        setShowSnackBar(() => ({
-            open: true,
-            message : "O número de pacientes é muito grande, filtre e ordene a lista para facilitar o carregamento da impressão",
-            background: "#FFF0E1",
-            color: "black",
-        }))    
-    }}
-    const showImpressao = ()=>{
     if(typeof window !== 'undefined') {
         const largura = window.innerWidth;
         const altura = window.innerHeight;
@@ -50,7 +38,5 @@ export const Imprimir = (
         `);
         janelaImpressao.document.close();
         janelaImpressao.print();
-    }}
-    toastBar()
-    setTimeout(showImpressao, 5000)
+    }
 }
