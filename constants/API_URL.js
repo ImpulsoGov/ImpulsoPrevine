5ec31505ce89561920a238afcd4f31af8303b1cb
Danilo Lopes <donylopes@gmail.com>
2024-04-29 13:58:42 -0300

    Update API_URL.js

--- constants/API_URL.js
+++ constants/API_URL.js
@@ -1,9 +1,9 @@
-export const API_URL = 'https://api-usuarios-feat-prime-biejkp.herokuapp.com/'
+export const API_URL = 'https://impulsoapi.herokuapp.com/'
 
 export const API_URL_DADOS_PUBLICOS = 'https://api-ip-publico.impulsogov.org/'
 
 export const API_LOCAL = 'http://127.0.0.1:8000/'
 
-export const API_URL_USUARIOS = 'https://api-usuarios-feat-prime-biejkp.herokuapp.com/'
+export const API_URL_USUARIOS = 'https://api-usuarios.impulsogov.org/'
 
-export const API_URL_DADOS_NOMINAIS = 'https://api-usuarios-feat-prime-biejkp.herokuapp.com/'
+export const API_URL_DADOS_NOMINAIS = 'https://api-ip-nominal.impulsogov.org/'
