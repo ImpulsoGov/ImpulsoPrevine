56edca564a4ef72b9676d20497a697d0219be5ee
dlopes14 <donylopes@gmail.com>
2024-06-27 11:17:30 -0300

    add banner webnnar

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import {
   NovoTituloTexto,
   ImagensFull2,
   Margem,
+  ModalAlert,
+  Alert_v4
 } from "@impulsogov/design-system";
 
 
@@ -39,6 +41,50 @@ export async function getServerSideProps(ctx) {
 const Index = ({res}) => {
   return (
     <div style={{backgroundColor: "#E6ECF0"}}>
+      <ModalAlert
+          Child = {Alert_v4}
+          childProps ={ {
+            titulos : {
+                Titulo : "Qualificação do cuidado da população",
+                SubTitulo : "Capacitação sobre Monitoramento na APS:"
+            },
+            Info : [
+                {
+                    icon : "https://media.graphassets.com/mK0XWA2qSyK3tSPER5SM",
+                    info : "17/07"
+                },
+                {
+                    icon : "https://media.graphassets.com/Bsq3OasQQxWNFuN0Ldhs",
+                    info : "19 horas"
+                },
+                {
+                    icon : "https://media.graphassets.com/wb3wQPKRQY6o1Mb7pLzE",
+                    info : "Online"
+                },
+                {
+                    icon : "https://media.graphassets.com/Ui2qHF9IR9WyqEQv8H1v",
+                    info : "Gratuito"
+                },
+            ],
+            cardProfissional_1 : {
+                profissional : "https://media.graphassets.com/2sqDyLFbTJylgJKYorEy",
+                logo : "https://media.graphassets.com/et6MBNobT9OA39JxsjNi",
+                nome : "Isabela dos Santos",
+                cargo : "Especialista em Saúde Coletiva"
+            },
+            cardProfissional_2 : {
+              profissional : "https://media.graphassets.com/oqZ1IfJQT9yzzcjEWtL3",
+              logo : "https://media.graphassets.com/et6MBNobT9OA39JxsjNi",
+              nome : "Fernanda Soares",
+              cargo : "Especialista em Saúde Coletiva"
+          },
+
+            botao : {
+                label : "QUERO ME INSCREVER",
+                url : "https://bit.ly/webinar-monitoramento-APS-10"
+            }
+        }}
+      />
       <Margem 
         componente={
           <>
