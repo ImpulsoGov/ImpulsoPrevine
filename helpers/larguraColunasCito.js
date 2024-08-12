498fe9694853c49203c73548073d064de1435665
Tainá Santana <tainnaps@gmail.com>
2024-08-12 12:10:03 -0300

    Atualiza impressao citopatologico coaps

---
+++ helpers/larguraColunasCito.js
@@ -0,0 +1,21 @@
+export const larguraColunasCitoPaisagem = {
+  "paciente_nome": "250px",
+  "cidadao_cpf_dt_nascimento": "85px",
+  "id_status_usuario": "280px",
+  "vencimento_da_coleta": "50px",
+  "prazo_proxima_coleta": "100px",
+  "idade": "80px",
+  "equipe_nome": "130px",
+  "acs_nome": "240px",
+};
+
+export const larguraColunasCitoRetrato = {
+  "paciente_nome": "120px",
+  "cidadao_cpf_dt_nascimento": "80px",
+  "id_status_usuario": "235px",
+  "vencimento_da_coleta": "100px",
+  "prazo_proxima_coleta": "130px",
+  "idade": "60px",
+  "equipe_nome": "63px",
+  "acs_nome": "120px",
+};
