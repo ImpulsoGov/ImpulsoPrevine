83c6c2192776e765f9b8df61ca7888432f4fbd3d
Tainá Santana <tainnaps@gmail.com>
2024-08-26 15:05:18 -0300

    Ajusta largura colunas impressao diabetes

--- helpers/larguraColunasDiabetes.js
+++ helpers/larguraColunasDiabetes.js
@@ -23,23 +23,23 @@ export const larguraColunasDiabetesRetratoAPS = {
 };
 
 export const larguraColunasDiabetesPaisagemEquipe = {
-  "cidadao_nome": "183px",
-  "cpf_e_identificacao_condicao": "146px",
+  "cidadao_nome": "253px",
+  "cpf_e_identificacao_condicao": "149px",
   "cidadao_idade": "60px",
   "dt_consulta_mais_recente": "130px",
   "prazo_proxima_consulta": "120px",
   "dt_solicitacao_hemoglobina_glicada_mais_recente": "130px",
   "prazo_proxima_solicitacao_hemoglobina": "120px",
-  "acs_nome_cadastro": "183px",
+  "acs_nome_cadastro": "253px",
 };
 
 export const larguraColunasDiabetesRetratoEquipe = {
-  "cidadao_nome": "99px",
-  "cpf_e_identificacao_condicao": "120px",
+  "cidadao_nome": "130px",
+  "cpf_e_identificacao_condicao": "130px",
   "cidadao_idade": "40px",
   "dt_consulta_mais_recente": "114px",
   "prazo_proxima_consulta": "105px",
   "dt_solicitacao_hemoglobina_glicada_mais_recente": "114px",
   "prazo_proxima_solicitacao_hemoglobina": "105px",
-  "acs_nome_cadastro": "97px",
+  "acs_nome_cadastro": "130px",
 };
