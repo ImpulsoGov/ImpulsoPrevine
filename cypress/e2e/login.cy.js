64c103425e377926c936e7cb39dd52aa7b0e57ac
Tainá Santana <tainnaps@gmail.com>
2024-09-20 09:29:21 -0300

    Separa testes com e sem cypress testing library

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,29 +6,30 @@ const PASSWORD_LOGIN_TEST = Cypress.env('PASSWORD_LOGIN_TEST');
 describe('Login', () => {
   beforeEach(() => {
     cy.viewport(1280, 720);
+    cy.visit('http://localhost:3000');
   });
 
-  it('should login successfully', async () => {
-    cy.visit('http://localhost:3000');
+  it('should login successfully (not using cypress testing library queries)', () => {
+    const openModalButton = cy.contains('ACESSO RESTRITO');
+    openModalButton.click();
 
-    // const openModalButton = cy.contains('div', 'ACESSO RESTRITO');
-    // openModalButton.click();
+    cy.get('[data-testid="Modal"]').should('be.visible');
 
-    // cy.get('[data-testid="Modal"]').should('be.visible');
+    const showLoginButton = cy.get('button').contains('ENTRAR');
+    showLoginButton.click();
 
-    // const showLoginButton = cy.get('button').contains('ENTRAR');
-    // showLoginButton.click();
+    cy.get('[data-testid="Modal"]').should('be.visible');
 
-    // cy.get('[data-testid="Modal"]').should('be.visible');
+    cy.get('input[placeholder="CPF"]').type(CPF_LOGIN_TEST || '12345678909');
+    cy.get('input[placeholder="Senha"]').type(PASSWORD_LOGIN_TEST || '12345678');
 
-    // cy.get('input[placeholder="CPF"]').type(CPF_LOGIN_TEST || '12345678909');
-    // cy.get('input[placeholder="Senha"]').type(PASSWORD_LOGIN_TEST || '12345678');
+    const loginButton = cy.get('button').contains('ENTRAR');
+    loginButton.click();
 
-    // const loginButton = cy.get('button').contains('ENTRAR');
-    // loginButton.click();
+    cy.url({ timeout: 8000 }).should('eq', 'http://localhost:3000/inicio');
+  });
 
-    // cy.url({ timeout: 8000 }).should('eq', 'http://localhost:3000/inicio');
-
+  it('should login successfully (using cypress testing library queries)', () => {
     const openModalButton = cy.findByText(/acesso restrito/i);
     openModalButton.click();
 
