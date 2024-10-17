09c5d0e35dc4fc50435cdd3eb8715eea40e6c764
dlopes14 <donylopes@gmail.com>
2024-10-17 13:09:47 -0300

    altera requisicao gestantes equipe

--- src/services/busca_ativa/Gestantes.js
+++ src/services/busca_ativa/Gestantes.js
@@ -2,21 +2,24 @@ import axios from "axios";
 import { API_URL_DADOS_NOMINAIS } from "../../constants/API_URL";
 
 const tabelaGestantesEquipe = async(municipio_id_sus,equipe,token)=>{
-  try {
-    let config = {
-        method: 'get',
-        maxBodyLength: Infinity,
-        url: API_URL_DADOS_NOMINAIS + `impulsoprevine/busca-ativa/gestantes?municipio_id_sus=${municipio_id_sus}&equipe=${equipe}`,
-        headers: { 
-          'Authorization': 'Bearer ' + token
-        }
-      };
-      
-    const res = await axios.request(config)
-    return res
-    } catch (error) {
-        return error.response.data
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: API_URL_DADOS_NOMINAIS + `impulsoprevine/busca-ativa/gestantes?municipio_id_sus=${municipio_id_sus}&equipe=${equipe}`,
+    headers: { 
+      'Authorization': 'Bearer ' + token
     }
+  };
+    
+  const res = axios.request(config)
+  .then((response) => {
+    return response.data;
+  })
+  .catch((error) => {
+    return error.response.data;
+  });      
+
+  return res
 }
 const tabelaGestantesAPS = async(municipio_id_sus,token)=>{
     let config = {
