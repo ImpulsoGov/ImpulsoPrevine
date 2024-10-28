24c1dc89bf9f0a5a92aaceca1bc30c0ee4aa4a9a
Tainá Santana <tainnaps@gmail.com>
2024-10-28 18:07:20 -0300

    Ajusta espacamento lateral da pagina

--- src/app/inicio/Inicio.tsx
+++ src/app/inicio/Inicio.tsx
@@ -3,10 +3,8 @@ import React from 'react';
 import { useSession } from "next-auth/react";
 import { SituacaoPorIndicador } from '@/types/inicio';
 import { CardsGrid } from './CardsGrid';
-import {
-    Greeting,
-    Texto
-} from '@impulsogov/design-system';
+import { Greeting, Texto } from '@impulsogov/design-system';
+import style from './Inicio.module.css';
 
  interface InicioProps {
     cargo: string;
@@ -21,52 +19,49 @@ export const Inicio : React.FC<InicioProps> = ({
 
     if (session) {
         return (
-            <>
+            <div className={style.Container}>
                 <div style={{paddingBottom: "35px"}}>
                     <Greeting
                         cargo = {cargo}
                         greeting = "Boas vindas"
                         municipio_uf = {session?.user.municipio}
                         nome_usuario = {session?.user.nome}
+                        margin="45px 0"
                     />
                 </div>
 
-                <div style={{
-                    margin: window.screen.width >= 1024 ? "0 80px" : "0 15px 0 80px"
-                }}>
-                    <Texto
-                        texto="Confira a situação geral dos cidadãos do seu município"
-                        color="#1F1F1F"
-                        fontSize="24px"
-                        fontWeight="500"
-                        lineHeight="31.2px"
-                    />
+                <Texto
+                    texto="Confira a situação geral dos cidadãos do seu município"
+                    color="#1F1F1F"
+                    fontSize="24px"
+                    fontWeight="500"
+                    lineHeight="31.2px"
+                />
 
-                    <Texto
-                        texto="Veja abaixo as listas nominais das áreas de cuidado contempladas no programa de financiamento e o número de pacientes em atraso em cada uma delas."
-                        color="#1F1F1F"
-                        fontSize="16px"
-                        fontWeight="400"
-                        lineHeight="20.8px"
-                        margin="15px 0 0 0"
-                    />
+                <Texto
+                    texto="Veja abaixo as listas nominais das áreas de cuidado contempladas no programa de financiamento e o número de pacientes em atraso em cada uma delas."
+                    color="#1F1F1F"
+                    fontSize="16px"
+                    fontWeight="400"
+                    lineHeight="20.8px"
+                    margin="15px 0 0 0"
+                />
 
-                    <div style={{paddingTop: "20px"}}>
-                        <CardsGrid
-                            situacaoPorIndicador={situacaoPorIndicador}
-                        />
-                    </div>
-
-                    <Texto
-                        texto="*Na lista de vacinação, o número total mostrado no card equivale ao total de crianças que contabilizam nesse quadrimestre."
-                        color="#1F1F1F"
-                        fontSize="14px"
-                        fontWeight="400"
-                        lineHeight="18.2px"
-                        margin="0 0 60px 0"
+                <div style={{paddingTop: "20px"}}>
+                    <CardsGrid
+                        situacaoPorIndicador={situacaoPorIndicador}
                     />
                 </div>
-            </>
+
+                <Texto
+                    texto="*Na lista de vacinação, o número total mostrado no card equivale ao total de crianças que contabilizam nesse quadrimestre."
+                    color="#1F1F1F"
+                    fontSize="14px"
+                    fontWeight="400"
+                    lineHeight="18.2px"
+                    margin="0 0 60px 0"
+                />
+            </div>
         )
     }
 }
