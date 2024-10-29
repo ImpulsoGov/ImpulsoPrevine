51bfca42495172610e6b6d3a4c06644ae414d1cb
Tainá Santana <tainnaps@gmail.com>
2024-10-29 13:03:38 -0300

    Add outros pesos fonte Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,53 @@ import { Base } from "./Base";
 import { SessionWrapperLayout } from "./SessionWrapperLayout";
 
 const geistInter = localFont({
-  src: "./fonts/Inter-Regular.woff",
-  variable: "--font-geist-inter",
-  // weight: "400",
+  src: [
+    {
+      path: "./fonts/Inter-Thin.woff",
+      style: "normal",
+      weight: "100"
+    },
+    {
+      path: "./fonts/Inter-ExtraLight.woff",
+      style: "normal",
+      weight: "200"
+    },
+    {
+      path: "./fonts/Inter-Light.woff",
+      style: "normal",
+      weight: "300"
+    },
+    {
+      path: "./fonts/Inter-Regular.woff",
+      style: "normal",
+      weight: "400"
+    },
+    {
+      path: "./fonts/Inter-Medium.woff",
+      style: "normal",
+      weight: "500"
+    },
+    {
+      path: "./fonts/Inter-SemiBold.woff",
+      style: "normal",
+      weight: "600"
+    },
+    {
+      path: "./fonts/Inter-Bold.woff",
+      style: "normal",
+      weight: "700"
+    },
+    {
+      path: "./fonts/Inter-ExtraBold.woff",
+      style: "normal",
+      weight: "800"
+    },
+    {
+      path: "./fonts/Inter-Black.woff",
+      style: "normal",
+      weight: "900"
+    },
+  ]
 });
 
 export default function RootLayout({
@@ -16,7 +60,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${geistInter.variable}`}>
+      <body className={`${geistInter.className}`}>
         <SessionWrapperLayout>
           <Base>
             {children}
