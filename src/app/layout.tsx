fbf5af9f518a9383bf37cc1ccd230db322aba329
Tainá Santana <tainnaps@gmail.com>
2024-10-29 13:38:26 -0300

    Add import da fonte

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,58 +1,61 @@
 import "./globals.css";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import { Base } from "./Base";
 import { SessionWrapperLayout } from "./SessionWrapperLayout";
 
-const geistInter = localFont({
-  src: [
-    {
-      path: "./fonts/Inter-Thin.woff",
-      style: "normal",
-      weight: "100"
-    },
-    {
-      path: "./fonts/Inter-ExtraLight.woff",
-      style: "normal",
-      weight: "200"
-    },
-    {
-      path: "./fonts/Inter-Light.woff",
-      style: "normal",
-      weight: "300"
-    },
-    {
-      path: "./fonts/Inter-Regular.woff",
-      style: "normal",
-      weight: "400"
-    },
-    {
-      path: "./fonts/Inter-Medium.woff",
-      style: "normal",
-      weight: "500"
-    },
-    {
-      path: "./fonts/Inter-SemiBold.woff",
-      style: "normal",
-      weight: "600"
-    },
-    {
-      path: "./fonts/Inter-Bold.woff",
-      style: "normal",
-      weight: "700"
-    },
-    {
-      path: "./fonts/Inter-ExtraBold.woff",
-      style: "normal",
-      weight: "800"
-    },
-    {
-      path: "./fonts/Inter-Black.woff",
-      style: "normal",
-      weight: "900"
-    },
-  ],
-  variable: "--font-geist-inter",
-});
+const inter = Inter({ subsets: ['latin'] });
+
+// const geistInter = localFont({
+//   src: [
+//     {
+//       path: "./fonts/Inter-Thin.woff",
+//       style: "normal",
+//       weight: "100"
+//     },
+//     {
+//       path: "./fonts/Inter-ExtraLight.woff",
+//       style: "normal",
+//       weight: "200"
+//     },
+//     {
+//       path: "./fonts/Inter-Light.woff",
+//       style: "normal",
+//       weight: "300"
+//     },
+//     {
+//       path: "./fonts/Inter-Regular.woff",
+//       style: "normal",
+//       weight: "400"
+//     },
+//     {
+//       path: "./fonts/Inter-Medium.woff",
+//       style: "normal",
+//       weight: "500"
+//     },
+//     {
+//       path: "./fonts/Inter-SemiBold.woff",
+//       style: "normal",
+//       weight: "600"
+//     },
+//     {
+//       path: "./fonts/Inter-Bold.woff",
+//       style: "normal",
+//       weight: "700"
+//     },
+//     {
+//       path: "./fonts/Inter-ExtraBold.woff",
+//       style: "normal",
+//       weight: "800"
+//     },
+//     {
+//       path: "./fonts/Inter-Black.woff",
+//       style: "normal",
+//       weight: "900"
+//     },
+//   ],
+//   variable: "--font-geist-inter",
+// });
 
 export default function RootLayout({
   children,
@@ -61,7 +64,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${geistInter.className}`}>
+      <body className={inter.className}>
         <SessionWrapperLayout>
           <Base>
             {children}
