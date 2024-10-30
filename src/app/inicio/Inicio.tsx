f6fbd1b81fa4bd108389a908e881816584c41bc2
Tainá Santana <tainnaps@gmail.com>
2024-10-30 18:29:07 -0300

    Remove banners de impressao

--- src/app/inicio/Inicio.tsx
+++ src/app/inicio/Inicio.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Greeting, CardTrilha, CardLarge, AtualizacaoCadastral, ModalAlert } from '@impulsogov/design-system'
+import { Greeting, CardTrilha, CardLarge } from '@impulsogov/design-system'
 import React from 'react';
 import { useSession } from "next-auth/react"
 
@@ -29,33 +29,6 @@ export const Inicio : React.FC<InicioProps> = ({
     if (session) {
         return (
             <>
-                {(session.user.perfis.includes(5) || session.user.perfis.includes(8)) &&
-                    <ModalAlert
-                        Child = {AtualizacaoCadastral}
-                        childProps ={ {
-                            titulos : {
-                                Titulo : "NOVIDADE",
-                                SubTitulo : "Agora você pode imprimir as listas nominais divididas por equipe em um clique!"
-                            },
-                            Info : "<div style='color: #606E78;font-size: 17px;font-weight: 400;line-height: 21.78px;'><div>Clicando no botão de <span style='color: #2EB280; font-weight: 600;'>IMPRIMIR LISTA</span>, além de poder dividir as listas por equipes automaticamente você também pode:</div><ul style='margin-top: 0;padding-left: 30px;'><li>separar cada equipe em uma folha</li><li>ordenar por profissional responsável dentro de cada equipe</li></ul><div>Visualize e distribua as listas de forma simples, com apenas um clique.</div></div>",
-                            imagem : "https://media.graphassets.com/7bt3S4Q82t8wDRjxjaoQ",
-                        }}
-                    />
-                }
-
-                {session.user.perfis.includes(9) &&
-                    <ModalAlert
-                        Child = {AtualizacaoCadastral}
-                        childProps ={ {
-                            titulos : {
-                                Titulo : "NOVIDADE",
-                                SubTitulo : "Agora você pode imprimir as listas nominais divididas por profissional responsável em um clique!"
-                            },
-                            Info : "<div style='color: #606E78;font-size: 17px;font-weight: 400;line-height: 21.78px;'>Clicando no botão de <span style='color: #2EB280; font-weight: 600;'>IMPRIMIR LISTA</span>, além de poder dividir as listas por profissionais automaticamente, você também pode separar cada profissional em uma folha.<div style='margin-top: 17px;'>Visualize e distribua as listas de forma simples, com apenas um clique.</div></div>",
-                            imagem : "https://media.graphassets.com/fNIDuaBST1616xMFxx4X",
-                        }}
-                    />
-                }
                 <Greeting
                     cargo = {cargo}
                     greeting = "Bem-vindo(a)"
