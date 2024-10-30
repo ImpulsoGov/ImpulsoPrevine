abac06036c6e9c3d8242547beb6b3c792b44575f
dlopes14 <donylopes@gmail.com>
2024-10-30 18:47:39 -0300

    atualiza dependencias

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,55 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || "main";
+var config_default = defineConfig({
+  branch,
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public"
+    }
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          },
+          {
+            name: "draft",
+            label: "Draft",
+            type: "boolean",
+            required: true,
+            description: "If this is checked the post will not be published"
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
