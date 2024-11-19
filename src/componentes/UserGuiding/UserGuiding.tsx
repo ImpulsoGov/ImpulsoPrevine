bb3cc95d3cf53f681b75118e980775ed04e16853
Taina Santana <tainnaps@gmail.com>
2024-11-19 12:23:28 -0300

    fix(UserGuiding): add id to next script

--- src/componentes/UserGuiding/UserGuiding.tsx
+++ src/componentes/UserGuiding/UserGuiding.tsx
@@ -2,10 +2,12 @@ import Script from 'next/script';
 
 export function UserGuiding() {
   return (
-    <Script dangerouslySetInnerHTML={{
-      __html: `
-      (function(g,u,i,d,e,s){g[e]=g[e]||[];var f=u.getElementsByTagName(i)[0];var k=u.createElement(i);k.async=true;k.src='https://static.userguiding.com/media/user-guiding-'+s+'-embedded.js';f.parentNode.insertBefore(k,f);if(g[d])return;var ug=g[d]={q:[]};ug.c=function(n){return function(){ug.q.push([n,arguments])};};var m=['previewGuide','finishPreview','track','identify','hideChecklist','launchChecklist'];for(var j=0;j<m.length;j+=1){ug[m[j]]=ug.c(m[j]);}})(window,document,'script','userGuiding','userGuidingLayer',${process.env.USER_GUIDING_APP_ID});
-      `
+    <Script
+      id='UserGuiding'
+      dangerouslySetInnerHTML={{
+        __html: `
+        (function(g,u,i,d,e,s){g[e]=g[e]||[];var f=u.getElementsByTagName(i)[0];var k=u.createElement(i);k.async=true;k.src='https://static.userguiding.com/media/user-guiding-'+s+'-embedded.js';f.parentNode.insertBefore(k,f);if(g[d])return;var ug=g[d]={q:[]};ug.c=function(n){return function(){ug.q.push([n,arguments])};};var m=['previewGuide','finishPreview','track','identify','hideChecklist','launchChecklist'];for(var j=0;j<m.length;j+=1){ug[m[j]]=ug.c(m[j]);}})(window,document,'script','userGuiding','userGuidingLayer',${process.env.USER_GUIDING_APP_ID});
+        `
     }} />
   )
 }
