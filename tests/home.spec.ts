9a3153bcc34c9de7c5a6ebbc00ea66c73b184136
Taina Santana <tainnaps@gmail.com>
2024-12-04 16:13:31 -0300

    feat: config web server and base url

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Home', () => {
   test('deve exibir o modal de divulgação do webnar', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+    await page.goto('/');
 
     const modalTitle = page.getByText(/transição de gestão da saúde municipal/i);
     const subscribeButton = page.getByRole('button', { name: /garanta já sua inscrição/i });
