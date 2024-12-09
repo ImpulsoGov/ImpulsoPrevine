6bb9002923aadd03fefaf04e3140f0ff76320392
Taina Santana <tainnaps@gmail.com>
2024-12-09 09:48:41 -0300

    feat: enable web server configuration in Playwright with extended timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'yarn build && yarn start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'yarn build && yarn start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
 });
