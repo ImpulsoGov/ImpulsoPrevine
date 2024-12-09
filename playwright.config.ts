a02dbd6614d14f7051db41d9cc722f93adb1e092
Taina Santana <tainnaps@gmail.com>
2024-12-09 15:38:19 -0300

    fix: update Playwright configuration to change base URL and web server URL to port 3001

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3001',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,8 +72,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
-    url: 'http://localhost:3000',
+    command: 'yarn build && yarn start -- -p 3001 -H 0.0.0.0',
+    url: 'http://localhost:3001',
     reuseExistingServer: !process.env.CI,
     timeout: 10 * 60 * 1000,
   },
