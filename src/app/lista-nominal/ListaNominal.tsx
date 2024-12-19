20a2b3161629e1d03204ca86b2f2fad3a66c274e
Taina Santana <tainnaps@gmail.com>
2024-12-19 08:02:49 -0300

    refactor(ListaNominal): atualiza props do seletor de paineis

--- src/app/lista-nominal/ListaNominal.tsx
+++ src/app/lista-nominal/ListaNominal.tsx
@@ -2,117 +2,116 @@
 import { ListConteiner } from "./ListConteiner";
 import { PanelSelectorWithCards } from '@impulsogov/design-system';
 
-export type ListaNominalProps = {
-  data?: {
-    data?: Record<string, string | number | Date | Record<string, string | null>>[];
-    totalRows?: number;
-  };
-}
-
-export const ListaNominal = ({ 
-    data,
-    // totalRows,
-}: ListaNominalProps) => {
-    //esses dados vao vir da API do CMS, com exceção do data e children que é passado por props
-    const selectorProps = {
-        breadcrumb: [
+export const ListaNominal = () => {
+  //esses dados vao vir da API do CMS, com exceção do data e children que é passado por props
+  const props = {
+    breadcrumb: [
+      {
+        label: 'Inicio',
+        link: '/inicio'
+      },
+      {
+        label: 'Pré-natal (indicadores 1, 2 e 3)',
+        link: '/lista=pre-natal'
+      }
+    ],
+    cards: [
+      {
+        customStyles: {
+          backgroundColor: '#FFF',
+          width: '180px'
+        },
+        title: 'Card Title 1',
+        titlePosition: 'top',
+        value: '100'
+      },
+      {
+        customStyles: {
+          backgroundColor: '#FFF',
+          width: '180px'
+        },
+        title: 'Card Title 2',
+        titlePosition: 'top',
+        value: '100'
+      },
+      {
+        customStyles: {
+          backgroundColor: '#FFF',
+          width: '180px'
+        },
+        title: 'Card Title 3',
+        titlePosition: 'top',
+        value: '100'
+      }
+    ],
+    inicialContent: {
+      subTabID: 'ChartSubTabID1',
+      tabID: 'charts'
+    },
+    municipality: "São Paulo - SP",
+    tabs: {
+      charts: {
+        subTabs: [
           {
-            label: 'Inicio',
-            link: '/inicio'
+            child: <div key="1a" style={{padding:"250px",textAlign: "center", fontSize: "28px"}}>Gráficos em breve</div>,
+            icon: {
+              active: 'https://media.graphassets.com/dVxSjSUyROm9A2YkiEMj',
+              inactive: 'https://media.graphassets.com/jPDKYUhTXaEkGHFpjpfr'
+            },
+            subTabID: 'ChartSubTabID1',
+            text: 'Sub-Aba 1'
           },
           {
-            label: 'Pré-natal (indicadores 1, 2 e 3)',
-            link: '/lista=pre-natal'
+            child: <div key="1b" style={{padding:"250px",textAlign: "center", fontSize: "28px"}}>Gráficos em breve</div>,
+            icon: {
+              active: 'https://media.graphassets.com/Tx39n37HTGWapXUq8UBv',
+              inactive: 'https://media.graphassets.com/veVDjWw1ROmaXsuOY7LX'
+            },
+            subTabID: 'ChartSubTabID2',
+            text: 'Sub-Aba 2'
           }
         ],
-        municipio: 'São Paulo - SP',
-        titulo: 'Pré-natal (indicadores 1, 2 e 3)',
-        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id.',
-        cards : [
+        tabID: 'chart',
+        title: 'Gráficos'
+      },
+      lists: {
+        subTabs: [
           {
-            value: '100',
-            title: 'Card Title',
-            titlePosition: 'top',
-            customStyles: {
-              width: "180px",
-              backgroundColor: "#FFF",
-            }
+            // A prop list virá da URL com nome da lista acessada
+            child: <ListConteiner key={1} list="hipertensao" />,
+            icon: {
+              active: 'https://media.graphassets.com/dVxSjSUyROm9A2YkiEMj',
+              inactive: 'https://media.graphassets.com/jPDKYUhTXaEkGHFpjpfr'
+            },
+            subTabID: 'subTabID1',
+            text: 'text1'
           },
           {
-            value: '100',
-            title: 'Card Title',
-            titlePosition: 'top',
-            customStyles: {
-              width: "180px",
-              backgroundColor: "#FFF",
-            }
+            child: <ListConteiner key={2} list="hipertensao" />,
+            icon: {
+              active: 'https://media.graphassets.com/Tx39n37HTGWapXUq8UBv',
+              inactive: 'https://media.graphassets.com/veVDjWw1ROmaXsuOY7LX'
+            },
+            subTabID: 'subTabID2',
+            text: 'text2'
           },
           {
-            value: '100',
-            title: 'Card Title',
-            titlePosition: 'top',
-            customStyles: {
-              width: "180px",
-              backgroundColor: "#FFF",
-            }
-          },
+            child: <ListConteiner key={3} list="hipertensao" />,
+            icon: {
+              active: 'https://media.graphassets.com/EQSROnFQRm20pX1CVbT2',
+              inactive: 'https://media.graphassets.com/8sikQD27QLO3IkoxkQ9D'
+            },
+            subTabID: 'subTabID3',
+            text: 'text3'
+          }
         ],
-        tabs: {
-          titles: ['Gráficos', 'Listas Nominais'],
-          subTabs: [
-            [
-              { 
-                icon: {
-                  active: 'https://media.graphassets.com/dVxSjSUyROm9A2YkiEMj',
-                  inactive: 'https://media.graphassets.com/jPDKYUhTXaEkGHFpjpfr'
-                }, 
-                text: 'Sub-Aba 1' 
-              },
-              { 
-                icon: {
-                  active: 'https://media.graphassets.com/Tx39n37HTGWapXUq8UBv',
-                  inactive: 'https://media.graphassets.com/veVDjWw1ROmaXsuOY7LX'
-                }, 
-                text: 'Sub-Aba 2' 
-              },
-            ],
-            [
-              { 
-                icon: {
-                  active: 'https://media.graphassets.com/dVxSjSUyROm9A2YkiEMj',
-                  inactive: 'https://media.graphassets.com/jPDKYUhTXaEkGHFpjpfr'
-                }, 
-                text: 'text1' 
-              },
-              { 
-                icon: {
-                  active: 'https://media.graphassets.com/Tx39n37HTGWapXUq8UBv',
-                  inactive: 'https://media.graphassets.com/veVDjWw1ROmaXsuOY7LX'
-                }, 
-                text: 'text2' 
-              },
-              { 
-                icon: {
-                  active: 'https://media.graphassets.com/EQSROnFQRm20pX1CVbT2',
-                  inactive: 'https://media.graphassets.com/8sikQD27QLO3IkoxkQ9D'
-                }, 
-                text: 'text3' 
-              },
-            ],
-          ]
-        },
-        children: [
-          [
-            <div key="1a" style={{padding:"250px",textAlign: "center", fontSize: "28px"}}>Gráficos em breve</div>,
-            <div key="1b" style={{padding:"250px",textAlign: "center", fontSize: "28px"}}>Gráficos em breve</div>,
-          ],
-          [
-            <ListConteiner key={1} data={data} />,
-            <ListConteiner key={2} data={data} />,
-            <ListConteiner key={3} data={data} />,
-          ],
-    
-      ]
-    }
-    return <PanelSelectorWithCards {...selectorProps}/>
+        tabID: 'list',
+        title: 'Listas'
+      }
+    },
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id.",
+    title: "Pré-natal (indicadores 1, 2 e 3)"
+  }
+
+  return <PanelSelectorWithCards {...props}/>
 }
