d5437a48bc6696e49480584a9194285776930d83
Danilo Lopes <donylopes@gmail.com>
2024-12-20 10:35:33 -0300

    Feature/alog 219 gatilhos de novas requisicoes de filtros (#282)
    * feat(ListaNominal): add ListaNominal component and related services
    chore: update package.json for design system dependency
    
    * refactor(ListaNominal): update package dependencies and improve code structure
    
    * refactor(ListaNominal): remove unused session prop from ListaNominal component
    
    * refactor(ListaNominal): comment out unused import for getListData
    
    * refactor(ListaNominal): clean up unused imports and improve ListConteiner component structure
    
    * refactor(ListaNominal): replace duplicate divs with unique keys for better rendering
    
    * refactor(ListaNominal): add comment to clarify data source for component props
    
    * refactor(ListaNominal): update comment to include 'children' prop in data source explanation
    
    * refactor(ListaNominal): adicionar comentários sobre dados mockados e futura integração com API
    
    * feat(ListaNominal): adicionar ChartsContainer e funções utilitárias para filtragem de dados
    
    * refactor(ListaNominal): comentar a propriedade subTabID para melhor clareza no código
    
    * refactor(ListaNominal): corrigir nomes de tipos e componentes para consistência
    
    * refactor(ListaNominal): renomear propriedades e otimizar a construção de URLs com parâmetros de ordenação e filtragem
    
    * refactor(ListaNominal): renomear propriedade multiSelect para isMultiSelect e otimizar a função subTabChildrenSelector
    
    * refactor(FilterData): melhorar a tipagem e otimizar a função de filtragem de dados
    
    * refactor(ListaNominalPage): melhorar a tipagem da sessão e otimizar o acesso às propriedades do usuário
    
    * refactor(FilterData): otimizar a filtragem de dados ao melhorar a desestruturação nas funções de filtro
    
    * refactor(FilterData): melhorar a organização e legibilidade da função filterData
    
    * refactor(ListaNominalPage): renomear selectorProps para props para melhorar a clareza do código
    
    * refactor(ListaNominal): renomear selectorProps para props para melhorar a clareza do código
    
    * refactor(ListaNominal): renomear campos de FilterItem para fieldName e fieldValue para melhorar a clareza do código
    
    * refactor(ListaNominal): renomear campos de SortingItem para sortField e sortOrder para melhorar a clareza do código

--- src/app/lista-nominal/ListContainer.tsx
+++ src/app/lista-nominal/ListContainer.tsx
@@ -44,10 +44,6 @@ const filters = [
             { value: 'ACS 13', label: 'ACS 13' },
             { value: 'ACS 14', label: 'ACS 14' },
             { value: 'ACS 15', label: 'ACS 15' },
-<<<<<<< HEAD
-=======
-
->>>>>>> cd86e82 (fix: fix conflit)
         ],
         label: 'ACS Responsável',
         id : 'acs_nome_cadastro',
