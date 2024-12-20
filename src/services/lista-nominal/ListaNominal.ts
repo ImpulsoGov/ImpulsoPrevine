5134b371fe86ecc99ae98d9becfe6cd82ec94994
Danilo Lopes <donylopes@gmail.com>
2024-12-20 10:35:33 -0300

    Feature/alog 219 gatilhos de novas requisicoes de filtros (#282)
    * feat(ListaNominal): add ListaNominal component and related services
    chore: update package.json for design system dependency
    
    * refactor(ListaNominal): update package dependencies and improve code structure
    
    * refactor(ListaNominal): remove unused session prop from ListaNominal component
    
    * refactor(ListaNominal): comment out unused import for getListData
    
    * refactor(ListaNominal): clean up unused imports and improve ListConteiner component structure
    
    * refactor(ListaNominal): replace duplicate divs with unique keys for better rendering
    
    * refactor(ListaNominal): add comment to clarify data source for component props
    
    * refactor(ListaNominal): update comment to include 'children' prop in data source explanation
    
    * refactor(ListaNominal): adicionar comentários sobre dados mockados e futura integração com API
    
    * feat(ListaNominal): adicionar ChartsContainer e funções utilitárias para filtragem de dados
    
    * refactor(ListaNominal): comentar a propriedade subTabID para melhor clareza no código
    
    * refactor(ListaNominal): corrigir nomes de tipos e componentes para consistência
    
    * refactor(ListaNominal): renomear propriedades e otimizar a construção de URLs com parâmetros de ordenação e filtragem
    
    * refactor(ListaNominal): renomear propriedade multiSelect para isMultiSelect e otimizar a função subTabChildrenSelector
    
    * refactor(FilterData): melhorar a tipagem e otimizar a função de filtragem de dados
    
    * refactor(ListaNominalPage): melhorar a tipagem da sessão e otimizar o acesso às propriedades do usuário
    
    * refactor(FilterData): otimizar a filtragem de dados ao melhorar a desestruturação nas funções de filtro
    
    * refactor(FilterData): melhorar a organização e legibilidade da função filterData
    
    * refactor(ListaNominalPage): renomear selectorProps para props para melhorar a clareza do código
    
    * refactor(ListaNominal): renomear selectorProps para props para melhorar a clareza do código
    
    * refactor(ListaNominal): renomear campos de FilterItem para fieldName e fieldValue para melhorar a clareza do código
    
    * refactor(ListaNominal): renomear campos de SortingItem para sortField e sortOrder para melhorar a clareza do código

---
+++ src/services/lista-nominal/ListaNominal.ts
@@ -0,0 +1,93 @@
+import axios from "axios";
+import { baseURL } from "@/utils/baseURL";
+
+export interface SortingItem {
+    sortField: string;
+    sortOrder: 'asc' | 'desc';
+  }
+
+export interface FilterItem {
+    fieldName: string;
+    fieldValue: string[];
+}
+
+const buildSortingParams = (sorting: SortingItem[]): string => {
+    return sorting
+      .map(item => `sortBy[${item.sortField}]=${item.sortOrder}`)
+      .join('&');
+};
+const buildFilterParams = (filters: FilterItem[]): string => {
+    return filters
+      .map(filter => 
+        filter.fieldValue
+          .map(value => `filters[${filter.fieldName}]=${value}`)
+          .join('&')
+      )
+      .join('&');
+  };
+
+/**
+ * Adds sorting and filtering parameters to a URL
+ * @param baseUrl - Base URL to add parameters to
+ * @param params - Object containing sorting, filter parameters, listName, ine, and municipio_id_sus
+ * @returns URL with added query parameters
+ */
+export const buildUrlWithParams = (
+    baseUrl: string, 
+    params?: {
+      sorting?: SortingItem[],
+      filters?: FilterItem[],
+      listName: string,
+      ine?: string,
+      municipio_id_sus: string
+    }
+  ): string => {
+    let url = baseUrl;
+    const { sorting, filters, listName, ine, municipio_id_sus } = params || {};
+    if (listName) {
+      url += `/${listName}`;
+    }
+    if (municipio_id_sus) {
+      url += `/${municipio_id_sus}`;
+    }
+    if (ine) {
+      url += `/${ine}`;
+    }
+    if (sorting?.length) {
+      url += `?${buildSortingParams(sorting)}`;
+    }
+    if (filters?.length) {
+      const prefix = url.includes('?') ? '&' : '?';
+      url += `${prefix}${buildFilterParams(filters)}`;
+    }
+    return url;
+  };  
+
+export type getListDataProps = {
+    municipio_id_sus: string;
+    token: string;
+    listName: string;
+    sorting?: SortingItem[];
+    filters?: FilterItem[];
+    ine?: string;
+};
+
+export const getListData = async ({
+    municipio_id_sus,
+    token,
+    listName,
+    sorting,
+    filters,
+    ine
+}: getListDataProps) => {
+    const url = `${baseURL()}/lista-nominal`;
+    const urlWithParams = buildUrlWithParams(url, { sorting, filters, listName, ine, municipio_id_sus });
+    return axios.request({
+      method: 'get',
+      maxBodyLength: Number.POSITIVE_INFINITY,
+      url: urlWithParams,
+      headers: {
+          'Authorization': `Bearer ${token}`
+      }
+  });
+};
