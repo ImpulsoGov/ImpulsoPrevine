ef90eca4887e7a8917fcd7b272c08f05ea322583
dlopes14 <donylopes@gmail.com>
2025-01-06 18:38:30 -0300

    remove componente BannerWebnarMounted da página inicial

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -10,11 +10,9 @@ import {
     ImagensFull2,
     Margem,
   } from "@impulsogov/design-system";
-import { BannerWebnarMounted } from '@/componentes/mounted/banner/BannerWebnar';  
 export const Home = () => {
     return(
         <div style={{backgroundColor: "#E6ECF0"}}>
-        <BannerWebnarMounted />
         <Margem 
           componente={
             <>
