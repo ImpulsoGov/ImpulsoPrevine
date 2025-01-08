c9fa26155eb01a74decd763ec59a2f233c4e6a11
Taina Santana <tainnaps@gmail.com>
2025-01-08 16:52:25 -0300

    feat: adiciona endpoint GET para filtrar dados de cards por tipo, município, lista e ine

---
+++ src/app/api/card/[type]/[list]/[municipio_id_sus]/[ine]/route.ts
@@ -0,0 +1,34 @@
+import { CardType, getDataByType, validateCardType } from "@/app/api/card/utils/cardType";
+import { handleError } from "@/app/api/errorHandler";
+import { NextRequest } from "next/server";
+
+export type RequestParams = {
+  type: CardType;
+  list: string;
+  municipio_id_sus: string;
+  ine: string;
+}
+
+export async function GET(
+  _req: NextRequest,
+  { params }: { params: RequestParams }
+) {
+  try {
+    const { type, list, municipio_id_sus, ine } = params;
+
+    validateCardType(type);
+
+    const data = getDataByType(type);
+
+    // Será substituído por consulta no banco de dados.
+    const filteredData = data.filter(card =>
+      card.municipio_id_sus === municipio_id_sus
+      && card.lista === list
+      && card.equipe_ine === ine
+    );
+
+    return Response.json(filteredData, { status: 200 });
+  } catch (error) {
+    handleError(error as Error);
+  }
+}
