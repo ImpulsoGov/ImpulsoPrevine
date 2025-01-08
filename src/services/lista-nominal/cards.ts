6ec2624be4717167a775d053d11078e286091aab
Taina Santana <tainnaps@gmail.com>
2025-01-08 16:53:27 -0300

    feat: adiciona funções para construir URL e requisitar dados de cards

---
+++ src/services/lista-nominal/cards.ts
@@ -0,0 +1,77 @@
+import { CardType } from "@/app/api/card/utils/cardType";
+import axios from "axios";
+import type { AxiosResponse } from "axios";
+
+/**
+ * Builds URL with given parameters
+ * @param baseUrl - Base URL to add parameters to
+ * @param params - Object containing listName, ine, cardType and municipio_id_sus
+ * @returns URL with added query parameters
+ */
+export const buildUrlWithParams = (
+  baseUrl: string,
+  params?: {
+    listName: string,
+    ine?: string,
+    municipio_id_sus: string;
+    cardType: CardType;
+  }
+): string => {
+  let url = baseUrl;
+  const { listName, ine, municipio_id_sus, cardType } = params || {};
+  if (cardType) {
+    url += `/${cardType}`;
+  }
+  if (listName) {
+    url += `/${listName}`;
+  }
+  if (municipio_id_sus) {
+    url += `/${municipio_id_sus}`;
+  }
+  if (ine) {
+    url += `/${ine}`;
+  }
+  return url;
+};
+
+export type getCardsDataProps = {
+  municipio_id_sus: string;
+  token: string;
+  listName: string;
+  cardType: CardType;
+  ine?: string;
+};
+
+export type CardsDataResponse = {
+  municipio_id_sus: string;
+  lista: string;
+  descricao: string;
+  valor: number;
+  equipe_ine?: string;
+}
+
+export const getCardsData = async ({
+  municipio_id_sus,
+  token,
+  listName,
+  cardType,
+  ine,
+}: getCardsDataProps): Promise<AxiosResponse<CardsDataResponse[]>> => {
+  if (!token) throw new Error('Token de autenticação é obrigatório');
+  if (!municipio_id_sus) throw new Error('ID do município é obrigatório');
+  if (!listName) throw new Error('Tipo de lista é obrigatório');
+  if (!cardType) throw new Error('Tipo de card é obrigatório');
+
+  const currentURL = new URL(window.location.href);
+  const url = `${currentURL.origin}/api/lista-nominal`;
+  const urlWithParams = buildUrlWithParams(url, { listName, ine, municipio_id_sus, cardType });
+
+  return axios.request({
+    method: 'get',
+    maxBodyLength: Number.POSITIVE_INFINITY,
+    url: urlWithParams,
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
+};
