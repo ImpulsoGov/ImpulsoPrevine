2cf56ab6740215c3bcc0758e80bbf3bc0bb19b85
Tainá Santana <tainnaps@gmail.com>
2025-01-09 11:57:21 -0300

    feat(getCardsProps): adiciona verificação para descrição de card não encontrada

--- src/helpers/cardsList.ts
+++ src/helpers/cardsList.ts
@@ -4,15 +4,27 @@ import { CardProps } from '@impulsogov/design-system/dist/molecules/Card/Card';
 export type CardDetails = Omit<CardProps, 'value'>;
 export type CardDetailsMap = Record<string, CardDetails>;
 
+/**
+ * Merge different cards data sets into the one needed for the Card component
+ * @param details personalizable details for each card usually coming from the CMS
+ * @param data cards data usually coming from the API response
+ * @returns an array of cards data with the props needed for the Card component
+ * @throws if a card description is not found in the details object
+ */
 export function getCardsProps(
   details: CardDetailsMap,
   data: CardsDataResponse[]
 ) {
   return data.map<CardProps>((card) => {
     const cardDetails = details[card.descricao];
+
+    if (!cardDetails) {
+      throw new Error(`Detalhes do card ${card.descricao} não encontrados na resposta do CMS`);
+    }
+
     return {
       ...cardDetails,
-      value: card.valor.toString(),
+      value: card.valor?.toString(),
     };
   });
 }
