8ccdcd4730c79016fa9f57d57321ee299c3c6324
dlopes14 <donylopes@gmail.com>
2025-01-13 19:16:33 -0300

    fix: adiciona tratamento de erro para carregamento de dados no componente BlogPost

--- src/app/blog/artigos/[post]/page.tsx
+++ src/app/blog/artigos/[post]/page.tsx
@@ -24,20 +24,21 @@ export async function generateStaticParams() {
 }
 
 const BlogPost = async ({ params }: Params) => {
-  const res = await getData(POST(params.post)) as { blogArtigo: BlogPost };
-  
-  return (
+  const res = await getData(POST(params?.post)) as { blogArtigo: BlogPost };
+
+  if(res?.blogArtigo) return (
     <BlogContent
-      titulo={res.blogArtigo?.titulo}
-      texto={res.blogArtigo?.texto.html}
-      capa={res.blogArtigo?.capa?.url}
+      titulo={res?.blogArtigo?.titulo}
+      texto={res?.blogArtigo?.texto.html}
+      capa={res?.blogArtigo?.capa?.url}
       autor={{
-        avatar: res.blogArtigo?.avatar?.url,
-        nome: res.blogArtigo?.autor,
-        data: res.blogArtigo?.createdAt,
+        avatar: res?.blogArtigo?.avatar?.url,
+        nome: res?.blogArtigo?.autor,
+        data: res?.blogArtigo?.createdAt,
       }}
     />
   );
+  return <p style={{padding: "60px", width: "100%", textAlign: "center"}}>Erro no carregamento de dados</p>
 };
 
 export default BlogPost;
