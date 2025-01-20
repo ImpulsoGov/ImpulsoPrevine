17dad81da7c50ebf3cc2f603037d390352805530
João Vittor Souza <jvns2010@gmail.com>
2025-01-20 17:39:00 -0300

    fix: nome da env var e remove console.log

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -26,7 +26,3 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
-
-console.log("SENTRY_REPLAY_SAMPLE_RATE:", process.env.SENTRY_REPLAY_SAMPLE_RATE);
-console.log("NEXT_PUBLIC_SENTRY_REPLAY_SAMPLE_RATE:", process.env.NEXT_PUBLIC_SENTRY_REPLAY_SAMPLE_RATE);
-console.log("VERCEL_ENV:", process.env.VERCEL_ENV);
