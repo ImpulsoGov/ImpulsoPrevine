6a6d409fd620522daca7714dfd6990ac36c769fc
dlopes14 <donylopes@gmail.com>
2025-01-21 12:23:20 -0300

    refator: configuração do Next.js e adiciona suporte a carregamento dinâmico

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 import { withSentryConfig } from '@sentry/nextjs';
-import bundleAnalyzer from '@next/bundle-analyzer';
 /** @type {import('next').NextConfig} */
-
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
+const isDev = process.env.NODE_ENV === 'dev';
 
 const nextConfig = {
     redirects: async () => {
@@ -29,55 +25,39 @@ const nextConfig = {
     experimental: {
       turbo: {
         resolveExtensions: [
-          '.mdx',
           '.tsx',
           '.ts',
           '.jsx',
           '.js',
           '.mjs',
-          '.json',
         ],
+        cache:{
+          enabled: true,
+          path: './.turbo-cache',
+        }
       }
     },
-    swcMinify: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
 };
 
-export default withBundleAnalyzer(withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
-
-org: "impulsogov",
-project: "impulso-previne",
-
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
-
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
-
-// Automatically annotate React components to show their full name in breadcrumbs and session replay
-reactComponentAnnotation: {
-enabled: true,
-},
-
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
-
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-}));
+export default 
+  isDev ?
+  nextConfig :
+  withSentryConfig(nextConfig, {
+    org: "impulsogov",
+    project: "impulso-previne",
+    silent: !process.env.CI,
+    widenClientFileUpload: true,
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+    tunnelRoute: "/monitoring",
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
+});
