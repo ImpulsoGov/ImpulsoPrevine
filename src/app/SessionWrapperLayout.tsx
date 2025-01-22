8826a236a559056ab0021172dd19d9ecb813c142
João Vittor Souza <jvns2010@gmail.com>
2025-01-22 11:09:12 -0300

    add SentryUserSetter

--- src/app/SessionWrapperLayout.tsx
+++ src/app/SessionWrapperLayout.tsx
@@ -1,11 +1,28 @@
 'use client'
-import { SessionProvider } from "next-auth/react";
-import React from "react";
+import { SessionProvider, useSession } from "next-auth/react";
+import * as Sentry from "@sentry/nextjs";
+import React, { useEffect } from "react";
 interface SessionWrapperLayoutProps {
     children : React.ReactNode
 }
-export const SessionWrapperLayout : React.FC<SessionWrapperLayoutProps>= ({ children }) => {
-    return <SessionProvider refetchInterval={ 60 * 60 } refetchOnWindowFocus={ true }>
-        { children }
-    </SessionProvider>
-}
+const SentryUserSetter = () => {
+    const { data: session } = useSession();
+    useEffect(() => {
+      if (session?.user) {
+        Sentry.setUser({
+          id: session.user.id, 
+          username: session.user.nome,
+        });
+      }
+    }, [session]);
+  
+    return null; // This component doesn't render anything, it just updates Sentry
+  };
+  export const SessionWrapperLayout: React.FC<SessionWrapperLayoutProps> = ({ children }) => {
+    return (
+      <SessionProvider refetchInterval={60 * 60} refetchOnWindowFocus={true}>
+        <SentryUserSetter /> {/* This runs the user tracking logic */}
+        {children}
+      </SessionProvider>
+    );
+  };
