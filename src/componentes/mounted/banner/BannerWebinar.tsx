617be894a69a0dd80ef88ef161d5e2d4d9068c89
Danilo Lopes <donylopes@gmail.com>
2025-01-24 12:52:40 -0300

    feature: adicionar banner de webinar na página inicial com informaçõe… (#305)
    * fix: resolve conflito
    
    * fix: corrigir nome do componente BannerWebinarMonted
    
    * fix(bannerwebinar): renomeia arquivo WebinarMounted
    
    * fix(bannerwebinar): renomeia variavel displayBannerWebinar

--- src/componentes/mounted/banner/BannerWebinar.tsx
+++ src/componentes/mounted/banner/BannerWebinar.tsx
@@ -1,24 +1,26 @@
-import { ModalAlertDisplay,Alert_v4 } from '@impulsogov/design-system'
-import { useState, useEffect } from 'react'
-export const BannerWebnarMounted = () => {
-    const [display, setDisplay] = useState(true)
-    const [DisplayBannerWebinar, setDisplayBannerWebinar] = useState(true)
+import { ModalAlertDisplay, Alert_v4 } from '@impulsogov/design-system';
+import { useState, useEffect } from 'react';
+
+export const BannerWebinarMounted = () => {
+    const [display, setDisplay] = useState(true);
+    const [displayBannerWebinar, setDisplayBannerWebinar] = useState(true);
     useEffect(() => {
         if(!display) localStorage.setItem("DisplayBannerWebinar", "false")
         setDisplayBannerWebinar(!!localStorage.getItem("DisplayBannerWebinar"))
-    },[display])
-    return !DisplayBannerWebinar && <ModalAlertDisplay
+    },[display]);
+    
+    return !displayBannerWebinar && <ModalAlertDisplay
         displayStates = {{display, setDisplay}}
         Child = {Alert_v4}
         childProps = {{
             titulos : {
-                Titulo : "Transição de gestão da Saúde Municipal",
-                SubTitulo : "O papel dos profissionais e gestores da APS:"
+                Titulo : "Gestão em Saúde",
+                SubTitulo : "Desafios e Oportunidades:"
             },
             Info : [
                 {
                     icon : "https://media.graphassets.com/mK0XWA2qSyK3tSPER5SM",
-                    info : "11/12"
+                    info : "12/02"
                 },
                 {
                     icon : "https://media.graphassets.com/Bsq3OasQQxWNFuN0Ldhs",
@@ -47,7 +49,7 @@ export const BannerWebnarMounted = () => {
             },
             botao : {
                 label : "GARANTA JÁ SUA INSCRIÇÃO",
-                url : "https://bit.ly/webinar-transicao-gestao4"
+                url : "https://bit.ly/webinar-gestao-em-saude3"
             }
         }}
     />
