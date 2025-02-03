56b7d3f6e0028e8f420648dd3c653e118e29e2f6
Joao Vittor Souza <jvns2010@gmail.com>
2025-02-03 17:29:14 -0300

    chore: chore: Adiciona browser agent do new relic em layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,35 @@
 import "./globals.css";
 import { Base } from "./Base";
 import { SessionWrapperLayout } from "./SessionWrapperLayout";
+import Script from "next/script";
+import newrelic from "newrelic";
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	if(process.env.NEW_RELIC_LICENSE_KEY !== undefined) {
+	// Ensure the New Relic agent is connected before injecting the script
+	if (newrelic.agent.collector.isConnected() === false) {
+		await new Promise((resolve) => {
+			newrelic.agent.on("connected", resolve);
+		});
+	}
+}
+	// Get New Relic browser monitoring script
+	const browserTimingHeader = newrelic.getBrowserTimingHeader({
+		hasToRemoveScriptWrapper: true,
+		allowTransactionlessInjection: true,
+	});
 	return (
 		<html lang="pt-BR">
+			<head>
+				<Script
+					id="nr-browser-agent"
+					dangerouslySetInnerHTML={{ __html: browserTimingHeader }}
+				/>
+			</head>
 			<body>
 				<SessionWrapperLayout>
 					<Base>{children}</Base>
@@ -16,4 +37,5 @@ export default function RootLayout({
 			</body>
 		</html>
 	);
+
 }
