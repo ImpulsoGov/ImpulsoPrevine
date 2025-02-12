2eb5554c43231654b6370066e1528a0fc5ffac65
Danilo Lopes <donylopes@gmail.com>
2025-02-12 10:42:04 -0300

    Feature/alog 276 refatoracao tooltip de informacoes do indicador nao (#326)
    * feat: adiciona tooltip no titulo da pagina de lista nominal
    
    * refactor: atualiza design-system
    
    * refactor: atualiza design system
    
    * refactor: atualiza design system

--- src/app/lista-nominal/page.tsx
+++ src/app/lista-nominal/page.tsx
@@ -79,6 +79,7 @@ const ListaNominalPage = async() => {
         ],
         municipality: 'São Paulo - SP',
         title: 'Pré-natal (indicadores 1, 2 e 3)',
+        tooltip: 'Tooltip, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id.',
         cards : externalCardsProps,
         listaNominalID: params.listName,
