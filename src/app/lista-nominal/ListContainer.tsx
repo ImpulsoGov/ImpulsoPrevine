c7b7a48e9c4f7c0762b15068bc8e481db23165aa
Danilo Lopes <donylopes@gmail.com>
2025-02-12 07:27:51 -0300

    Update src/app/lista-nominal/ListContainer.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/app/lista-nominal/ListContainer.tsx
+++ src/app/lista-nominal/ListContainer.tsx
@@ -270,7 +270,6 @@ export const ListContainer = ({
                 setIsLoading(false);
             };
             getListDataResponse();
-            console.log(search)
         }
     }, [user, value, list, pagination, sorting, search]);
 
