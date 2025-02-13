7b82a69874185fe21ed31cf26dfdc8987f518071
Joao Vittor Souza <jvns2010@gmail.com>
2025-02-13 18:46:39 -0300

    refactor: corrige documentacao e adiciona URL como env var

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,19 +1,21 @@
 import { test, expect } from "@chromatic-com/playwright";
 
-test.describe('NavBarMounted Component', () => {
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
+
+test.describe('Componente NavBarMounted ', () => {
   test.beforeEach(async ({ page }) => {
-    // Navigate to the home page before each test
-    await page.goto('http://localhost:3000');
+    // Navega de volta para a página inicial antes de cada teste
+    await page.goto(BASE_URL);
   });
 
-  test('should redirect to "/quem-somos" when clicking on "Quem somos"', async ({ page }) => {
+  test('Deve redirecionar para a página "/quem-somos" quando clicar em "Quem somos"', async ({ page }) => {
     const quemSomosLink = page.getByRole('link', { name: 'Quem Somos' }).first();
     await quemSomosLink.click();
     await page.waitForURL('http://localhost:3000/quem-somos');
     expect(page.url()).toBe('http://localhost:3000/quem-somos')
   });
 
-  test('should redirect to "/apoio" when clicking on "Apoio aos Municípios"', async ({ page }) => {
+  test('Deve redirecionar para página "/apoio" quando clicar em "Apoio aos Municípios"', async ({ page }) => {
     const apoioLink = page.getByRole('link', { name: 'Apoio aos Municípios' }).first();
     await apoioLink.click();
     await page.waitForURL('http://localhost:3000/apoio');
@@ -21,23 +23,23 @@ test.describe('NavBarMounted Component', () => {
   });
 
   
-  test('should redirect to "/analise" when clicking on "Dados do SISAB"', async ({ page }) => {
+  test('Deve redirecionar para a página "/analise" quando clicar em "Dados do SISAB"', async ({ page }) => {
     const analiseLink = page.getByRole('link', { name: 'Dados do SISAB' }).first();
     await analiseLink.click();
     await page.waitForURL('http://localhost:3000/analise');
     expect(page.url()).toBe('http://localhost:3000/analise');
   });
 
-  test('should redirect to "/faq" when clicking on "FAQ"', async ({ page }) => {
-    const analiseLink = page.getByRole('link', { name: 'FAQ' }).first();
-    await analiseLink.click();
+  test('Deve redirecionar para a página "/faq" quando clicar em "FAQ"', async ({ page }) => {
+    const faqLink = page.getByRole('link', { name: 'FAQ' }).first();
+    await faqLink.click();
     await page.waitForURL('http://localhost:3000/faq');
     expect(page.url()).toBe('http://localhost:3000/faq');
   });
 
-  test('should redirect to "/blog" when clicking on "Blog"', async ({ page }) => {
-    const analiseLink = page.getByRole('link', { name: 'Blog' }).first();
-    await analiseLink.click();
+  test('Deve redirecionar para a página "/blog" quando clicar em "Blog"', async ({ page }) => {
+    const blogLink = page.getByRole('link', { name: 'Blog' }).first();
+    await blogLink.click();
     await page.waitForURL('http://localhost:3000/blog');
     expect(page.url()).toBe('http://localhost:3000/blog');
   });
