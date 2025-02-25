a91bb05a8993f199f84ec416c4158b63348a376a
Joao Vittor Souza <jvns2010@gmail.com>
2025-02-25 08:18:25 -0300

    refactor: paraleliza e melhora a estrutura dos testes iniciais

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,44 +2,26 @@ import { test, expect } from "@playwright/test";
 
 const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
 
-test.describe('Componente NavBarMounted', () => {
+test.describe.parallel('Componente NavBarMounted', () => {
+  const routes = [
+    { name: 'Quem Somos', path: '/quem-somos' },
+    { name: 'Apoio aos Municípios', path: '/apoio' },
+    { name: 'Dados do SISAB', path: '/analise' },
+    { name: 'FAQ', path: '/faq' },
+    { name: 'Blog', path: '/blog' }
+  ];
+
   test.beforeEach(async ({ page }) => {
     // Navega de volta para a página inicial antes de cada teste
     await page.goto(BASE_URL);
   });
 
-  test('Deve redirecionar para a página "/quem-somos" quando clicar em "Quem somos"', async ({ page }) => {
-    const quemSomosLink = page.getByRole('link', { name: 'Quem Somos' }).first();
-    await quemSomosLink.click();
-    await page.waitForURL(`${BASE_URL}/quem-somos`);
-    expect(page.url()).toBe(`${BASE_URL}/quem-somos`);
-  });
-
-  test('Deve redirecionar para página "/apoio" quando clicar em "Apoio aos Municípios"', async ({ page }) => {
-    const apoioLink = page.getByRole('link', { name: 'Apoio aos Municípios' }).first();
-    await apoioLink.click();
-    await page.waitForURL(`${BASE_URL}/apoio`);
-    expect(page.url()).toBe(`${BASE_URL}/apoio`);
-  });
-
-  test('Deve redirecionar para a página "/analise" quando clicar em "Dados do SISAB"', async ({ page }) => {
-    const analiseLink = page.getByRole('link', { name: 'Dados do SISAB' }).first();
-    await analiseLink.click();
-    await page.waitForURL(`${BASE_URL}/analise`);
-    expect(page.url()).toBe(`${BASE_URL}/analise`);
-  });
-
-  test('Deve redirecionar para a página "/faq" quando clicar em "FAQ"', async ({ page }) => {
-    const faqLink = page.getByRole('link', { name: 'FAQ' }).first();
-    await faqLink.click();
-    await page.waitForURL(`${BASE_URL}/faq`);
-    expect(page.url()).toBe(`${BASE_URL}/faq`);
-  });
-
-  test('Deve redirecionar para a página "/blog" quando clicar em "Blog"', async ({ page }) => {
-    const blogLink = page.getByRole('link', { name: 'Blog' }).first();
-    await blogLink.click();
-    await page.waitForURL(`${BASE_URL}/blog`);
-    expect(page.url()).toBe(`${BASE_URL}/blog`);
-  });
+  for (const { name, path } of routes) {
+    test(`Deve redirecionar para a página "${path}" quando clicar em "${name}"`, async ({ page }) => {
+      const link = page.getByRole('link', { name, exact: true });
+      await link.click();
+      await page.waitForURL(`${BASE_URL}${path}`);
+      expect(page.url()).toBe(`${BASE_URL}${path}`);
+    });
+  }
 });
