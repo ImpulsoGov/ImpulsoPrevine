2f63a605ed7f5ebaf9b36ed884d043b91a526155
dlopes14 <donylopes@gmail.com>
2025-02-28 09:43:14 -0300

    feat: adiciona redirect para rotas publicas

--- src/middlewares/middlewarePages.ts
+++ src/middlewares/middlewarePages.ts
@@ -27,11 +27,16 @@ const ExibirURL = [
     "/conteudo-programatico",
     "/dadosPublicos",
 ];
-
+const secret = process.env.NEXTAUTH_SECRET;
 export const middlewarePages = async (request: NextRequest) => {
     const url = request.nextUrl;
     const headers = new Headers(request.headers);
-    let response = NextResponse.next();
+	const token = (await getToken({ req: request, secret })) as {
+		exp: number;
+	} | null;
+	let response = NextResponse.next();
+	if (token && rotasPublicas.includes(url.pathname))
+		return NextResponse.redirect(new URL("/inicio", request.url));
     if (ExibirURL.includes(url.pathname)) {
         headers.set("x-current-url", url.href);
         response = NextResponse.next({ headers });
