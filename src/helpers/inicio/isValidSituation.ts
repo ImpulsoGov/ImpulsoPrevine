8e43ce501859061febcd4af9228ec2bf99f56768
João Vittor Souza <jvns2010@gmail.com>
2025-03-21 11:57:33 -0300

    refactor: move isValidSituation para o modulo de helpers

---
+++ src/helpers/inicio/isValidSituation.ts
@@ -0,0 +1,13 @@
+import type { SituacaoPorIndicador } from '../../types/inicio';
+import type { Indicadores } from '../../types/inicio';
+
+// null | value 
+export const isValidSituation = (
+	situacaoPorIndicador: SituacaoPorIndicador,
+	indicador: Indicadores,
+): boolean => {
+	return !!(
+		situacaoPorIndicador[indicador]?.total !== null &&
+		situacaoPorIndicador[indicador]?.pendente !== null
+	);
+};
