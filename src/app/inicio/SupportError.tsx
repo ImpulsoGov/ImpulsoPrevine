183c4243607995914ce099a7e6501aa631599d49
danilo <donylopes@gmail.com>
2025-03-21 16:42:10 -0300

    refactor: altera link suporte whatapp

--- src/app/inicio/SupportError.tsx
+++ src/app/inicio/SupportError.tsx
@@ -25,7 +25,7 @@ export const SupportError = () => {
                     posicao: "right"
                 }}
                 label="Falar com o suporte"
-                link="https://api.whatsapp.com/send?phone=5511941350260&text=Ol%C3%A1,%20atendente!%0AAs%20informa%C3%A7%C3%B5es%20de%20um%20indicador%20mostradas%20na%20tela%20inicial%20n%C3%A3o%20est%C3%A3o%20carregando.%20Me%20ajuda%20a%20resolver%20esse%20problema?"
+                link="https://api.whatsapp.com/send?phone=5511941350260&text=Ol%C3%A1,%20atendente!%0AEntrei%20na%20minha%20conta%20do%20Impulso%20Previne%20e%20me%20deparei%20com%20uma%20mensagem%20de%20erro%20na%20tela%20inicial.%20N%C3%A3o%20consigo%20acessar%20nenhuma%20lista%20nominal!%20Me%20ajuda%20a%20resolver%20esse%20problema?"
             />
         </div>
         <Image 
