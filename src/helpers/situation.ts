2c9b4238ea9f8347cf329a90e9cacfa69fadc8fd
João Vittor Souza <jvns2010@gmail.com>
2025-03-25 19:09:44 -0300

    feat: implementa a função isValid e testes unitários correspondentes

---
+++ src/helpers/situation.ts
@@ -0,0 +1,13 @@
+import type { SituacaoPorIndicador } from "../types/inicio";
+import type { Indicadores } from "../types/inicio";
+
+const isValid = (
+	situacaoPorIndicador: SituacaoPorIndicador,
+	indicador: Indicadores,
+): boolean => {
+	if(situacaoPorIndicador[indicador]?.total > 0 || situacaoPorIndicador[indicador]?.pendente > 0) return true 
+    if(situacaoPorIndicador[indicador]?.total === 0 || situacaoPorIndicador[indicador]?.pendente === 0) return true
+    return false;
+}
+
+export { isValid };
