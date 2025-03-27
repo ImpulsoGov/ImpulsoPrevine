f7bf0605fa13544773b9f39ec047feb19828baf7
mverardo <mauricio.verardo@gmail.com>
2025-03-27 16:00:55 -0300

    fix: altera jest para rodar apenas os testes dentro de /__tests__/unit

--- jest.config.ts
+++ jest.config.ts
@@ -173,10 +173,9 @@ const config: Config = {
     // testLocationInResults: false,
 
     // The glob patterns Jest uses to detect test files
-    // testMatch: [
-    //   "**/__tests__/**/*.[jt]s?(x)",
-    //   "**/?(*.)+(spec|test).[tj]s?(x)"
-    // ],
+    testMatch: [
+        "**/__tests__/unit/**/*.[jt]s?(x)",
+    ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     // testPathIgnorePatterns: [
