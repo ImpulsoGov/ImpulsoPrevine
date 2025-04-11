03dcef3f1df182f03703489eec8a7b4aa24b11c3
Danilo Lopes <donylopes@gmail.com>
2025-04-11 17:02:38 -0300

    Dinamiza nome do municipio (#370)
    * feat: remove texto do municipio do arquivo de constantes e deriva o texto da sessão do usuário
    
    * fix: renomeia propriedade userMunicipality para municipalityIdSus no componente PanelSelector
    
    ---------
    
    Co-authored-by: mverardo <mauricio.verardo@gmail.com>

--- src/features/acf/modules/AcfDashboardPage/modules/PanelSelector/PanelSelector.container.tsx
+++ src/features/acf/modules/AcfDashboardPage/modules/PanelSelector/PanelSelector.container.tsx
@@ -1,5 +1,6 @@
 import { getCardsProps } from "@/helpers/cardsList";
 import type { ProfileIdValue } from "@/types/profile";
+import { MUNICIPIOS } from "@constants/municipios";
 import type { CardProps } from "@impulsogov/design-system/dist/molecules/Card/Card";
 import { captureException } from "@sentry/nextjs";
 import type { AcfDashboardType } from "../../types";
@@ -18,6 +19,19 @@ type PanelSelectorContainerProps = {
     acfDashboardType: AcfDashboardType
 }
 
+//TODO: Mover esta função para algum lugar que seja reutilizável
+const municipalityName = (municipalityId: string): string => {
+    //TODO: Criar um mapa associativo municipioId->municipio e usar aqui
+    const municipalityData = MUNICIPIOS.find((municipality) => municipality.municipioIdSus === municipalityId);
+
+    //TODO: Tentar encontrar uma forma de tipar as coisas pra esse caso ser impossível.
+    if (!municipalityData) {
+        return "Município - UF";
+    }
+
+    return `${municipalityData.nome} - ${municipalityData.uf}`;
+}
+
 // Container aqui se refere ao padrão Container/Presentation, descrito em: https://www.patterns.dev/react/presentational-container-pattern/
 export const PanelSelectorContainer = async ({
     municipalitySusId,
@@ -28,7 +42,7 @@ export const PanelSelectorContainer = async ({
     acfDashboardType,
 }: PanelSelectorContainerProps) => {
     let externalCardsProps: CardProps[] = [];
-    
+
     try {
         const data = await externalCardsAcfDashboardDataControllerForTeam(
             acfDashboardType,
@@ -42,11 +56,11 @@ export const PanelSelectorContainer = async ({
     }
     return externalCardsProps && 
         <PanelSelector
+                municipalityIdSus={municipalityName(municipalitySusId)}
                 externalCardsProps={externalCardsProps}
                 listName={acfDashboardType}
                 tabID = {initialTabId}
                 subTabID = {initialSubTabId}
                 userProfiles={userProfiles}
-                municipalityIdSus={municipalitySusId}
         />
 };
