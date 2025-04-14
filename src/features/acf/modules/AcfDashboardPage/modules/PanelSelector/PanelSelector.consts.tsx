738d46c375ab37fe2ef644f316fa8df9999ddd3d
danilo <donylopes@gmail.com>
2025-04-14 11:27:07 -0300

    feat: torna texto da tooltip estilizavel

--- src/features/acf/modules/AcfDashboardPage/modules/PanelSelector/PanelSelector.consts.tsx
+++ src/features/acf/modules/AcfDashboardPage/modules/PanelSelector/PanelSelector.consts.tsx
@@ -147,8 +147,12 @@ export const tabDefinitions = {
 
 export const header = {
     title: "Diabetes",
-    tooltip:
-        "Legenda\n\nTipo de diagnóstico:\n\nAutorreferido - a condição foi identificada como \"autorreferida\" quando é relatada pelo usuário na realização do Cadastro Individual.\n\nDiagnóstico Clínico - a condição foi identificada como \"diagnóstico clínico\" por haver atendimento individual confirmando o diagnóstico.",
+    tooltip:<div>
+        <p>Legenda</p>
+        <p>Tipo de diagnóstico:</p>
+        <p><b>Autorreferido</b> - a condição foi identificada como "autorreferida" quando é relatada pelo usuário na realização do Cadastro Individual.</p>
+        <p><b>Diagnóstico Clínico</b>  - a condição foi identificada como "diagnóstico clínico" por haver atendimento individual confirmando o diagnóstico.</p>
+        </div>,
     text: "A lista nominal de diabetes reúne os cidadãos que possuem a condição, seja por diagnóstico clínico ou autorreferido, e traz a situação da consulta e da solicitação de hemoblogina, que devem ser realizadas a cada seis meses para acompanhamento. Além disso, você encontrará também o nome profissional responsável pelo cidadão, para facilitar a organização da busca ativa. Utilize os filtros para segmentar a lista como preferir.",
 }
 
