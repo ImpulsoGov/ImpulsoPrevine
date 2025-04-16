729973db5d4ec56cb64003e5535fa7b6cdc468ba
Tainá Santana <tainnaps@gmail.com>
2025-04-16 16:26:08 -0300

    test: adiciona testes para a função formatDate

---
+++ __tests__/unit/lista-nominal/table/formatters/time.test.ts
@@ -0,0 +1,28 @@
+import { formatDate } from "@/common/time";
+
+describe("formatDate", () => {
+    it("deve formatar uma data corretamente no formato dd/mm/aa", () => {
+        const date = new Date("2023-10-05");
+        expect(formatDate(date)).toBe("05/10/23");
+    });
+
+    it("deve lidar corretamente com datas no início do mês e do ano em diferentes fusos", () => {
+        const date = new Date("2023-01-01T15:00:00+03:00");
+        expect(formatDate(date)).toBe("01/01/23");
+    });
+
+    it("deve lidar corretamente com datas no final do mês e do ano", () => {
+        const date = new Date("2023-12-31T00:00:00Z");
+        expect(formatDate(date)).toBe("31/12/23");
+    });
+
+    it("deve lidar com anos anteriores a 2000", () => {
+        const date = new Date("1999-07-15T00:00:00Z");
+        expect(formatDate(date)).toBe("15/07/99");
+    });
+
+    it("deve retornar a string vazia quando é passada uma data inválida", () => {
+        const date = new Date("invalid-date");
+        expect(formatDate(date)).toBe("");
+    });
+});
