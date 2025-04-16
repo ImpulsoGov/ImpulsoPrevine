1b3b6ca6d1f8b16a94b23d0dcb9613179169e8c9
Tainá Santana <tainnaps@gmail.com>
2025-04-16 16:25:33 -0300

    feat: adiciona verificação para data inválida na função formatDate

--- src/common/time.ts
+++ src/common/time.ts
@@ -1,6 +1,8 @@
 //TODO: criar tipo da string de saida que verifica formato
 //TODO criar teste unitario
 export const formatDate = (date: Date): string => {
+    if (Number.isNaN(date.valueOf())) return "";
+
     const day = date.getUTCDate().toString().padStart(2, "0");
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const year = date.getFullYear().toString().slice(2);
