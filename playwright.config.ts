d1f8106ac1307165767f30ab9fae401418a3ee0a
danilo <donylopes@gmail.com>
2025-05-05 15:03:15 -0300

    ci: altera comando de execucao playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
     ],
     /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'yarn build && yarn start',
     url: baseURL(),
     reuseExistingServer: process.env.ENV === "development",
   },
