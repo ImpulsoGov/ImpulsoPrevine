827d87f5ab7a297e69fd7b617e78d2f3e3a56d9f
João Vittor Souza <jvns2010@gmail.com>
2025-05-08 02:28:00 -0300

    ci: reduz timeout dos testes e2e e adiciona timeout para visibilidade do botao

--- __tests__/e2e/lista-nominal/lista-nominal.spec.tsx
+++ __tests__/e2e/lista-nominal/lista-nominal.spec.tsx
@@ -6,7 +6,9 @@ const pageUrl = `${baseUrl}/lista-nominal`;
 
 const login = async ({ page }: { page: Page }) => {
     await page.goto(`${baseUrl}/`);
-    await page.locator('div[class*="__NavBarLogin"]').filter({ hasText: "ACESSO RESTRITO" }).click();
+    const button =  page.locator('div[class*="__NavBarLogin"]').filter({ hasText: "ACESSO RESTRITO" });
+    button.waitFor({ state: "visible", timeout: 20000 });
+    await button.click();
     await page.getByRole('button', { name: 'ENTRAR' }).click();
     await page.getByRole('textbox', { name: 'CPF' }).fill(process.env.TEST_USER || "000.000.000-00");
     await page
