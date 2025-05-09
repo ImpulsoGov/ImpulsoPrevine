c79d4ee1026d0b7c0ab96f1c8faf2e50c103b280
danilo <donylopes@gmail.com>
2025-05-09 12:04:41 -0300

    refactor: remove arquivos duplicados

--- src/features/acf/modules/AcfDashboardPage/modules/PanelSelector/modules/dashboards/modules/filters/modules/diabetes/diabetes.model.ts
+++
@@ -1,7 +0,0 @@
-import type { DiabetesAcfItem } from "../../../TableWithFilters/modules/PaginatedTable/modules/DataTable/modules/diabetes/diabetes.model";
-
-export type DiabetesFilterItem = {
-  visitantCommunityHealthWorker: DiabetesAcfItem['visitantCommunityHealthWorker'][];
-  patientStatus: DiabetesAcfItem['patientStatus'][];
-  conditionIdentifiedBy: DiabetesAcfItem['conditionIdentifiedBy'][];
-}
