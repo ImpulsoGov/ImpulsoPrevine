c14e82d262b0384a0cd7af97dfddc6e33c05db3f
mverardo <mauricio.verardo@gmail.com>
2025-05-13 19:23:13 -0300

    feat: usa o zod para definir e parsear schema do body da requisição

--- src/app/api/lista-nominal/diabetes/query/route.ts
+++ src/app/api/lista-nominal/diabetes/query/route.ts
@@ -1,42 +1,31 @@
 import type { NextRequest } from "next/server";
 import type { QueryParams } from "./schema";
+import { queryParams as queryParamsSchema } from './schema';
 import { AuthenticationError, decodeToken, getEncodedSecret, getToken, type JWTToken } from "@/utils/token";
-import { validatePaginationParams } from "../../utils/validatePaginationParams";
 import { diabetesData, diabetesDataCount } from "@/features/acf/modules/AcfDashboardPage/modules/PanelSelector/modules/dashboards/modules/PaginatedTable/modules/DataTable/modules/diabetes/diabetes.controller";
 import { BadRequestError } from "../../utils/errors";
+import { ZodError } from "zod";
 
-//TODO: Arrumar esses imports para ficarem fora dos módulos
-
-//TODO: Adicionar schema validation com zod: https://reacthustle.com/blog/next-js-add-typescript-types-to-api-request-body-using-zod
-
+//TODO: Controllers não deveriam morar dentro de módulos privados, deveriamos passar eles pra fora
 export async function POST(
     req: NextRequest
 ) {
-    console.log("Request chegou");
-
-    //TODO: Só fazer esse cast se a validação com zod funcionar
-    const queryParams = await req.json() as QueryParams;
-
-    console.log("query params");
-    console.log(queryParams);
-   
     try {
-        // const { list } = await params; // quando for utilizar a conexao com o banco de dados
         const token = getToken(req.headers);
         const secret = getEncodedSecret();
         const { payload } = (await decodeToken(token, secret)) as JWTToken;
+        //TODO: Se alguém estiver logado com um municipalitySusID e passar outro no body, o que acontece?
         const municipalitySusID = payload?.municipio as string;
+        //TODO: Se alguém estiver logado com um teamIne e passar outro no body, o que acontece?
         const teamIne = payload?.equipe as string;
 
-        const pagination =  queryParams.pagination
-        const filters = queryParams.filters;
+        const body = await req.json();
+        const queryParams = queryParamsSchema.parse(body) as QueryParams;
 
-        //TODO: Checar se dá pra validar valores < 0 usando o zod
-        if (pagination.page || pagination.pageSize) validatePaginationParams(pagination);
-
-        //TODO: Tirar esse filtro vazio
-        const data = await diabetesData(municipalitySusID, teamIne, pagination, filters || {});
-        const totalRows = await diabetesDataCount(municipalitySusID, teamIne, filters || {});
+        //TODO: Tirar esse filtro vazio || {}
+        const data = await diabetesData(municipalitySusID, teamIne, queryParams.pagination, queryParams.filters || {});
+        //TODO: Tirar esse filtro vazio || {}
+        const totalRows = await diabetesDataCount(municipalitySusID, teamIne, queryParams.filters || {});
 
         return Response.json(
             {
@@ -48,6 +37,9 @@ export async function POST(
     } catch (error) {
         //TODO: Fazer essa lógica em algum middleware
         console.error(error);
+        if (error instanceof ZodError) {
+            return Response.json({ message: error.message }, { status: 400 });
+        }
         if (error instanceof BadRequestError) {
             return Response.json({ message: error.message }, { status: 400 });
         }
