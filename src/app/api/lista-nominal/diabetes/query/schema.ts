94ec71070443ef893cfd035ab34264941ad34a2a
mverardo <mauricio.verardo@gmail.com>
2025-05-13 19:23:13 -0300

    feat: usa o zod para definir e parsear schema do body da requisição

--- src/app/api/lista-nominal/diabetes/query/schema.ts
+++ src/app/api/lista-nominal/diabetes/query/schema.ts
@@ -1,21 +1,55 @@
-import type { ConditionIdentifiedBy, PatientAgeRange, PatientStatus } from "@/features/acf/modules/AcfDashboardPage/modules/PanelSelector/modules/dashboards/modules/PaginatedTable/modules/DataTable/modules/diabetes/diabetes.model"
+import { z } from "zod";
 
-//TODO: Usar unions ao invés de ?
-export type FilterParams = {
-    municipalitySusID: string,
-    teamIne: string,
-    patientStatus?: PatientStatus,
-    conditionIdentifiedBy?: ConditionIdentifiedBy,
-    visitantCommunityHealthWorker?: string,
-    patientAgeRange?: PatientAgeRange,
-}
+export const patientStatus = z.enum([
+    "Apenas a consulta a fazer",
+    "Apenas o exame a fazer",
+    "Consulta e exame a fazer",
+    "Consulta e exame em dia"
+])
 
-export type PaginationParams = {
-    page: number,
-    pageSize: number,
-}
+export type PatientStatus = z.infer<typeof patientStatus>;
 
-export type QueryParams = {
-    pagination: PaginationParams,
-    filters?: FilterParams
-}
+export const conditionIdentifiedBy = z.enum([ 
+    "Diagnóstico clínico", 
+    "Autorreferido"
+]);
+
+export type ConditionIdentifiedBy = z.infer<typeof conditionIdentifiedBy>;
+
+export const patientAgeRange = z.enum([ 
+    "Menos de 17 anos " ,
+    "Entre 18 e 24 anos" , 
+    "Entre 25 e 34 anos" , 
+    "Entre 35 e 44 anos" , 
+    "Entre 45 e 54 anos" , 
+    "Entre 55 e 65 anos" , 
+    "65 anos ou mais"
+])
+
+export type PatientAgeRange = z.infer<typeof patientAgeRange>;
+
+export const filterParams = z.object({
+    municipalitySusID: z.string(),
+    teamIne: z.string(),
+    patientStatus: z.optional(patientStatus),
+    conditionIdentifiedBy: z.optional(conditionIdentifiedBy),
+    visitantCommunityHealthWorker: z.optional(z.string()),
+    patientAgeRange: z.optional(patientAgeRange),
+})
+
+export type FilterParams = z.infer<typeof filterParams>;
+
+export const paginationParams = z.object({
+    page: z.number().nonnegative(),
+    pageSize: z.number().nonnegative(),
+})
+
+export type PaginationParams = z.infer<typeof paginationParams>;
+
+export const queryParams = z.object({
+    pagination: paginationParams,
+    filters: z.optional(filterParams),
+})
+
+export type QueryParams = z.infer<typeof queryParams>;
+
