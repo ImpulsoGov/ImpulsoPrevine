37c009e6a1e657f6902494ea9d292f0abfd27a1e
João Vittor Souza <jvns2010@gmail.com>
2025-05-13 12:34:21 -0300

    chore: remove .node-version, adiciona .prettierignore e prettier, atualiza regras do eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,67 @@
 // @ts-check
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import prettierConfig from "eslint-config-prettier";
 export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.strictTypeChecked,{
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-     },
-   },
- }
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  prettierConfig, // desativa regras de formatação do eslint para evitar conflitos com o prettier
+  {
+    rules: {
+      "@typescript-eslint/switch-exhaustiveness-check": "error",
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-expect-error": "allow-with-description",
+        },
+      ],
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/array-type": ["error", { default: "generic" }],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "TSEnumDeclaration",
+          message: "Replace enum with a literal type or a const assertion.",
+        },
+      ],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          types: ["boolean"],
+          format: ["PascalCase"],
+          prefix: ["is", "are", "should", "has", "can", "did", "will"],
+        },
+        {
+          selector: "typeAlias",
+          format: ["PascalCase"],
+        },
+        {
+          // Generic type parameter must start with letter T, followed by any uppercase letter.
+          selector: "typeParameter",
+          format: ["PascalCase"],
+          custom: { regex: "^T[A-Z]", match: true },
+        },
+      ],
+      "react/jsx-handler-names": [
+        "error",
+        {
+          eventHandlerPrefix: "handle",
+          eventHandlerPropPrefix: "on",
+        },
+      ],
+      "react-hooks/rules-of-hooks": "error",
+      "react/hook-use-state": "error",
+    },
+  },
 );
