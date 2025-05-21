6c52a536f96a92ce1fd2cb3481477c38c7eeb15f
João Vittor Souza <jvns2010@gmail.com>
2025-05-21 15:21:26 -0300

    feat: adiciona ignorepattern para regras de variaveis nao utilizadas

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 // @ts-check
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import prettierConfig from 'eslint-config-prettier'
-import reactConfig from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+import reactConfig from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default tseslint.config([
     eslint.configs.recommended,
@@ -11,18 +11,18 @@ export default tseslint.config([
     {
         plugins: {
             react: reactConfig,
-            'react-hooks': reactHooks,
+            "react-hooks": reactHooks,
         },
         rules: {
-            'react/jsx-handler-names': [
-                'error',
+            "react/jsx-handler-names": [
+                "error",
                 {
-                    eventHandlerPrefix: 'handle',
-                    eventHandlerPropPrefix: 'on',
+                    eventHandlerPrefix: "handle",
+                    eventHandlerPropPrefix: "on",
                 },
             ],
-            'react-hooks/rules-of-hooks': 'error',
-            'react/hook-use-state': 'error',
+            "react-hooks/rules-of-hooks": "error",
+            "react/hook-use-state": "error",
         },
         languageOptions: {
             parserOptions: {
@@ -38,51 +38,59 @@ export default tseslint.config([
     prettierConfig,
     {
         rules: {
-            '@typescript-eslint/switch-exhaustiveness-check': 'error',
-            '@typescript-eslint/ban-ts-comment': [
-                'error',
+            "@typescript-eslint/switch-exhaustiveness-check": "error",
+            "@typescript-eslint/ban-ts-comment": [
+                "error",
                 {
                     // só permite @ts-expect-error com descrição
-                    'ts-expect-error': 'allow-with-description',
+                    "ts-expect-error": "allow-with-description",
                 },
             ],
-            '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-            '@typescript-eslint/array-type': ['error', { default: 'generic' }],
-            '@typescript-eslint/consistent-type-imports': 'error',
-            '@typescript-eslint/explicit-function-return-type': 'warn',
-            'no-restricted-syntax': [
-                'error',
+            "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+            "@typescript-eslint/no-unused-vars": [
+                "warn", // or "error"
                 {
-                    selector: 'TSEnumDeclaration',
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_",
+                },
+            ],
+            "@typescript-eslint/array-type": ["error", { default: "generic" }],
+            "@typescript-eslint/consistent-type-imports": "error",
+            "@typescript-eslint/explicit-function-return-type": "warn",
+            "no-restricted-syntax": [
+                "error",
+                {
+                    selector: "TSEnumDeclaration",
                     message:
-                        'Replace enum with a literal type or a const assertion.',
+                        "Replace enum with a literal type or a const assertion.",
                 },
             ],
-            '@typescript-eslint/naming-convention': [
-                'warn',
+            "@typescript-eslint/naming-convention": [
+                "warn",
                 {
-                    selector: 'variable',
-                    types: ['boolean'],
-                    format: ['PascalCase'],
+                    selector: "variable",
+                    types: ["boolean"],
+                    format: ["PascalCase"],
                     prefix: [
-                        'is',
-                        'are',
-                        'should',
-                        'has',
-                        'can',
-                        'did',
-                        'will',
+                        "is",
+                        "are",
+                        "should",
+                        "has",
+                        "can",
+                        "did",
+                        "will",
                     ],
                 },
                 {
-                    selector: 'typeAlias',
-                    format: ['PascalCase'],
+                    selector: "typeAlias",
+                    format: ["PascalCase"],
                 },
                 {
                     // Parâmetros de tipo genéricos devem começar com a letra T, seguida de qualquer letra maiúscula. Exemplos: TUser, TProduct.
-                    selector: 'typeParameter',
-                    format: ['PascalCase'],
-                    custom: { regex: '^T[A-Z]', match: true },
+                    selector: "typeParameter",
+                    format: ["PascalCase"],
+                    custom: { regex: "^T[A-Z]", match: true },
                 },
             ],
         },
@@ -90,12 +98,12 @@ export default tseslint.config([
     {
         // substitui o uso do `.eslintignore`
         ignores: [
-            '.next/',
-            'node_modules/',
-            'coverage/',
-            '**/__snapshots__/**',
-            'package-lock.json',
-            'public/',
+            ".next/",
+            "node_modules/",
+            "coverage/",
+            "**/__snapshots__/**",
+            "package-lock.json",
+            "public/",
         ],
     },
-])
+]);
