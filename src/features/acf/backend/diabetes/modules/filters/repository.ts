bf156d6f05df9611f145173d51c41a54451c9957
mverardo <mauricio.verardo@gmail.com>
2025-06-16 18:14:56 -0300

    Remove duplicação para fazer select distinct

--- src/features/acf/backend/diabetes/modules/filters/repository.ts
+++ src/features/acf/backend/diabetes/modules/filters/repository.ts
@@ -3,145 +3,74 @@ import type {
     PatientAgeRange,
     PatientStatus,
 } from "@/features/acf/shared/diabetes/model";
-import type { impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos } from "@prisma/client";
+import { type impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos } from "@prisma/client";
 import { prisma } from "@prisma/prismaClient";
 import type { FiltersOptions } from "./model";
 
-const communityHealthWorkerOptions = async (
+async function fieldOptions<
+    TField extends
+        keyof impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos,
+>(
+    field: TField,
     municipalitySusId: string,
     teamIne: string
 ): Promise<
     ReadonlyArray<
-        Pick<
-            impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos,
-            "acs_nome_cadastro"
-        >
+        impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos[TField]
     >
-> => {
-    return await prisma.impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos.findMany(
-        {
-            select: {
-                acs_nome_cadastro: true,
-            },
-            distinct: ["acs_nome_cadastro"],
-            where: {
-                municipio_id_sus: municipalitySusId,
-                equipe_ine_cadastro: teamIne,
-            },
-        }
-    );
-};
+> {
+    const result =
+        await prisma.impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos.findMany(
+            {
+                select: {
+                    [field]: true,
+                },
+                distinct: [field],
+                where: {
+                    municipio_id_sus: municipalitySusId,
+                    equipe_ine_cadastro: teamIne,
+                },
+            }
+        );
+    return result.map((item) => item[field]);
+}
 
-const patientStatusOptions = async (
-    municipalitySusId: string,
-    teamIne: string
-): Promise<
-    ReadonlyArray<
-        Pick<
-            impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos,
-            "status_usuario"
-        >
-    >
-> => {
-    return await prisma.impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos.findMany(
-        {
-            select: {
-                status_usuario: true,
-            },
-            distinct: ["status_usuario"],
-            where: {
-                municipio_id_sus: municipalitySusId,
-                equipe_ine_cadastro: teamIne,
-            },
-        }
-    );
-};
-
-const conditionIdentifiedByOptions = async (
-    municipalitySusId: string,
-    teamIne: string
-): Promise<
-    ReadonlyArray<
-        Pick<
-            impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos,
-            "identificacao_condicao_diabetes"
-        >
-    >
-> => {
-    return await prisma.impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos.findMany(
-        {
-            select: {
-                identificacao_condicao_diabetes: true,
-            },
-            distinct: ["identificacao_condicao_diabetes"],
-            where: {
-                municipio_id_sus: municipalitySusId,
-                equipe_ine_cadastro: teamIne,
-            },
-        }
-    );
-};
-
-const patientAgeRangeOptions = async (
-    municipalitySusId: string,
-    teamIne: string
-): Promise<
-    ReadonlyArray<
-        Pick<
-            impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos,
-            "cidadao_faixa_etaria"
-        >
-    >
-> => {
-    return await prisma.impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos.findMany(
-        {
-            select: {
-                cidadao_faixa_etaria: true,
-            },
-            distinct: ["cidadao_faixa_etaria"],
-            where: {
-                municipio_id_sus: municipalitySusId,
-                equipe_ine_cadastro: teamIne,
-            },
-        }
-    );
-};
-
-//TODO: Arrumar o nome
 export const coeqFilterOptions = async (
     municipalitySusId: string,
     teamIne: string
 ): Promise<FiltersOptions> => {
-    const visitantCommunityHealthWorker = await communityHealthWorkerOptions(
+    const visitantCommunityHealthWorker = await fieldOptions(
+        "acs_nome_cadastro",
         municipalitySusId,
         teamIne
     );
-    const patientStatus = await patientStatusOptions(
+
+    const patientStatus = await fieldOptions(
+        "status_usuario",
         municipalitySusId,
         teamIne
     );
-    const conditionIdentifiedBy = await conditionIdentifiedByOptions(
+    const conditionIdentifiedBy = await fieldOptions(
+        "identificacao_condicao_diabetes",
         municipalitySusId,
         teamIne
     );
-    const patientAgeRange = await patientAgeRangeOptions(
+    const patientAgeRange = await fieldOptions(
+        "cidadao_faixa_etaria",
         municipalitySusId,
         teamIne
     );
 
     return {
         visitantCommunityHealthWorker: visitantCommunityHealthWorker.map(
-            (field) => field.acs_nome_cadastro || ""
+            (field) => field || ""
         ),
-        patientStatus: patientStatus.map(
-            (field) => field.status_usuario as PatientStatus
-        ),
+        patientStatus: patientStatus.map((field) => field as PatientStatus),
         conditionIdentifiedBy: conditionIdentifiedBy.map(
-            (field) =>
-                field.identificacao_condicao_diabetes as ConditionIdentifiedBy
+            (field) => field as ConditionIdentifiedBy
         ),
         patientAgeRange: patientAgeRange.map(
-            (field) => field.cidadao_faixa_etaria as PatientAgeRange
+            (field) => field as PatientAgeRange
         ),
     };
 };
