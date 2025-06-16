80caee430fe8ceff21b9cf4e4a46279e07b2bba7
Maurício Verardo <mauricio.verardo@gmail.com>
2025-06-16 18:23:47 -0300

    Separa o código dos filtros para fazer um select distinct para cada campo (#419)
    * Usa vários distincts para retornar as opções possíveis de cada filtro
    
    * Remove duplicação para fazer select distinct
    
    * Remove código não utilizado

--- src/features/acf/backend/diabetes/modules/filters/model.ts
+++ src/features/acf/backend/diabetes/modules/filters/model.ts
@@ -1,20 +1,14 @@
 import type { DiabetesAcfItem } from "@features/acf/shared/diabetes/model";
-import type { impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos } from "@prisma/client";
 
 //TODO: sumir com esse arquivo, esses tipos podem ir pra dentro dos módulos
 export type FiltersOptions = {
-    visitantCommunityHealthWorker: Array<
+    //TODO: Este nome está errado. No DB nós usamos o acs_nome_cadastro, e não o acs que faz as visitas. Precisamos renomear.
+    visitantCommunityHealthWorker: ReadonlyArray<
         DiabetesAcfItem["visitantCommunityHealthWorker"]
     >;
-    patientStatus: Array<DiabetesAcfItem["patientStatus"]>;
-    conditionIdentifiedBy: Array<DiabetesAcfItem["conditionIdentifiedBy"]>;
-    patientAgeRange: Array<DiabetesAcfItem["patientAgeRange"]>;
+    patientStatus: ReadonlyArray<DiabetesAcfItem["patientStatus"]>;
+    conditionIdentifiedBy: ReadonlyArray<
+        DiabetesAcfItem["conditionIdentifiedBy"]
+    >;
+    patientAgeRange: ReadonlyArray<DiabetesAcfItem["patientAgeRange"]>;
 };
-
-export type FilterOptionsDb = Pick<
-    impulso_previne_dados_nominais___painel_enfermeiras_lista_nominal_diabeticos,
-    | "acs_nome_cadastro"
-    | "status_usuario"
-    | "identificacao_condicao_diabetes"
-    | "cidadao_faixa_etaria"
->;
