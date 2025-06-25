3f5ab1cc24c5adddf3799b367f6f66ef81947a89
mverardo <mauricio.verardo@gmail.com>
2025-06-25 12:06:22 -0300

    Volta comentário retirado

--- src/features/acf/frontend/common/WithSorting/context.ts
+++ src/features/acf/frontend/common/WithSorting/context.ts
@@ -6,6 +6,7 @@ import { createContext } from "react";
 //      O ideal seria restringir este tipo e só transformar em string na hora de usar no data grid.
 
 //O campo que define a ordem da ordenação no tipo do x-data-grid se chama sort, por isso decidimos padronizar como sort no lugar de order.
+//TODO: Este default deveria estar no backend, e não aqui no front. Depois de adaptar lá, precisamos mudar aqui pra se tornar um GridSortItem | null
 export const DEFAULT_SORTING: GridSortItem = {
     field: "patientName",
     sort: "asc",
