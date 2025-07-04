01900621f5881e0e0641a02392049f72eca17699
João Vittor Souza <jvns2010@gmail.com>
2025-07-04 17:43:09 -0300

    feat: Ajusta formatacao das opcões do filtro de equipe

--- src/features/acf/frontend/diabetes/modules/AcfPage/modules/PanelSelector/modules/List/modules/FiltersBar/modules/CoapsFiltersBar/logic.ts
+++ src/features/acf/frontend/diabetes/modules/AcfPage/modules/PanelSelector/modules/List/modules/FiltersBar/modules/CoapsFiltersBar/logic.ts
@@ -5,6 +5,7 @@ import {
     toSelectConfigsShared,
 } from "../../logic";
 import type { SelectConfig } from "../common/SelectConfig";
+import { nameFormatter } from "../../../../../../logic";
 
 //TODO: Existem funcoes duplicadas entre CoapsFiltersBar e CoeqFiltersBar, podemos criar uma camada de abstração para elas
 
@@ -20,9 +21,9 @@ export const toSelectConfigsCoaps = (
     return [
         ...toSelectConfigsShared(filtersValues),
         {
-            options: toHtmlSelectOptions(filtersValues.careTeamName).sort(
-                sortedOptions
-            ),
+            options: toHtmlSelectOptions(filtersValues.careTeamName)
+                .map((item) => ({ ...item, label: nameFormatter(item.label) }))
+                .sort(sortedOptions),
             label: "Equipe",
             id: "careTeamName",
             isMultiSelect: true,
