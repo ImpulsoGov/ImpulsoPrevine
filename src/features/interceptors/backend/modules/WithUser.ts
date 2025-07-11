2f1a5fbe815d22fbc709377359b639432ebd9257
Tainá Santana <tainnaps@gmail.com>
2025-07-11 16:56:31 -0300

    Altera criacao de user dentro do context de withUser

--- src/features/interceptors/backend/modules/WithUser.ts
+++ src/features/interceptors/backend/modules/WithUser.ts
@@ -19,15 +19,12 @@ export const withUser = <TContext>(
         const secret = getEncodedSecret();
         const { payload } = (await decodeToken(token, secret)) as JWTToken;
 
-        const contextWithUser: ContextWithUser<TContext> = {
-            ...context,
-            user: {
-                municipalitySusId: payload.municipio,
-                teamIne: payload.equipe,
-                profiles: payload.perfis,
-            },
+        context.user = {
+            municipalitySusId: payload.municipio,
+            teamIne: payload.equipe,
+            profiles: payload.perfis,
         };
 
-        return handler(request, contextWithUser);
+        return handler(request, context);
     };
 };
