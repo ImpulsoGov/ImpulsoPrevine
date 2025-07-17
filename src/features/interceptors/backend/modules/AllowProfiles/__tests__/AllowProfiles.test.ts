4ab4795c4e90b978fb5d0f08e07a92e3be22b20c
Tainá Santana <tainnaps@gmail.com>
2025-07-17 10:31:08 -0300

    Remove duplicacoes e melhora escrita do teste de allowProfiles

--- src/features/interceptors/backend/modules/AllowProfiles/__tests__/AllowProfiles.test.ts
+++ src/features/interceptors/backend/modules/AllowProfiles/__tests__/AllowProfiles.test.ts
@@ -2,47 +2,58 @@ import type { NextRequest } from "next/server";
 import { allowProfiles } from "..";
 import { PROFILE_ID } from "@/types/profile";
 import { AuthorizationError } from "@/features/errors/backend";
+import type { Handler } from "../../common/Handler";
+import { decodeToken, getEncodedSecret, getToken } from "@/utils/token";
+
+jest.mock("@/utils/token", () => ({
+    getToken: jest.fn(),
+    getEncodedSecret: jest.fn(),
+    decodeToken: jest.fn(),
+}));
 
 const allowedProfiles = [PROFILE_ID.COAPS, PROFILE_ID.userManagement];
 const notAllowedProfiles = [PROFILE_ID.COEQ, PROFILE_ID.impulser];
+const tokenPayload = { perfis: allowedProfiles };
+const token = "fake.jwt.token";
+const secret = "encoded-secret";
+const request = {} as NextRequest;
+const context: Record<string, string> = {};
 
-jest.mock("@/utils/token", () => ({
-    getToken: jest.fn().mockReturnValue("mocked-token"),
-    getEncodedSecret: jest
-        .fn()
-        .mockReturnValue(new TextEncoder().encode("mocked-secret")),
-    decodeToken: jest.fn().mockResolvedValue({
-        payload: {
-            perfis: [8, 2],
-        },
-    }),
-}));
+const mockGetToken = getToken as jest.Mock;
+const mockGetEncodedSecret = getEncodedSecret as jest.Mock;
+const mockDecodeToken = decodeToken as jest.Mock;
+const mockHandler: jest.MockedFunction<Handler<typeof context>> = jest.fn();
 
 describe("allowProfiles", () => {
-    it("deve permitir acesso a usuários que possuen os perfis permitidos", async () => {
-        const response = { message: "Acesso permitido" };
-        const context = {};
-        const request = {} as NextRequest;
-        const handler = jest.fn().mockResolvedValue(response);
+    it("deve permitir acesso a usuários que possuem os perfis permitidos", async () => {
+        const response = { message: "Acesso permitido" } as unknown as Response;
+
+        mockHandler.mockResolvedValue(response);
+        mockGetToken.mockReturnValue(token);
+        mockGetEncodedSecret.mockReturnValue(secret);
+        mockDecodeToken.mockResolvedValue({ payload: tokenPayload });
+
         const interceptor = allowProfiles(allowedProfiles);
-        const routeHandler = interceptor(handler);
+        const routeHandler = interceptor(mockHandler);
 
         const routeHandlerResponse = await routeHandler(request, context);
 
         expect(routeHandlerResponse).toEqual(response);
-        expect(handler).toHaveBeenCalledWith(request, context);
+        expect(mockHandler).toHaveBeenCalledWith(request, context);
     });
 
-    it("deve negar acesso a usuários que não possuen os perfis permitidos", async () => {
-        const context = {};
-        const request = {} as NextRequest;
-        const handler = jest.fn();
+    it("deve lançar um erro de autorização quando o usuário não possue os perfis permitidos", async () => {
+        mockHandler.mockResolvedValue({} as Response);
+        mockGetToken.mockReturnValue(token);
+        mockGetEncodedSecret.mockReturnValue(secret);
+        mockDecodeToken.mockResolvedValue({ payload: tokenPayload });
+
         const interceptor = allowProfiles(notAllowedProfiles);
-        const routeHandler = interceptor(handler);
+        const routeHandler = interceptor(mockHandler);
 
         await expect(routeHandler(request, context)).rejects.toThrow(
             AuthorizationError
         );
-        expect(handler).not.toHaveBeenCalled();
+        expect(mockHandler).not.toHaveBeenCalled();
     });
 });
