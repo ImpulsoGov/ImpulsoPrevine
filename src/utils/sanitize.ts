96608e8c802673111961840e176157214140bf0c
danilo <donylopes@gmail.com>
2025-08-04 12:01:48 -0300

    fix: atualiza dompurify e suas definições de tipo para versões mais recentes

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -1,6 +1,6 @@
 import DOMPurify from "dompurify";
 
-export const sanitize = (content: any) => {
+export const sanitize = (content: string | Node): string | Node => {
     return typeof window !== "undefined"
         ? DOMPurify.sanitize(content)
         : content;
