572b64896a001075c4f2cfd994b29fdfd6041250
danilo <donylopes@gmail.com>
2025-08-04 15:43:54 -0300

    atualiza importações do Prisma para usar o cliente correto

--- prisma/prismaClient.tsx
+++ prisma/prismaClient.tsx
@@ -1,3 +1,3 @@
-import { PrismaClient } from ".prisma/serviceLayerClient";
+import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
--- src/features/acf/backend/diabetes/modules/table/repository.ts
+++ src/features/acf/backend/diabetes/modules/table/repository.ts
@@ -1,4 +1,4 @@
-import type { DiabetesAcfItem } from ".prisma/serviceLayerClient";
+import type { DiabetesAcfItem } from "@prisma/client";
 import type {
     CoapsFilters,
     CoapsSort,
