1b8e951e96cf64bddf8b46530632e7db37068b47
Tainá Santana <tainnaps@gmail.com>
2025-08-08 12:03:58 -0300

    refactor: altera textos do PanelSelector

--- src/features/acf/frontend/hypertension/modules/AcfPage/consts.tsx
+++ src/features/acf/frontend/hypertension/modules/AcfPage/consts.tsx
@@ -16,6 +16,6 @@ export const sharedHeader = {
 };
 
 export const textCoaps =
-    "Acompanhe duas boas práticas definidas pelo Ministério da Saúde do indicador C5 de cuidado da pessoa com hipertensão de forma simples visualizando a realização de consultas com profissional da saúde e aferição da pressão arterial. A lista mostra, com base nos registros da base local do município, como anda o cuidado das pessoas com hipertensão arterial nas diferentes equipes do território.";
+    "Acompanhe duas boas práticas definidas pelo Ministério da Saúde do indicador C5 de cuidado da pessoa com hipertensão de forma simples: visualizando a realização de consultas com profissional da saúde e aferição da pressão arterial. A lista mostra, com base nos registros da base local do município, como anda o cuidado das pessoas com hipertensão arterial nas diferentes equipes do território.";
 export const textCoeq =
-    "Acompanhe duas boas práticas definidas pelo Ministério da Saúde do indicador C5 de cuidado da pessoa com hipertensão de forma simples visualizando a realização de consultas com profissional da saúde e aferição da pressão arterial. A lista mostra, com base nos registros da base local do município, como anda o cuidado das pessoas com hipertensão vinculadas à sua equipe.";
+    "Acompanhe duas boas práticas definidas pelo Ministério da Saúde do indicador C5 de cuidado da pessoa com hipertensão de forma simples: visualizando a realização de consultas com profissional da saúde e aferição da pressão arterial. A lista mostra, com base nos registros da base local do município, como anda o cuidado das pessoas com hipertensão arterial vinculadas à sua equipe.";
