24e42c4609a30588284749f37d98a053f7abdc32
João Vittor Souza <jvns2010@gmail.com>
2025-08-12 20:20:54 -0300

    wip: problemas escrevendo teste de unidade do RenderPatientNameCpfCns

---
+++ src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/RenderPatientNameCpfCns/__tests__/RenderPatientNameCpfCns.test.tsx
@@ -0,0 +1,19 @@
+// import "@testing-library/jest-dom"
+// import {render, screen} from "@testing-library/react"
+// import { RenderPatientNameCpfCns } from ".."
+// import type { HypertensionAcfItem } from "@/features/acf/shared/hypertension/model";
+// import type { GridRenderCellParams } from "@mui/x-data-grid";
+// import type { BaseRow } from "..";
+// const mockParams = (row: BaseRow): GridRenderCellParams<BaseRow> => ({
+//   row,
+//   api: {} as any,
+// });
+
+// const baseRow: BaseRow = {patientName:"JOSÉ DA SILVA", patientCpf: "12345678922", patientCns: ""}
+// describe("RenderPatientNameCpfCns", () => {
+//     it("Deve renderizar o nome e o Cpf formatados corretamente quando ambos existirem", () => {
+
+//         render(<RenderPatientNameCpfCns row={mockParams(baseRow)}/>)
+//     })
+
+// })
