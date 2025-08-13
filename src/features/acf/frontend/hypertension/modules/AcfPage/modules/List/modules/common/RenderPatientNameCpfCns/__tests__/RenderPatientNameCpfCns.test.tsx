98f33eaab376c734b8e69038a4b6a29f248a7d6a
Tainá Santana <tainnaps@gmail.com>
2025-08-13 12:57:32 -0300

    test: adiciona testes de RenderPatientNameCpfCns com alguns falhando

--- src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/RenderPatientNameCpfCns/__tests__/RenderPatientNameCpfCns.test.tsx
+++ src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/RenderPatientNameCpfCns/__tests__/RenderPatientNameCpfCns.test.tsx
@@ -1,19 +1,115 @@
-// import "@testing-library/jest-dom"
-// import {render, screen} from "@testing-library/react"
-// import { RenderPatientNameCpfCns } from ".."
-// import type { HypertensionAcfItem } from "@/features/acf/shared/hypertension/model";
-// import type { GridRenderCellParams } from "@mui/x-data-grid";
-// import type { BaseRow } from "..";
-// const mockParams = (row: BaseRow): GridRenderCellParams<BaseRow> => ({
-//   row,
-//   api: {} as any,
-// });
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import type { BaseRow } from "..";
+import { RenderPatientNameCpfCns } from "..";
+import type { GridRenderCellParams } from "@mui/x-data-grid";
+import { nameFormatter } from "@/features/acf/frontend/common/NameFormatter";
+import { cpf } from "cpf-cnpj-validator";
+import { cnsFormatter } from "../modules/CnsFormatter";
 
-// const baseRow: BaseRow = {patientName:"JOSÉ DA SILVA", patientCpf: "12345678922", patientCns: ""}
-// describe("RenderPatientNameCpfCns", () => {
-//     it("Deve renderizar o nome e o Cpf formatados corretamente quando ambos existirem", () => {
+jest.mock("cpf-cnpj-validator", () => ({ cpf: { format: jest.fn() } }));
+jest.mock("../modules/CnsFormatter", () => ({ cnsFormatter: jest.fn() }));
+jest.mock("@/features/acf/frontend/common/NameFormatter", () => ({
+    nameFormatter: jest.fn(),
+}));
 
-//         render(<RenderPatientNameCpfCns row={mockParams(baseRow)}/>)
-//     })
+const mockedNameFormatter = nameFormatter as jest.Mock;
+const mockedCpfFormat = cpf.format as jest.Mock;
+const mockedCnsFormatter = cnsFormatter as jest.Mock;
 
-// })
+describe("RenderPatientNameCpfCns", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("Deve renderizar o nome e o CPF quando CPF e CNS existem", () => {
+        const params = {
+            row: {
+                patientName: "JOSÉ DA SILVA",
+                patientCpf: "12345678922",
+                patientCns: "123456789012345",
+            },
+        } as GridRenderCellParams<BaseRow>;
+        const formattedName = "José da Silva";
+        const formattedCpf = "123.456.789-22";
+        const formattedCns = "123 4567 8901 2345";
+
+        mockedNameFormatter.mockReturnValue(formattedName);
+        mockedCpfFormat.mockReturnValue(formattedCpf);
+        mockedCnsFormatter.mockReturnValue(formattedCns);
+
+        render(<RenderPatientNameCpfCns {...params} />);
+
+        expect(screen.getByText(formattedName)).toBeInTheDocument();
+        expect(screen.getByText(formattedCpf)).toBeInTheDocument();
+        expect(screen.queryByText(formattedCns)).not.toBeInTheDocument();
+    });
+
+    it("Deve renderizar o nome e o CPF quando CPF existe e o CNS não", () => {
+        const params = {
+            row: {
+                patientName: "JOSÉ DA SILVA",
+                patientCpf: "12345678922",
+                patientCns: "",
+            },
+        } as GridRenderCellParams<BaseRow>;
+        const formattedName = "José da Silva";
+        const formattedCpf = "123.456.789-22";
+        const formattedCns = "";
+
+        mockedNameFormatter.mockReturnValue(formattedName);
+        mockedCpfFormat.mockReturnValue(formattedCpf);
+        mockedCnsFormatter.mockReturnValue(formattedCns);
+
+        render(<RenderPatientNameCpfCns {...params} />);
+
+        expect(screen.getByText(formattedName)).toBeInTheDocument();
+        expect(screen.getByText(formattedCpf)).toBeInTheDocument();
+        expect(screen.queryByText(formattedCns)).not.toBeInTheDocument();
+    });
+
+    it("Deve renderizar o nome e o CNS quando CNS existe e o CPF não", () => {
+        const params = {
+            row: {
+                patientName: "JOSÉ DA SILVA",
+                patientCpf: "",
+                patientCns: "123456789012345",
+            },
+        } as GridRenderCellParams<BaseRow>;
+        const formattedName = "José da Silva";
+        const formattedCpf = "";
+        const formattedCns = "123 4567 8901 2345";
+
+        mockedNameFormatter.mockReturnValue(formattedName);
+        mockedCpfFormat.mockReturnValue(formattedCpf);
+        mockedCnsFormatter.mockReturnValue(formattedCns);
+
+        render(<RenderPatientNameCpfCns {...params} />);
+
+        expect(screen.getByText(formattedName)).toBeInTheDocument();
+        expect(screen.getByText(formattedCns)).toBeInTheDocument();
+        expect(screen.queryByText(formattedCpf)).not.toBeInTheDocument();
+    });
+
+    it("Deve renderizar o nome e - quando CPF e CNS não existem", () => {
+        const params = {
+            row: {
+                patientName: "JOSÉ DA SILVA",
+                patientCpf: "",
+                patientCns: "",
+            },
+        } as GridRenderCellParams<BaseRow>;
+        const formattedName = "José da Silva";
+        const formattedCpf = "";
+        const formattedCns = "";
+
+        mockedNameFormatter.mockReturnValue(formattedName);
+        mockedCpfFormat.mockReturnValue(formattedCpf);
+        mockedCnsFormatter.mockReturnValue(formattedCns);
+
+        render(<RenderPatientNameCpfCns {...params} />);
+
+        expect(screen.getByText(formattedName)).toBeInTheDocument();
+        expect(screen.getByText("-")).toBeInTheDocument();
+    });
+});
