e96dcc90ad959b17409652e5c9cd82231278ec40
Tainá Santana <tainnaps@gmail.com>
2025-08-14 11:40:09 -0300

    refactor: remove modulo inutilizado

--- src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/RenderPatientNameCpfCns/modules/CnsFormatter/index.ts
+++
@@ -1,7 +0,0 @@
-export const cnsFormatter = (cns: string): string => {
-    const part1 = cns.slice(0, 3);
-    const part2 = cns.slice(3, 7);
-    const part3 = cns.slice(7, 11);
-    const part4 = cns.slice(11, 15);
-    return `${part1} ${part2} ${part3} ${part4}`;
-};
