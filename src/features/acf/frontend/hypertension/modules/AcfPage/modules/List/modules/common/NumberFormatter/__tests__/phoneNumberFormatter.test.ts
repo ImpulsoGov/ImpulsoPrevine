5937d433b1545ed405c47418189b47a42400f8ba
Tainá Santana <tainnaps@gmail.com>
2025-08-15 09:26:06 -0300

    test: adiciona teste de integracao para phoneNumberFormatter

---
+++ src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/NumberFormatter/__tests__/phoneNumberFormatter.test.ts
@@ -0,0 +1,86 @@
+import { phoneNumberFormatter } from "../numberFormatter";
+
+// ? O mock parcial não estava funcionando, então fiz um teste de integração
+// jest.mock("../numberFormatter", () => {
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//     const originalModule = jest.requireActual("../numberFormatter");
+
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+//     return {
+//         __esModule: true,
+//         ...originalModule,
+//         invalidPhoneNumberFormatter: jest.fn(() => { console.log("mocked invalidPhoneNumberFormatter") }),
+//     };
+// });
+
+// const mockedInvalidPhoneNumberFormatter = invalidPhoneNumberFormatter as jest.Mock;
+
+describe("phoneNumberFormatter", () => {
+    // afterEach(() => {
+    //     jest.clearAllMocks();
+    // });
+
+    it("deve retornar '-' quando o número de telefone for nulo", () => {
+        const formatted = phoneNumberFormatter(null);
+        expect(formatted).toBe("-");
+    });
+
+    describe("quando o número de telefone não pode ser parseado", () => {
+        it("deve retornar Inválido se o número não possuir DDI válido", () => {
+            const formatted = phoneNumberFormatter("+0098984684501");
+            expect(formatted).toBe("Inválido");
+        });
+
+        it("deve retornar Inválido se o número não for válido", () => {
+            const formatted = phoneNumberFormatter("98984-a84501");
+            expect(formatted).toBe("Inválido");
+        });
+    });
+
+    describe("quando o número de telefone tem DDD", () => {
+        it("deve remover caracteres não númericos (exceto + e -) ao redor do número e formata-lo", () => {
+            const formatted = phoneNumberFormatter("a/98984684501*");
+            expect(formatted).toBe("(98) 98468-4501");
+            // expect(mockedInvalidPhoneNumberFormatter).not.toHaveBeenCalled();
+        });
+
+        it("deve formatar o número de telefone celular com o 9 na frente", () => {
+            const formatted = phoneNumberFormatter("98984684501");
+            expect(formatted).toBe("(98) 98468-4501");
+            // expect(mockedInvalidPhoneNumberFormatter).not.toHaveBeenCalled();
+        });
+
+        it("deve formatar o número de telefone fixo", () => {
+            const formatted = phoneNumberFormatter("9832564501");
+            expect(formatted).toBe("(98) 3256-4501");
+            // expect(mockedInvalidPhoneNumberFormatter).not.toHaveBeenCalled();
+        });
+
+        it("deve formatar o número de telefone celular sem o 9 na frente", () => {
+            const formatted = phoneNumberFormatter("7399568450");
+            expect(formatted).toBe("(73) 9956-8450");
+        });
+    });
+
+    describe("quando o número de telefone não tem DDD", () => {
+        // it("deve executar a função invalidPhoneNumberFormatter para telefone fixo", () => {
+        //     phoneNumberFormatter("32564501");
+        //     expect(mockedInvalidPhoneNumberFormatter).toHaveBeenCalled();
+        // });
+
+        it("deve formatar o número de telefone celular sem o 9 na frente", () => {
+            const formatted = phoneNumberFormatter("81568450");
+            expect(formatted).toBe("( ) 8156-8450");
+        });
+
+        it("deve formatar o número de telefone fixo", () => {
+            const formatted = phoneNumberFormatter("33167890");
+            expect(formatted).toBe("( ) 3316-7890");
+        });
+
+        it("deve formatar o número de telefone celular com o 9 na frente", () => {
+            const formatted = phoneNumberFormatter("981568450");
+            expect(formatted).toBe("( ) 98156-8450");
+        });
+    });
+});
