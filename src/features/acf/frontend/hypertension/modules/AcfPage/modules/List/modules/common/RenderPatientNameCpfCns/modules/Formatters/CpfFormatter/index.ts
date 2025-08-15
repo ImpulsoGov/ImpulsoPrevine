48462dad30fabe6e9878b6227a67501b3496847d
Tainá Santana <tainnaps@gmail.com>
2025-08-15 17:23:38 -0300

    test: adiciona testes para cpfFormatter

--- src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/RenderPatientNameCpfCns/modules/Formatters/CpfFormatter/index.ts
+++ src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/RenderPatientNameCpfCns/modules/Formatters/CpfFormatter/index.ts
@@ -1,6 +1,5 @@
 import { cpf } from "cpf-cnpj-validator";
 
-// TODO: escrever testes unitários
 export const cpfFormatter = (cpfString: string): string => {
     if (!cpfString) return "";
     const paddedCpf = cpfString.padStart(11, "0");
