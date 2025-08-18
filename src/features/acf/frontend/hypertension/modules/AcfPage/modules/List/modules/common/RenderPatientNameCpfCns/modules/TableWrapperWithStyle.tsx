b45cf625445dedb337997bbeeec07bd118be03b3
Tainá Santana <tainnaps@gmail.com>
2025-08-18 15:47:19 -0300

    feat: adiciona padding superior e inferior na TableWrapperWithStyle

--- src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/RenderPatientNameCpfCns/modules/TableWrapperWithStyle.tsx
+++ src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/RenderPatientNameCpfCns/modules/TableWrapperWithStyle.tsx
@@ -8,6 +8,8 @@ export const TableWrapperWithStyle: React.FC<React.PropsWithChildren> = ({
             sx={{
                 "& .patient-name-cpf-cns": {
                     whiteSpace: "break-spaces !important",
+                    paddingTop: "8px !important",
+                    paddingBottom: "8px !important",
                 },
             }}
         >
