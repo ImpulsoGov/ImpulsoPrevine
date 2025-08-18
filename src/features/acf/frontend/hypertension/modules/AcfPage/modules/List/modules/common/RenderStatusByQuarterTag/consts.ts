84160964d4b6be8272edcefb50ccb6802e84210c
João Vittor Souza <jvns2010@gmail.com>
2025-08-18 17:19:49 -0300

    feat: atualiza altura e largura das tags

--- src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/RenderStatusByQuarterTag/consts.ts
+++ src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/RenderStatusByQuarterTag/consts.ts
@@ -12,8 +12,8 @@ export const tagDetailsByStatus: Record<StatusByQuarter, TagDetails> = {
         icon: {
             src: "https://sa-east-1.graphassets.com/AH0lIsPT8QrCidoSKZ1cPz/cmecx3h8z006507lpwb1nwogs",
             alt: "Ícone com símbolo da letra x",
-            width: 17,
-            height: 4,
+            width: 18,
+            height: 10,
         },
         theme: "warning",
     },
@@ -21,8 +21,8 @@ export const tagDetailsByStatus: Record<StatusByQuarter, TagDetails> = {
         icon: {
             src: "https://sa-east-1.graphassets.com/AH0lIsPT8QrCidoSKZ1cPz/cmecx6gsg007s07kn5oa1q3vs",
             alt: "Ícone de uma exclamação",
-            width: 17,
-            height: 4,
+            width: 18,
+            height: 10,
         },
         theme: "attention",
     },
@@ -30,8 +30,8 @@ export const tagDetailsByStatus: Record<StatusByQuarter, TagDetails> = {
         icon: {
             src: "https://sa-east-1.graphassets.com/AH0lIsPT8QrCidoSKZ1cPz/cmecx3h9e006a07lpnnvguact",
             alt: "Ícone de uma ampulheta",
-            width: 17,
-            height: 4,
+            width: 18,
+            height: 10,
         },
         theme: "danger",
     },
