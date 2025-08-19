8fa0be6cca96a0083621ef6c876d6498cedb80cb
danilo <donylopes@gmail.com>
2025-08-19 16:14:28 -0300

    fix: atualiza versão do design-system para 1.0.366 e ajusta margens na lista de hipertensão

--- src/features/acf/frontend/hypertension/modules/AcfPage/consts.tsx
+++ src/features/acf/frontend/hypertension/modules/AcfPage/consts.tsx
@@ -14,8 +14,23 @@ export const breadcrumb = {
 export const sharedHeader = {
     title: "Cuidado da pessoa com hipertensão",
 };
-
-export const textCoaps =
-    "Acompanhe duas boas práticas definidas pelo Ministério da Saúde do indicador C5 de cuidado da pessoa com hipertensão de forma simples: visualizando a realização de consultas com profissional da saúde e aferição da pressão arterial. A lista mostra, com base nos registros da base local do município, como anda o cuidado das pessoas com hipertensão arterial nas diferentes equipes do território.";
-export const textCoeq =
-    "Acompanhe duas boas práticas definidas pelo Ministério da Saúde do indicador C5 de cuidado da pessoa com hipertensão de forma simples: visualizando a realização de consultas com profissional da saúde e aferição da pressão arterial. A lista mostra, com base nos registros da base local do município, como anda o cuidado das pessoas com hipertensão arterial vinculadas à sua equipe.";
+export const textCoaps = (
+    <div>
+        Acompanhe duas boas práticas definidas pelo Ministério da Saúde do{" "}
+        <b>indicador C5 de cuidado da pessoa com hipertensão</b> de forma
+        simples: visualizando a realização de consultas com profissional da
+        saúde e aferição da pressão arterial. A lista mostra, com base nos{" "}
+        <b>registros da base local do município</b>, como anda o cuidado das
+        pessoas com hipertensão arterial nas diferentes equipes do território.
+    </div>
+);
+export const textCoeq = (
+    <div>
+        Acompanhe duas boas práticas definidas pelo Ministério da Saúde do{" "}
+        <b>indicador C5 de cuidado da pessoa com hipertensão</b> de forma
+        simples: visualizando a realização de consultas com profissional da
+        saúde e aferição da pressão arterial. A lista mostra, com base nos{" "}
+        <b>registros da base local do município</b>, como anda o cuidado das
+        pessoas com hipertensão arterial vinculadas à sua equipe.
+    </div>
+);
