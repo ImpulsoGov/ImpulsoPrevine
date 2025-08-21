28d8da6424875dc1b560289d83f8a845ca5568df
danilo <donylopes@gmail.com>
2025-08-21 08:41:12 -0300

    wip

--- src/features/acf/frontend/common/PrintModal/consts.ts
+++
@@ -1,3 +0,0 @@
-// TODO: rever se esse cara deveria viver aqui e se esses valores ainda fazem sentido
-export type GroupedValuesType = { yes: string; no: string };
-export const groupedValues: GroupedValuesType = { yes: "sim", no: "não" };
