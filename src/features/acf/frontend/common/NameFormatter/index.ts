155d1bcc787f64e4af2d3791bc21084ccb76baff
Kevin Souza <ksouza.dev@gmail.com>
2025-09-01 16:42:05 -0300

    fix: ajuste na formatação do nome de equipe

--- src/features/acf/frontend/common/NameFormatter/index.ts
+++ src/features/acf/frontend/common/NameFormatter/index.ts
@@ -10,3 +10,33 @@ export const nameFormatter = (value: string | undefined): string => {
     });
     return formattedNames.join(" ");
 };
+
+export const teamNameFormatter = (value: string | undefined): string => {
+    if (!value) return "-";
+
+    const commonPrepositions = ["da", "de", "do", "dos", "das", "e", "e/"];
+    const commonAcronyms = ["ESF", "PSF", "EAP"]; // adicione aqui outras siglas conhecidas
+
+    const namePieces = value.split(" ");
+
+    const formattedNames = namePieces.map((piece, index) => {
+        const upperPiece = piece.toUpperCase();
+        const lowerPiece = piece.toLowerCase();
+
+        if (commonAcronyms.includes(upperPiece)) {
+            return upperPiece;
+        }
+
+        if (/^[ivxlcdm]+$/i.test(piece)) {
+            return piece;
+        }
+
+        if (commonPrepositions.includes(lowerPiece)) {
+            return lowerPiece;
+        }
+
+        return piece.charAt(0).toUpperCase() + piece.slice(1).toLowerCase();
+    });
+
+    return formattedNames.join(" ");
+};
