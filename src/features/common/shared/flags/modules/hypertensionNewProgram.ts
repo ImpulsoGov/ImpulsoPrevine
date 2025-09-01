fb1b4ac8a83e776f6de9da8ceb6c16a57b45c6c6
João Vittor Souza <jvns2010@gmail.com>
2025-09-01 17:36:24 -0300

    atualiza lista de munis

--- src/features/common/shared/flags/modules/hypertensionNewProgram.ts
+++ src/features/common/shared/flags/modules/hypertensionNewProgram.ts
@@ -1,7 +1,13 @@
 import { flag } from "flags/next";
 import * as allowByMunicipalityId from "./common/allowByMunicipalityId";
 
-const allowedMunicipalities = ["111111", "220177", "290205", "240040"]; //altaneira - ce, boa hora - pi, araçás - ba, água nova - rn
+const allowedMunicipalities = [
+    "111111",
+    "220177",
+    "290205",
+    "240040",
+    "230060",
+];
 
 export const hypertensionNewProgram = flag<
     boolean,
