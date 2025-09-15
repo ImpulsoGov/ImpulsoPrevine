edf79ff3c00f25ecc727a178e20ba2a083869a03
Tainá Santana <tainnaps@gmail.com>
2025-09-15 15:30:07 -0300

    Adiciona funcao generica para mock das flags

--- __tests__/helpers/flag.ts
+++ __tests__/helpers/flag.ts
@@ -1,6 +1,23 @@
 import { jest } from "@jest/globals";
+import type * as Flags from "@/features/common/shared/flags";
 
-// TODO: generalizar para fazer mock de outras flags também
+export const DIABETES_NEW_PROGRAM = "diabetesNewProgram";
+export const HYPERTENSION_NEW_PROGRAM = "hypertensionNewProgram";
+
+export const mockFlag = (
+    name: keyof typeof Flags
+): jest.Mock<() => Promise<boolean>> => {
+    const mockedFlag = jest.fn<() => Promise<boolean>>();
+
+    jest.doMock("@/features/common/shared/flags", () => ({
+        ...jest.requireActual<typeof Flags>("@/features/common/shared/flags"),
+        [name]: mockedFlag,
+    }));
+
+    return mockedFlag;
+};
+
+// TODO: remover essa função e usar mockFlag no lugar
 export const mockDiabetesNewProgram = (): jest.Mock<() => Promise<boolean>> => {
     const mockedDiabetesNewProgram = jest.fn<() => Promise<boolean>>();
 
