ea80ef976517739f551d5967b849eb066c24dcb0
Danilo Lopes <donylopes@gmail.com>
2025-09-16 15:37:42 -0300

    refactor: remove console.log do componente Text

--- src/features/common/frontend/atoms/Text/index.tsx
+++ src/features/common/frontend/atoms/Text/index.tsx
@@ -3,6 +3,5 @@ import React, { type PropsWithChildren } from "react";
 type TextProps = PropsWithChildren<React.HTMLAttributes<HTMLElement>>;
 
 export const Text: React.FC<TextProps> = ({ children, ...props }) => {
-    console.log(children);
     return <span {...props}>{children}</span>;
 };
