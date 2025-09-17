f24360da78fd82a703b03441a3a950efb9fcd434
Danilo Lopes <donylopes@gmail.com>
2025-09-17 16:37:10 -0300

    Remove campos desnecessários do tipo PageItem em model.ts

--- src/features/acf/backend/hypertension/modules/table/model.ts
+++ src/features/acf/backend/hypertension/modules/table/model.ts
@@ -2,9 +2,5 @@ import type * as model from "@/features/acf/shared/hypertension/model";
 
 export type PageItem = Omit<
     model.HypertensionAcfItem,
-    | "patientAgeRange"
-    | "goodPracticesStatusByQuarter"
-    | "totalGoodPracticesAttended"
-    | "totalGoodPracticesAvailable"
-    | "medicalRecordUpdated"
+    "patientAgeRange" | "goodPracticesStatusByQuarter" | "medicalRecordUpdated"
 >;
