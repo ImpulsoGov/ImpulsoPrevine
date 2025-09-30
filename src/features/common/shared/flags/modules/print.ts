696164167190a9cde230bf88b662851f3bac3b97
Danilo Lopes <donylopes@gmail.com>
2025-09-30 18:33:34 -0300

    [REFATORAÇÃO] Atualiza módulo de impressão para usar municípios permitidos

--- src/features/common/shared/flags/modules/print.ts
+++ src/features/common/shared/flags/modules/print.ts
@@ -1,19 +1,16 @@
 import { flag } from "flags/next";
-import * as allowByUserId from "./common/allowByUserId";
-import type { UserId } from "./common/allowByUserId/model";
+import * as allowByMunicipalityId from "./common/allowByMunicipalityId";
 
-const allowedIds = [
-    "ffdb7023-fcaf-4e88-bf22-a50ab3dd2722",
-    "75bbef0e-2c7c-441e-81ce-ddb32021db3f",
-    "7cef18ab-5d7c-436c-bf10-8b9891b932b8",
-    "3fabf432-deba-471f-8ec9-0b1cd446fb31",
-    "a4a2bea5-fe36-4cab-943a-5708b234a13a",
-    "b88a10e5-66bb-4032-bb47-437b7004c16b",
-    "0ae5c4dd-16d3-48dd-81c2-ca4198674574",
+const allowedMunicipalities = [
+    "111111",
+    "220177",
+    "290205",
+    "240040",
+    "230060",
 ];
 
-export const print = flag<boolean, UserId>({
+export const print = flag<boolean, allowByMunicipalityId.MunicipalityIdSus>({
     key: "print",
-    identify: allowByUserId.identify,
-    decide: allowByUserId.buildDecide(allowedIds),
+    identify: allowByMunicipalityId.identify,
+    decide: allowByMunicipalityId.buildDecide(allowedMunicipalities),
 });
