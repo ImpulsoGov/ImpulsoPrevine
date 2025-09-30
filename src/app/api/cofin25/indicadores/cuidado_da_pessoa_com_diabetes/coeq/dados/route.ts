e7c97a989498c8705941e7bdcd3281912714cd9a
Danilo Lopes <donylopes@gmail.com>
2025-09-30 14:21:23 -0300

    feat:  Adiciona endpoint para obter dado para impressao

---
+++ src/app/api/cofin25/indicadores/cuidado_da_pessoa_com_diabetes/coeq/dados/route.ts
@@ -0,0 +1,32 @@
+import { coapsPageRequestBody as queryParamsSchema } from "@/features/acf/shared/diabetes/schema";
+import { parseBody } from "@/features/common/backend/Schema";
+import { getUser } from "@/features/common/backend/User";
+import * as flags from "@/features/common/shared/flags";
+import * as interceptors from "@/features/interceptors/backend";
+import { PROFILE_ID } from "@/types/profile";
+import * as diabetesBackend from "@features/acf/backend/diabetes";
+import type { NextRequest } from "next/server";
+
+const handler = async (req: NextRequest): Promise<Response> => {
+    const user = await getUser(req);
+    const parsedBody = await parseBody(req, queryParamsSchema);
+
+    const data = await diabetesBackend.getAllDataCoeq({
+        municipalitySusId: user.municipalitySusId,
+        teamIne: user.teamIne,
+        sorting: parsedBody.sorting,
+        searchString: parsedBody.search,
+        filters: parsedBody.filters,
+    });
+
+    //TODO adicionar schema de saida
+    return Response.json(data, { status: 200 });
+};
+
+const composed = interceptors.compose(
+    interceptors.allowByFlag(flags.diabetesNewProgram),
+    interceptors.allowProfiles([PROFILE_ID.COEQ]),
+    interceptors.catchErrors
+);
+
+export const POST = composed(handler);
