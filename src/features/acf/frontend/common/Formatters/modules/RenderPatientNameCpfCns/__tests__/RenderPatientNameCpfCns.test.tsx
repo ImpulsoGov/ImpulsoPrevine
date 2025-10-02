7dceb55ee3515deda51ab966562a2b504c1f1c41
Tainá Santana <tainnaps@gmail.com>
2025-10-02 19:16:10 -0300

    test: corrige testes de RenderPatientNameCpfCns

--- src/features/acf/frontend/common/Formatters/modules/RenderPatientNameCpfCns/__tests__/RenderPatientNameCpfCns.test.tsx
+++ src/features/acf/frontend/common/Formatters/modules/RenderPatientNameCpfCns/__tests__/RenderPatientNameCpfCns.test.tsx
@@ -1,8 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import type { BaseRow } from "..";
 import { RenderPatientNameCpfCns } from "..";
-import type { GridRenderCellParams } from "@mui/x-data-grid";
 import { nameFormatter } from "@/features/acf/frontend/common/Formatters/modules/NameFormatter";
 import { cnsFormatter } from "../modules/Formatters/CnsFormatter";
 import { cpfFormatter } from "../modules/Formatters/CpfFormatter";
@@ -13,9 +11,12 @@ jest.mock("../modules/Formatters/CnsFormatter", () => ({
 jest.mock("../modules/Formatters/CpfFormatter", () => ({
     cpfFormatter: jest.fn(),
 }));
-jest.mock("@/features/acf/frontend/common/NameFormatter", () => ({
-    nameFormatter: jest.fn(),
-}));
+jest.mock(
+    "@/features/acf/frontend/common/Formatters/modules/NameFormatter",
+    () => ({
+        nameFormatter: jest.fn(),
+    })
+);
 const mockedNameFormatter = nameFormatter as jest.Mock;
 const mockedCpfFormatter = cpfFormatter as jest.Mock;
 const mockedCnsFormatter = cnsFormatter as jest.Mock;
@@ -27,12 +28,10 @@ describe("RenderPatientNameCpfCns", () => {
 
     it("Deve renderizar o nome e o CPF quando CPF e CNS existem", () => {
         const params = {
-            row: {
-                patientName: "JOSÉ DA SILVA",
-                patientCpf: "12345678922",
-                patientCns: "123456789012345",
-            },
-        } as GridRenderCellParams<BaseRow>;
+            name: "JOSÉ DA SILVA",
+            cpf: "12345678922",
+            cns: "123456789012345",
+        };
         const formattedName = "José da Silva";
         const formattedCpf = "123.456.789-22";
 
@@ -51,12 +50,10 @@ describe("RenderPatientNameCpfCns", () => {
 
     it("Deve renderizar o nome e o CPF quando CPF existe e o CNS não", () => {
         const params = {
-            row: {
-                patientName: "JOSÉ DA SILVA",
-                patientCpf: "12345678922",
-                patientCns: "",
-            },
-        } as GridRenderCellParams<BaseRow>;
+            name: "JOSÉ DA SILVA",
+            cpf: "12345678922",
+            cns: "",
+        };
         const formattedName = "José da Silva";
         const formattedCpf = "123.456.789-22";
 
@@ -75,12 +72,10 @@ describe("RenderPatientNameCpfCns", () => {
 
     it("Deve renderizar o nome e o CNS quando CNS existe e o CPF não", () => {
         const params = {
-            row: {
-                patientName: "JOSÉ DA SILVA",
-                patientCpf: "",
-                patientCns: "123456789012345",
-            },
-        } as GridRenderCellParams<BaseRow>;
+            name: "JOSÉ DA SILVA",
+            cpf: "",
+            cns: "123456789012345",
+        };
         const formattedName = "José da Silva";
         const formattedCns = "123 4567 8901 2345";
         const formattedCpf = "";
@@ -99,12 +94,10 @@ describe("RenderPatientNameCpfCns", () => {
 
     it("Deve renderizar o nome e - quando CPF e CNS não existem", () => {
         const params = {
-            row: {
-                patientName: "JOSÉ DA SILVA",
-                patientCpf: "",
-                patientCns: "",
-            },
-        } as GridRenderCellParams<BaseRow>;
+            name: "JOSÉ DA SILVA",
+            cpf: "",
+            cns: "",
+        };
         const formattedName = "José da Silva";
         const formattedCns = "";
         const formattedCpf = "";
