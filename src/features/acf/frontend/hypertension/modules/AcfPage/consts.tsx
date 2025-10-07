892646ab9925d47bc22796c9346a63bb8239f8ce
mari-ms <marilia.mugnol@gmail.com>
2025-10-07 14:42:55 -0300

    Ajuste texto descritivo da lista para versão com todas boas práticas.tsx
    Ajustes coaps e coeqs

--- src/features/acf/frontend/hypertension/modules/AcfPage/consts.tsx
+++ src/features/acf/frontend/hypertension/modules/AcfPage/consts.tsx
@@ -17,22 +17,23 @@ export const sharedHeader = {
 
 export const textCoaps = (
     <div>
-        Acompanhe duas boas práticas definidas pelo Ministério da Saúde do{" "}
-        <b>indicador C5 de cuidado da pessoa com hipertensão</b> de forma
-        simples: visualizando a realização de consultas com profissional da
-        saúde e aferição da pressão arterial. A lista mostra, com base nos{" "}
-        <b>registros da base local do município</b>, como anda o cuidado das
-        pessoas com hipertensão arterial nas diferentes equipes do território.
+        Acompanhe o <b>indicador C5 de cuidado da pessoa com hipertensão</b> de
+        forma prática, visualizando a realização das boas práticas definidas
+        pelo Ministério da Saúde: consulta com profissional da saúde, aferição
+        da pressão arterial, avaliação de peso e altura e visitas domiciliares. 
+        A lista mostra, com base nos{" "} <b>registros da base local do município</b>, 
+        como anda o cuidado das pessoas com hipertensão arterial nas diferentes equipes
+        do território.
     </div>
 );
 
 export const textCoeq = (
     <div>
-        Acompanhe duas boas práticas definidas pelo Ministério da Saúde do{" "}
-        <b>indicador C5 de cuidado da pessoa com hipertensão</b> de forma
-        simples: visualizando a realização de consultas com profissional da
-        saúde e aferição da pressão arterial. A lista mostra, com base nos{" "}
-        <b>registros da base local do município</b>, como anda o cuidado das
-        pessoas com hipertensão arterial vinculadas à sua equipe.
+        Acompanhe o <b>indicador C5 de cuidado da pessoa com hipertensão</b> de
+        forma prática, visualizando a realização das boas práticas definidas
+        pelo Ministério da Saúde: consulta com profissional da saúde, aferição
+        da pressão arterial, avaliação de peso e altura e visitas domiciliares. 
+        A lista mostra, com base nos{" "} <b>registros da base local do município</b>, 
+        como anda o cuidado das pessoas com hipertensão arterial vinculadas à sua equipe.
     </div>
 );
