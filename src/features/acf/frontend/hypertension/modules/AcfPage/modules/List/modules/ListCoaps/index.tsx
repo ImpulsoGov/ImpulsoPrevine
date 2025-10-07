c593854f341703898cb1a84355c6cc9eef00996a
Tainá Santana <tainnaps@gmail.com>
2025-10-07 11:56:51 -0300

    refactor: move List containers de hipertensao para index

--- src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/ListCoaps/index.tsx
+++ src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/ListCoaps/index.tsx
@@ -1,3 +1,110 @@
 "use client";
-export { ContentCoaps } from "./container";
+import { CurrentQuadrimester } from "@/features/acf/frontend/common/CurrentQuadrimester";
+import { FilterHint } from "@/features/acf/frontend/common/FilterHint";
+import { List } from "@/features/acf/frontend/common/List";
+import { Container as PrintTable } from "@/features/acf/frontend/common/Print/modules/PrintTable/container";
+import { PrintModal } from "@/features/acf/frontend/common/PrintModal";
+import { SearchToolBar } from "@/features/acf/frontend/common/SearchToolBar";
+import { WithCustomPrint } from "@/features/acf/frontend/common/WithCustomPrint";
+import { WithFilters } from "@/features/acf/frontend/common/WithFilters";
+import { WithFiltersBar } from "@/features/acf/frontend/common/WithFiltersBar";
+import { WithPagination } from "@/features/acf/frontend/common/WithPagination";
+import { WithPrintModal } from "@/features/acf/frontend/common/WithPrintModal";
+import { WithSearch } from "@/features/acf/frontend/common/WithSearch";
+import { WithSorting } from "@/features/acf/frontend/common/WithSorting";
+import React, { useState } from "react";
+import { orderPrintGroups } from "./logic";
+import {
+    type CoapsAppliedFilters,
+    CoapsDataTable,
+} from "./modules/CoapsDataTable";
+import { CoapsFiltersBar } from "./modules/CoapsFiltersBar";
+import {
+    printListProps,
+    apsLabelsModal,
+    columns,
+} from "./modules/Print/consts";
+import { getCoapsData } from "./modules/Print/service";
+
+type ContentCoapsProps = {
+    isPrintEnabled: boolean;
+};
+
+//TODO: Pensar se faz sentido que isso fique aqui mesmo
+const initialSelectedValuesCoaps: CoapsAppliedFilters = {
+    careTeamName: [],
+    microAreaName: [],
+    patientAgeRange: "",
+    goodPracticesStatusByQuarter: "",
+    medicalRecordUpdated: "",
+};
+
+//TODO: Escrever um componente que engloba o conteúdo compartilhado entre os perfis de coordenação.
+export const ContentCoaps: React.FC<ContentCoapsProps> = ({
+    isPrintEnabled,
+}) => {
+    const [shouldRenderPrintTable, setShouldRenderPrintTable] = useState(false);
+
+    return (
+        <>
+            <List>
+                <WithPrintModal>
+                    <WithFilters
+                        initialSelectedValues={initialSelectedValuesCoaps}
+                    >
+                        <WithCustomPrint orderGroup={orderPrintGroups}>
+                            <CurrentQuadrimester />
+                            <WithSearch
+                                SearchComponent={SearchToolBar}
+                                isPrintEnabled={isPrintEnabled}
+                                propTriggerPrintWithoutModal={
+                                    printListProps.propTriggerPrintWithoutModal
+                                }
+                                setShouldRenderPrintTable={
+                                    setShouldRenderPrintTable
+                                }
+                            >
+                                <hr style={{ width: "100%" }} />
+                                <WithSorting>
+                                    <FilterHint
+                                        title="Filtrar lista"
+                                        description="selecione filtros para refinar a busca de cidadãos"
+                                    />
+                                    <WithFiltersBar
+                                        FiltersBar={CoapsFiltersBar}
+                                    >
+                                        <WithPagination>
+                                            <CoapsDataTable />
+                                            <PrintModal
+                                                modalLabels={apsLabelsModal}
+                                                setShouldRenderPrintTable={
+                                                    setShouldRenderPrintTable
+                                                }
+                                            ></PrintModal>
+                                            {shouldRenderPrintTable && (
+                                                <PrintTable
+                                                    columns={columns}
+                                                    serviceGetData={
+                                                        getCoapsData
+                                                    }
+                                                    setShouldRenderPrintTable={
+                                                        setShouldRenderPrintTable
+                                                    }
+                                                    printListProps={
+                                                        printListProps
+                                                    }
+                                                />
+                                            )}
+                                        </WithPagination>
+                                    </WithFiltersBar>
+                                </WithSorting>
+                            </WithSearch>
+                        </WithCustomPrint>
+                    </WithFilters>
+                </WithPrintModal>
+            </List>
+        </>
+    );
+};
+
 export type { CoapsAppliedFilters } from "./modules/CoapsDataTable";
--- src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/ListCoeq/index.tsx
+++ src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/ListCoeq/index.tsx
@@ -1,3 +1,101 @@
 "use client";
-export { ContentCoeq } from "./container";
+import { CurrentQuadrimester } from "@/features/acf/frontend/common/CurrentQuadrimester";
+import { FilterHint } from "@/features/acf/frontend/common/FilterHint";
+import { List } from "@/features/acf/frontend/common/List";
+import { Container as PrintTable } from "@/features/acf/frontend/common/Print/modules/PrintTable/container";
+import { PrintModal } from "@/features/acf/frontend/common/PrintModal";
+import { SearchToolBar } from "@/features/acf/frontend/common/SearchToolBar";
+import { WithCustomPrint } from "@/features/acf/frontend/common/WithCustomPrint";
+import { WithFilters } from "@/features/acf/frontend/common/WithFilters";
+import { WithFiltersBar } from "@/features/acf/frontend/common/WithFiltersBar";
+import { WithPagination } from "@/features/acf/frontend/common/WithPagination";
+import { WithPrintModal } from "@/features/acf/frontend/common/WithPrintModal";
+import { WithSearch } from "@/features/acf/frontend/common/WithSearch";
+import { WithSorting } from "@/features/acf/frontend/common/WithSorting";
+import React, { useState } from "react";
+import { orderPrintGroups } from "./logic";
+import {
+    type CoeqAppliedFilters,
+    CoeqDataTable,
+} from "./modules/CoeqDataTable";
+import { CoeqFiltersBar } from "./modules/CoeqFiltersBar";
+import {
+    printListProps,
+    coeqLabelsModal,
+    coeqColumns,
+} from "./modules/Print/consts";
+import { getCoeqData } from "./modules/Print/service";
+
+type ContentCoeqProps = {
+    isPrintEnabled: boolean;
+};
+
+//TODO: Pensar se faz sentido que isso fique aqui mesmo
+const initialSelectedValuesCoeq: CoeqAppliedFilters = {
+    microAreaName: [],
+    patientAgeRange: "",
+    goodPracticesStatusByQuarter: "",
+    medicalRecordUpdated: "",
+};
+
+export const ContentCoeq: React.FC<ContentCoeqProps> = ({ isPrintEnabled }) => {
+    const [shouldRenderPrintTable, setShouldRenderPrintTable] = useState(false);
+    return (
+        <>
+            <List>
+                <WithPrintModal>
+                    <WithCustomPrint orderGroup={orderPrintGroups}>
+                        <CurrentQuadrimester />
+                        <WithFilters
+                            initialSelectedValues={initialSelectedValuesCoeq}
+                        >
+                            <WithSearch
+                                SearchComponent={SearchToolBar}
+                                isPrintEnabled={isPrintEnabled}
+                                propTriggerPrintWithoutModal={
+                                    printListProps.propTriggerPrintWithoutModal
+                                }
+                                setShouldRenderPrintTable={
+                                    setShouldRenderPrintTable
+                                }
+                            >
+                                <hr style={{ width: "100%" }} />
+                                <WithSorting>
+                                    <FilterHint
+                                        title="Filtrar lista"
+                                        description="selecione filtros para refinar a busca de cidadãos"
+                                    />
+                                    <WithFiltersBar FiltersBar={CoeqFiltersBar}>
+                                        <WithPagination>
+                                            <CoeqDataTable />
+                                            <PrintModal
+                                                modalLabels={coeqLabelsModal}
+                                                setShouldRenderPrintTable={
+                                                    setShouldRenderPrintTable
+                                                }
+                                            />
+                                            {shouldRenderPrintTable && (
+                                                <PrintTable
+                                                    columns={coeqColumns}
+                                                    serviceGetData={getCoeqData}
+                                                    setShouldRenderPrintTable={
+                                                        setShouldRenderPrintTable
+                                                    }
+                                                    printListProps={
+                                                        printListProps
+                                                    }
+                                                />
+                                            )}
+                                        </WithPagination>
+                                    </WithFiltersBar>
+                                </WithSorting>
+                            </WithSearch>
+                        </WithFilters>
+                    </WithCustomPrint>
+                </WithPrintModal>
+            </List>
+        </>
+    );
+};
+
 export type { CoeqAppliedFilters } from "./modules/CoeqDataTable";
