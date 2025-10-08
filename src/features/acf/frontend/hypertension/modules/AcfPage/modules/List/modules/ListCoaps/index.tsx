236b515d1ba28bfea7cbc0ad09138e81eae6594d
Tainá Santana <tainnaps@gmail.com>
2025-10-08 11:23:23 -0300

    Move componente List para common do frontend (#538)
    * feat: adiciona componente List na common
    
    * refactor: substitui componentes ListCoaps e ListCoeq diabetes por List
    
    * refactor: substitui componentes ListCoaps e ListCoeq hipertensao por List
    
    * refactor: move List containers de diabetes para index
    
    * refactor: move List containers de hipertensao para index
    
    * refactor: atualiza importações de módulos externos para formato absoluto

--- src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/ListCoaps/index.tsx
+++ src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/ListCoaps/index.tsx
@@ -1,29 +1,110 @@
 "use client";
-export { ContentCoaps } from "./container";
-export type { CoapsAppliedFilters } from "./modules/CoapsDataTable";
-import type { AcfDashboardType } from "@/features/acf/frontend/common/AcfDashboard";
+import { CurrentQuadrimester } from "@/features/acf/frontend/common/CurrentQuadrimester";
+import { FilterHint } from "@/features/acf/frontend/common/FilterHint";
+import { List } from "@/features/acf/frontend/common/List";
+import { Container as PrintTable } from "@/features/acf/frontend/common/Print/modules/PrintTable/container";
+import { PrintModal } from "@/features/acf/frontend/common/PrintModal";
+import { SearchToolBar } from "@/features/acf/frontend/common/SearchToolBar";
+import { WithCustomPrint } from "@/features/acf/frontend/common/WithCustomPrint";
+import { WithFilters } from "@/features/acf/frontend/common/WithFilters";
+import { WithFiltersBar } from "@/features/acf/frontend/common/WithFiltersBar";
+import { WithPagination } from "@/features/acf/frontend/common/WithPagination";
+import { WithPrintModal } from "@/features/acf/frontend/common/WithPrintModal";
+import { WithSearch } from "@/features/acf/frontend/common/WithSearch";
+import { WithSorting } from "@/features/acf/frontend/common/WithSorting";
+import React, { useState } from "react";
+import { orderPrintGroups } from "./logic";
+import {
+    type CoapsAppliedFilters,
+    CoapsDataTable,
+} from "./modules/CoapsDataTable";
+import { CoapsFiltersBar } from "./modules/CoapsFiltersBar";
+import {
+    printListProps,
+    apsLabelsModal,
+    columns,
+} from "./modules/Print/consts";
+import { getCoapsData } from "./modules/Print/service";
 
-export type ListProps = {
-    list: AcfDashboardType;
-    // title: string;
+type ContentCoapsProps = {
+    isPrintEnabled: boolean;
 };
 
-export const ListCoaps: React.FC<React.PropsWithChildren<ListProps>> = ({
-    children,
+//TODO: Pensar se faz sentido que isso fique aqui mesmo
+const initialSelectedValuesCoaps: CoapsAppliedFilters = {
+    careTeamName: [],
+    microAreaName: [],
+    patientAgeRange: "",
+    goodPracticesStatusByQuarter: "",
+    medicalRecordUpdated: "",
+};
+
+//TODO: Escrever um componente que engloba o conteúdo compartilhado entre os perfis de coordenação.
+export const ContentCoaps: React.FC<ContentCoapsProps> = ({
+    isPrintEnabled,
 }) => {
+    const [shouldRenderPrintTable, setShouldRenderPrintTable] = useState(false);
+
     return (
         <>
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "35px",
-                    padding: "0px 0px 150px 0px",
-                    marginTop: "50px",
-                }}
-            >
-                {children}
-            </div>
+            <List>
+                <WithPrintModal>
+                    <WithFilters
+                        initialSelectedValues={initialSelectedValuesCoaps}
+                    >
+                        <WithCustomPrint orderGroup={orderPrintGroups}>
+                            <CurrentQuadrimester />
+                            <WithSearch
+                                SearchComponent={SearchToolBar}
+                                isPrintEnabled={isPrintEnabled}
+                                propTriggerPrintWithoutModal={
+                                    printListProps.propTriggerPrintWithoutModal
+                                }
+                                setShouldRenderPrintTable={
+                                    setShouldRenderPrintTable
+                                }
+                            >
+                                <hr style={{ width: "100%" }} />
+                                <WithSorting>
+                                    <FilterHint
+                                        title="Filtrar lista"
+                                        description="selecione filtros para refinar a busca de cidadãos"
+                                    />
+                                    <WithFiltersBar
+                                        FiltersBar={CoapsFiltersBar}
+                                    >
+                                        <WithPagination>
+                                            <CoapsDataTable />
+                                            <PrintModal
+                                                modalLabels={apsLabelsModal}
+                                                setShouldRenderPrintTable={
+                                                    setShouldRenderPrintTable
+                                                }
+                                            ></PrintModal>
+                                            {shouldRenderPrintTable && (
+                                                <PrintTable
+                                                    columns={columns}
+                                                    serviceGetData={
+                                                        getCoapsData
+                                                    }
+                                                    setShouldRenderPrintTable={
+                                                        setShouldRenderPrintTable
+                                                    }
+                                                    printListProps={
+                                                        printListProps
+                                                    }
+                                                />
+                                            )}
+                                        </WithPagination>
+                                    </WithFiltersBar>
+                                </WithSorting>
+                            </WithSearch>
+                        </WithCustomPrint>
+                    </WithFilters>
+                </WithPrintModal>
+            </List>
         </>
     );
 };
+
+export type { CoapsAppliedFilters } from "./modules/CoapsDataTable";
