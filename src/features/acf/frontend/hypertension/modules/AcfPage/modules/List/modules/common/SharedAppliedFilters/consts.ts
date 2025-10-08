d994c930adaed7e1c6e1b4b7d25ce013e4aae29d
Tainá Santana <tainnaps@gmail.com>
2025-10-08 17:48:21 -0300

    feat: adiciona consts e model ao modulo SharedAppliedFilters de hipertensao

---
+++ src/features/acf/frontend/hypertension/modules/AcfPage/modules/List/modules/common/SharedAppliedFilters/consts.ts
@@ -0,0 +1,8 @@
+import type { HypertensionSharedAppliedFilters } from "./model";
+
+export const sharedInitialSelectedValues: HypertensionSharedAppliedFilters = {
+    microAreaName: [],
+    patientAgeRange: "",
+    goodPracticesStatusByQuarter: "",
+    medicalRecordUpdated: "",
+};
