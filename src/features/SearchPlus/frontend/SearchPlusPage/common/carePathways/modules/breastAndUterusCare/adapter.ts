155ab4999dacea13721d36b8c4ed8943b8af77a0
Tainá Santana <tainnaps@gmail.com>
2025-10-20 18:28:43 -0300

    feature: adiciona novos campos no adapter

--- src/features/SearchPlus/frontend/SearchPlusPage/common/carePathways/modules/breastAndUterusCare/adapter.ts
+++ src/features/SearchPlus/frontend/SearchPlusPage/common/carePathways/modules/breastAndUterusCare/adapter.ts
@@ -1,3 +1,4 @@
+import * as time from "@/features/common/shared/time";
 import type {
     BreastAndUterusCareCsvRow,
     BreastAndUterusCareItem,
@@ -7,6 +8,25 @@ export const csvRowToBreastAndUterusCareItem = (
     csvRows: Array<BreastAndUterusCareCsvRow>
 ): Array<BreastAndUterusCareItem> => {
     return csvRows.map((row) => {
+        const papTestLatestRequestDate =
+            row[
+                "Exame de rastreamento de câncer de colo de útero data última solicitação"
+            ];
+        const papTestLatestEvaluationDate =
+            row[
+                "Exame de rastreamento de câncer de colo de útero data última avaliação"
+            ];
+        const mammographyLatestRequestDate =
+            row[
+                "Exame de rastreamento de câncer de mama data Última solicitação"
+            ];
+        const mammographyLatestEvaluationDate =
+            row[
+                "Exame de rastreamento de câncer de mama data Última avaliação"
+            ];
+        const latestSexualAndReproductiveHealthAppointmentDate =
+            row["Data da última consulta de saúde sexual e reprodutiva"];
+
         return {
             patientName: row["Nome"],
             patientCpf: row["CPF"],
@@ -17,6 +37,40 @@ export const csvRowToBreastAndUterusCareItem = (
                 row["Telefone residencial"] ||
                 row["Telefone de contato"],
             microAreaName: row["Microárea"],
+            patientBirthDate: new Date(
+                time.nonStandardToISO(row["Data de nascimento"])
+            ),
+            // createdAt: new Date(time.nonStandardToISO(row["Gerado em"])),
+            papTestLatestRequestDate:
+                papTestLatestRequestDate === "-"
+                    ? null
+                    : new Date(time.nonStandardToISO(papTestLatestRequestDate)),
+            papTestLatestEvaluationDate:
+                papTestLatestEvaluationDate === "-"
+                    ? null
+                    : new Date(
+                          time.nonStandardToISO(papTestLatestEvaluationDate)
+                      ),
+            mammographyLatestRequestDate:
+                mammographyLatestRequestDate === "-"
+                    ? null
+                    : new Date(
+                          time.nonStandardToISO(mammographyLatestRequestDate)
+                      ),
+            mammographyLatestEvaluationDate:
+                mammographyLatestEvaluationDate === "-"
+                    ? null
+                    : new Date(
+                          time.nonStandardToISO(mammographyLatestEvaluationDate)
+                      ),
+            latestSexualAndReproductiveHealthAppointmentDate:
+                latestSexualAndReproductiveHealthAppointmentDate === "-"
+                    ? null
+                    : new Date(
+                          time.nonStandardToISO(
+                              latestSexualAndReproductiveHealthAppointmentDate
+                          )
+                      ),
         };
     });
 };
--- src/features/SearchPlus/frontend/SearchPlusPage/common/carePathways/modules/breastAndUterusCare/model.ts
+++ src/features/SearchPlus/frontend/SearchPlusPage/common/carePathways/modules/breastAndUterusCare/model.ts
@@ -1,3 +1,5 @@
+import type * as time from "@/features/common/shared/time";
+
 //Esse tipo esta em portugues para ficar igual ao cabeçalho do CSV
 export type BreastAndUterusCareCsvRow = {
     Nome: string;
@@ -8,6 +10,23 @@ export type BreastAndUterusCareCsvRow = {
     "Telefone residencial": string;
     "Telefone de contato": string;
     Microárea: string;
+    "Data de nascimento": time.DateStringWithFullYear;
+    "Exame de rastreamento de câncer de colo de útero data última solicitação":
+        | time.DateStringWithFullYear
+        | "-";
+    "Exame de rastreamento de câncer de colo de útero data última avaliação":
+        | time.DateStringWithFullYear
+        | "-";
+    "Exame de rastreamento de câncer de mama data Última solicitação":
+        | time.DateStringWithFullYear
+        | "-";
+    "Exame de rastreamento de câncer de mama data Última avaliação":
+        | time.DateStringWithFullYear
+        | "-";
+    "Data da última consulta de saúde sexual e reprodutiva":
+        | time.DateStringWithFullYear
+        | "-";
+    // "Gerado em": time.DateStringWithFullYear;
 };
 
 export type BreastAndUterusCareItem = {
@@ -17,4 +36,11 @@ export type BreastAndUterusCareItem = {
     patientCns: string;
     patientPhoneNumber: string;
     microAreaName: string;
+    patientBirthDate: Date;
+    papTestLatestRequestDate: Date | null;
+    papTestLatestEvaluationDate: Date | null;
+    mammographyLatestRequestDate: Date | null;
+    mammographyLatestEvaluationDate: Date | null;
+    latestSexualAndReproductiveHealthAppointmentDate: Date | null;
+    // createdAt: Date;
 };
