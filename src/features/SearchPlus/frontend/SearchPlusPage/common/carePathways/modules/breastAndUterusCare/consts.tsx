705715c93989ae2b8873e6ce66828dd6da74e366
Tainá Santana <tainnaps@gmail.com>
2025-10-20 21:49:16 -0300

    feature: adiciona coluna de exame de cancer de colo de utero

--- src/features/SearchPlus/frontend/SearchPlusPage/common/carePathways/modules/breastAndUterusCare/consts.tsx
+++ src/features/SearchPlus/frontend/SearchPlusPage/common/carePathways/modules/breastAndUterusCare/consts.tsx
@@ -2,6 +2,7 @@
 import type { ColumnsProps, SearchPlusItem } from "../../model";
 //TODO: Este módulo não deveria depender dos formatters. Talvez este arquivo devesse estar na UnitTable?
 import * as Formatters from "@features/SearchPlus/frontend/SearchPlusPage/common/UnitTable/modules/Formatters";
+import { CervixCancerCalculator } from "./modules/goodPractices";
 
 export const breastAndUterusCareColumns: Array<ColumnsProps<SearchPlusItem>> = [
     {
@@ -19,6 +20,70 @@ export const breastAndUterusCareColumns: Array<ColumnsProps<SearchPlusItem>> = [
                     cpf={cpf}
                     cns={cns}
                 />
+            );
+        },
+    },
+    {
+        fields: [
+            "patientBirthDate",
+            "papTestLatestRequestDate",
+            "papTestLatestEvaluationDate",
+            "mammographyLatestRequestDate",
+            "mammographyLatestEvaluationDate",
+            "latestSexualAndReproductiveHealthAppointmentDate",
+            // "createdAt",
+        ],
+        headerName: "Exame câncer de colo de útero\nData e situação",
+        width: {
+            landscape: 150,
+            portrait: 135,
+        },
+        renderCell: (param: unknown): React.ReactNode => {
+            const [
+                patientBirthDate,
+                papTestLatestRequestDate,
+                papTestLatestEvaluationDate,
+                mammographyLatestRequestDate,
+                mammographyLatestEvaluationDate,
+                latestSexualAndReproductiveHealthAppointmentDate,
+            ] = param as [
+                Date,
+                Date | null,
+                Date | null,
+                Date | null,
+                Date | null,
+                Date | null,
+            ];
+            const data = {
+                birthDate: patientBirthDate,
+                papTestLatestRequestDate,
+                papTestLatestEvaluationDate,
+                mammographyLatestRequestDate,
+                mammographyLatestEvaluationDate,
+                latestSexualAndReproductiveHealthAppointmentDate,
+                // TODO: trocar pela data de criação do CSV
+                createdAt: new Date(),
+            };
+            // TODO: usar factory para criar os calculadores
+            const cervixCancerCalc = new CervixCancerCalculator({ ...data });
+            const latestDate = cervixCancerCalc.computelatestDate();
+            // TODO: mover para um módulo para reutilizar
+            const ptBrDateFormatter = new Intl.DateTimeFormat("pt-BR", {
+                day: "2-digit",
+                month: "2-digit",
+                year: "2-digit",
+                timeZone: "America/Sao_Paulo",
+            });
+
+            return (
+                <div>
+                    <div>
+                        {latestDate
+                            ? ptBrDateFormatter.format(latestDate)
+                            : "-"}
+                    </div>
+                    <div>{cervixCancerCalc.computeStatus()}</div>
+                </div>
             );
         },
     },
