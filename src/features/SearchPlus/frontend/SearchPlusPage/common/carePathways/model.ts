257d9c45fc021772c4ea577b29c09315b4f72dd5
Danilo Lopes <donylopes@gmail.com>
2025-10-23 18:11:23 -0300

    feat: refatora componente InputContent e adiciona área de arrastar e soltar para upload de arquivos CSV

--- src/features/SearchPlus/frontend/SearchPlusPage/common/carePathways/model.ts
+++ src/features/SearchPlus/frontend/SearchPlusPage/common/carePathways/model.ts
@@ -1,4 +1,8 @@
 import type { BreastAndUterusCareItem } from "./modules/breastAndUterusCare";
+import {
+    breastAndUterusCareColumns,
+    csvRowToBreastAndUterusCareItem,
+} from "./modules/breastAndUterusCare";
 
 type RenderCell = (param: unknown) => React.ReactNode;
 
@@ -17,3 +21,29 @@ export type ColumnsProps<TSearchPlusItem extends SearchPlusItem> = {
 
 //TODO: tornar polimórfico
 export type SearchPlusItem = BreastAndUterusCareItem;
+
+export const THEMATIC_LISTS = ["Saúde da mulher"] as const;
+
+export type ThematicList = (typeof THEMATIC_LISTS)[number];
+
+export const ListTitles = {
+    "Saúde da mulher":
+        "LISTA NOMINAL CUIDADO DA MULHER E DO HOMEM TRANSGENERO NA PREVENÇÃO DO CÂNCER",
+};
+
+export type Lists = "breastAndUterusCare";
+
+export const adaptersMap = {
+    breastAndUterusCare: csvRowToBreastAndUterusCareItem,
+};
+
+export const columns: Record<
+    ThematicList,
+    Array<ColumnsProps<SearchPlusItem>>
+> = {
+    "Saúde da mulher": breastAndUterusCareColumns,
+};
+
+export const csvListTitleToListKey: Record<ThematicList, Lists> = {
+    "Saúde da mulher": "breastAndUterusCare",
+};
--- src/features/SearchPlus/frontend/SearchPlusPage/index.tsx
+++ src/features/SearchPlus/frontend/SearchPlusPage/index.tsx
@@ -2,8 +2,15 @@
 import { useState } from "react";
 import { ResultContent } from "./ResultContent";
 import { InputContent } from "./InputContent";
-import type { ThematicList, SearchPlusItem } from "./common/carePathways";
+import {
+    type ThematicList,
+    type SearchPlusItem,
+    ListTitles,
+} from "./common/carePathways";
 import type * as time from "@/features/common/shared/time";
+import Image from "next/image";
+import { nameFormatter } from "./common/UnitTable/modules/Formatters";
+import { Snackbar, SnackbarCloseReason } from "@mui/material";
 
 export type HeaderData = {
     thematicList: ThematicList | null;
@@ -11,16 +18,31 @@ export type HeaderData = {
     createdAtTime: time.BRTTimeString;
 };
 
+export type ErrorData = {
+    title: string;
+    message: string;
+};
+
 export const SearchPlusPage: React.FC = () => {
     const [jsonData, setJsonData] = useState<Array<SearchPlusItem>>([]);
-    const [error, setError] = useState<string | null>(null);
+    const [error, setError] = useState<ErrorData>({
+        title: "",
+        message: "",
+    });
     const [header, setHeader] = useState<HeaderData>({
         thematicList: null,
         createdAtDate: "01/01/1970",
         createdAtTime: "00:00",
     });
+    const [open, setOpen] = useState(true);
     return (
-        <>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
             <div
                 style={{
                     padding: "20px",
@@ -29,7 +51,15 @@ export const SearchPlusPage: React.FC = () => {
                     textAlign: "center",
                 }}
             >
-                Busca+
+                <span>
+                    <Image
+                        src="https://sa-east-1.graphassets.com/AH0lIsPT8QrCidoSKZ1cPz/cmh3gw6j802jv06lyyxoe9eip"
+                        alt="Busca+Mais Logo"
+                        width={56}
+                        height={28}
+                    />
+                </span>{" "}
+                Busca+Mais
             </div>
 
             <div
@@ -39,8 +69,6 @@ export const SearchPlusPage: React.FC = () => {
                     alignItems: "center",
                 }}
             >
-                {error && <p>{error}</p>}
-
                 {jsonData.length > 0 ? (
                     <ResultContent
                         jsonData={jsonData}
@@ -52,9 +80,109 @@ export const SearchPlusPage: React.FC = () => {
                         setError={setError}
                         setJsonData={setJsonData}
                         setHeader={setHeader}
+                        header={header}
                     />
                 )}
             </div>
-        </>
+            <Snackbar
+                open={open && error.message.length > 0}
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                sx={{ width: "80%" }}
+                onClose={(
+                    event?: React.SyntheticEvent | Event,
+                    reason?: SnackbarCloseReason
+                ) => {
+                    setOpen(false);
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: "12px",
+                        backgroundColor: "#FDEDED",
+                        borderRadius: "4px",
+                        padding: "12px 32px",
+                        width: "80%",
+                    }}
+                >
+                    <Image
+                        src="https://sa-east-1.graphassets.com/AH0lIsPT8QrCidoSKZ1cPz/cmh3vfms808eg08lxa9z4ud74"
+                        alt="Error Icon"
+                        width={18}
+                        height={18}
+                    />
+                    <div
+                        style={{
+                            color: "#5F2120",
+                        }}
+                    >
+                        <div
+                            style={{
+                                fontSize: "16px",
+                                fontWeight: 500,
+                                lineHeight: "150%",
+                            }}
+                        >
+                            {error.title}
+                        </div>
+                        <div
+                            style={{
+                                fontSize: "14px",
+                                fontWeight: 400,
+                                lineHeight: "143%",
+                            }}
+                        >
+                            {error.message}
+                        </div>
+                    </div>
+                </div>
+            </Snackbar>
+            <div
+                style={{
+                    backgroundColor: "#EABF2E",
+                    width: "100%",
+                    color: "#4F3D0C",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    padding: "42px 118px 118px 118px",
+                    lineHeight: "130%",
+                }}
+            >
+                <p
+                    style={{
+                        fontSize: "21px",
+                        fontWeight: 700,
+                        letterSpacing: "-0.4px",
+                    }}
+                >
+                    Listas disponíveis para conversão
+                </p>
+                <div
+                    style={{
+                        display: "flex",
+                        gap: "16px",
+                        flexWrap: "wrap",
+                    }}
+                >
+                    {Object.values(ListTitles).map((list: string) => (
+                        <div
+                            key={list}
+                            style={{
+                                borderRadius: "8px",
+                                border: "1px solid #4F3D0C",
+                                fontSize: "14px",
+                                fontWeight: 500,
+                                letterSpacing: "-0.3px",
+                                padding: "6px 12px",
+                            }}
+                        >
+                            {nameFormatter(list)}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     );
 };
