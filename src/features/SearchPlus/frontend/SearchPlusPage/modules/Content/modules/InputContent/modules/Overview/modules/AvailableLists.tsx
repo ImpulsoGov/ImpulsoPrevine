3a0a621be2243e8d8c6f819f46f606a45c731ab6
Tainá Santana <tainnaps@gmail.com>
2025-12-12 17:28:44 -0300

    feat: adiciona tooltip nas listas disponiveis
    Co-authored-by: João Vittor Souza <jvns2010@users.noreply.github.com>

--- src/features/SearchPlus/frontend/SearchPlusPage/modules/Content/modules/InputContent/modules/Overview/modules/AvailableLists.tsx
+++ src/features/SearchPlus/frontend/SearchPlusPage/modules/Content/modules/InputContent/modules/Overview/modules/AvailableLists.tsx
@@ -1,5 +1,7 @@
 import { ListTitles } from "@/features/SearchPlus/frontend/SearchPlusPage/modules/common/carePathways";
 import { nameFormatter } from "@/features/SearchPlus/frontend/SearchPlusPage/modules/common/UnitTable/modules/Formatters";
+import { Tooltip } from "@mui/material";
+import Image from "next/image";
 
 export const AvailableLists: React.FC = () => {
     return (
@@ -10,16 +12,39 @@ export const AvailableLists: React.FC = () => {
                 marginBottom: "24px",
             }}
         >
-            <p
-                style={{
-                    fontSize: "18px",
-                    fontWeight: 600,
-                    letterSpacing: "-0.36px",
-                    marginBottom: "12px",
-                }}
-            >
-                Listas disponíveis para conversão
-            </p>
+            <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+                <p
+                    style={{
+                        fontSize: "18px",
+                        fontWeight: 600,
+                        letterSpacing: "-0.36px",
+                        marginBottom: "12px",
+                    }}
+                >
+                    Listas disponíveis para conversão
+                </p>
+                <Tooltip
+                    title={
+                        <div style={{ fontSize: "14px" }}>
+                            Através dessa ferramenta nós conseguimos transformar
+                            o CSV gerado pelo prontuário eletrônico (PEC) em uma
+                            lista nominal pronta para impressão, adaptada ao
+                            novo programa de cofinanciamento. Essa lista já terá
+                            todas as novas boas práticas para você acompanhar a
+                            situação dos cidadãos vinculados a esse indicador.
+                        </div>
+                    }
+                    placement="top"
+                    arrow
+                >
+                    <Image
+                        src="https://sa-east-1.graphassets.com/AH0lIsPT8QrCidoSKZ1cPz/cmfwxvq1608u307ked86rl4v1"
+                        alt="Ícone de informação"
+                        width={16}
+                        height={16}
+                    />
+                </Tooltip>
+            </div>
             <div
                 style={{
                     display: "flex",
