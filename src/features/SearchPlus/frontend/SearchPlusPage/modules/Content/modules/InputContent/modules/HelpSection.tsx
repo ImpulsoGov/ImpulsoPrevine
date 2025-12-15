ab2f6d19fd1f00f3628a8e5320f36c4a821f2952
Tainá Santana <tainnaps@gmail.com>
2025-12-15 17:52:57 -0300

    feat: adiciona video na seção de ajuda

--- src/features/SearchPlus/frontend/SearchPlusPage/modules/Content/modules/InputContent/modules/HelpSection.tsx
+++ src/features/SearchPlus/frontend/SearchPlusPage/modules/Content/modules/InputContent/modules/HelpSection.tsx
@@ -31,9 +31,24 @@ export const HelpSection: React.FC = () => {
             >
                 Ainda tem dúvidas sobre como usar o busca+mais? Confira o{" "}
                 <br></br>
-                vídeo abaixo com mais instruções ou veja as perguntas
-                frequentes.
+                vídeo abaixo com mais instruções.
             </p>
+            <div style={{ padding: "54.16% 0 0 0", position: "relative" }}>
+                <iframe
+                    src="https://player.vimeo.com/video/1146719361?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                    allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "60%",
+                        height: "60%",
+                        border: "none",
+                    }}
+                ></iframe>
+            </div>
+            <script src="https://player.vimeo.com/api/player.js"></script>
         </div>
     );
 };
