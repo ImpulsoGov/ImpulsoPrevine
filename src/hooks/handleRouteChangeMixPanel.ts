7c17b472b47984bddb8443e92a07725114e8d68e
Tainá Santana <tainnaps@gmail.com>
2026-01-07 16:40:38 -0300

    feat: adiciona option de send_immediately no evento de page view

--- src/hooks/handleRouteChangeMixPanel.ts
+++ src/hooks/handleRouteChangeMixPanel.ts
@@ -6,12 +6,16 @@ export const handleRouteChangeMixPanel = (
     pathname?: string,
     search?: string | null
 ) => {
-    if (mixpanel?.track && sessionStatus !== "loading") {
-        mixpanel.track("Page View", {
-            Logged: sessionStatus === "authenticated",
-            path: pathname?.replace(/\/+$/, "") ?? "",
-            search: search ?? "",
-            url: typeof window !== "undefined" ? window.location.href : "",
-        });
+    if (mixpanel.track && sessionStatus !== "loading") {
+        mixpanel.track(
+            "Page View",
+            {
+                Logged: sessionStatus === "authenticated",
+                path: pathname?.replace(/\/+$/, "") ?? "",
+                search: search ?? "",
+                url: typeof window !== "undefined" ? window.location.href : "",
+            },
+            { send_immediately: true }
+        );
     }
 };
