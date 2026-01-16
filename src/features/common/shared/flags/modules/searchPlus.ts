4518ab477e3b93c38b29b3c338acc08d40643674
João Vittor Souza <jvns2010@gmail.com>
2026-01-16 11:07:33 -0300

    fix: retoma usuarios internos nas flags

--- src/features/common/shared/flags/modules/searchPlus.ts
+++ src/features/common/shared/flags/modules/searchPlus.ts
@@ -3,14 +3,14 @@ import * as allowByUserId from "./common/allowByUserId";
 
 const allowedUsersIds = [
     //time interno
-    // "ffdb7023-fcaf-4e88-bf22-a50ab3dd2722",
-    // "0ae5c4dd-16d3-48dd-81c2-ca4198674574",
-    // "75bbef0e-2c7c-441e-81ce-ddb32021db3f",
-    // "7cef18ab-5d7c-436c-bf10-8b9891b932b8",
-    // "3fabf432-deba-471f-8ec9-0b1cd446fb31",
-    // "c806dadf-e374-4422-a339-7635528fa2b4",
-    // "89921c52-a9d5-4a92-ab66-12d44c7817f0",
-    // "93fdf574-4c52-4392-a319-d3e1f3e40c52",
+    "ffdb7023-fcaf-4e88-bf22-a50ab3dd2722",
+    "0ae5c4dd-16d3-48dd-81c2-ca4198674574",
+    "75bbef0e-2c7c-441e-81ce-ddb32021db3f",
+    "7cef18ab-5d7c-436c-bf10-8b9891b932b8",
+    "3fabf432-deba-471f-8ec9-0b1cd446fb31",
+    "c806dadf-e374-4422-a339-7635528fa2b4",
+    "89921c52-a9d5-4a92-ab66-12d44c7817f0",
+    "93fdf574-4c52-4392-a319-d3e1f3e40c52",
     //usuarios
     "33de451c-afff-4357-8960-0915735b89ff",
     "21c803b4-0169-412e-9913-0a0a8c11aa3a",
