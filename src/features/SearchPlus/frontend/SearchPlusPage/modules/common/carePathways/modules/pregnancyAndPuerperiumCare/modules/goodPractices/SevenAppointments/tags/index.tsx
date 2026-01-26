8c9844ffdd57d6a06b8ba63c539e246ff7806435
Tainá Santana <tainnaps@gmail.com>
2026-01-26 11:26:56 -0300

    chore: comenta modulo de tags

--- src/features/SearchPlus/frontend/SearchPlusPage/modules/common/carePathways/modules/pregnancyAndPuerperiumCare/modules/goodPractices/SevenAppointments/tags/index.tsx
+++ src/features/SearchPlus/frontend/SearchPlusPage/modules/common/carePathways/modules/pregnancyAndPuerperiumCare/modules/goodPractices/SevenAppointments/tags/index.tsx
@@ -1,33 +1,33 @@
-import type { Status } from "../logic/SevenAppointmentsCalculator";
-import { PrintTag } from "@/features/common/frontend/molecules";
-import { Icon, Text } from "@/features/common/frontend/atoms";
+// import type { Status } from "../logic/SevenAppointmentsCalculator";
+// import { PrintTag } from "@/features/common/frontend/molecules";
+// import { Icon, Text } from "@/features/common/frontend/atoms";
 
-export const Tag: React.FC<{ content: Status }> = ({ content }) => {
-    if (content === 0) {
-        return (
-            <PrintTag theme="disabled">
-                <Icon
-                    loading="eager"
-                    src="https://sa-east-1.graphassets.com/AH0lIsPT8QrCidoSKZ1cPz/cmhnoilrs2vlh07keu437gdcq"
-                    alt="Ícone de um x dentro de um círculo"
-                    width={8}
-                    height={8}
-                />
-                <Text>0/1</Text>
-            </PrintTag>
-        );
-    }
+// export const Tag: React.FC<{ content: Status }> = ({ content }) => {
+//     if (content === 0) {
+//         return (
+//             <PrintTag theme="disabled">
+//                 <Icon
+//                     loading="eager"
+//                     src="https://sa-east-1.graphassets.com/AH0lIsPT8QrCidoSKZ1cPz/cmhnoilrs2vlh07keu437gdcq"
+//                     alt="Ícone de um x dentro de um círculo"
+//                     width={8}
+//                     height={8}
+//                 />
+//                 <Text>0/1</Text>
+//             </PrintTag>
+//         );
+//     }
 
-    return (
-        <PrintTag theme="success">
-            <Icon
-                loading="eager"
-                src="https://sa-east-1.graphassets.com/AH0lIsPT8QrCidoSKZ1cPz/cmecx3h8q006007lpwtiggqe3"
-                alt="Ícone de uma marca de verificação verde"
-                width={8}
-                height={8}
-            />
-            <Text>1/1</Text>
-        </PrintTag>
-    );
-};
+//     return (
+//         <PrintTag theme="success">
+//             <Icon
+//                 loading="eager"
+//                 src="https://sa-east-1.graphassets.com/AH0lIsPT8QrCidoSKZ1cPz/cmecx3h8q006007lpwtiggqe3"
+//                 alt="Ícone de uma marca de verificação verde"
+//                 width={8}
+//                 height={8}
+//             />
+//             <Text>1/1</Text>
+//         </PrintTag>
+//     );
+// };
