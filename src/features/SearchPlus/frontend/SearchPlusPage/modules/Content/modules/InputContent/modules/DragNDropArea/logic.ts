e6e4ceb8a2a148c07fed0882ae7856672de7d50d
Danilo Lopes <donylopes@gmail.com>
2026-03-12 14:07:29 -0300

    feat: refatora lógica de upload de arquivos e adiciona validações para campos nulos

--- src/features/SearchPlus/frontend/SearchPlusPage/modules/Content/modules/InputContent/modules/DragNDropArea/logic.ts
+++ src/features/SearchPlus/frontend/SearchPlusPage/modules/Content/modules/InputContent/modules/DragNDropArea/logic.ts
@@ -1,42 +1,16 @@
-import {
-    csvListTitleToListKey,
-    type ThematicList,
-} from "@features/SearchPlus/frontend/SearchPlusPage/modules/common/carePathways";
+import { type ThematicList } from "@features/SearchPlus/frontend/SearchPlusPage/modules/common/carePathways";
 import type {
     ErrorData,
     HeaderData,
 } from "@features/SearchPlus/frontend/SearchPlusPage";
-import * as time from "@/features/common/shared/time";
 import { parse } from "papaparse";
 import type { CsvRow } from "@features/SearchPlus/frontend/SearchPlusPage/modules/Content/modules/InputContent/model";
-import mixpanel from "mixpanel-browser";
-
-type ErrorType =
-    | "invalid_file_extension"
-    | "invalid_thematic_list"
-    | "invalid_file_encoding"
-    | "invalid_file_header"
-    | "invalid_creation_date"
-    | "invalid_patient_birth_date"
-    | "team_name_not_found"
-    | "creation_date_not_found"
-    | "unknown_error";
-
-const trackFileUploadWithError = (errorType: ErrorType | null = null): void => {
-    mixpanel.track("file_upload", {
-        status: "error",
-        error_type: errorType,
-        thematic_list: null,
-    });
-};
-
-const trackFileUploadWithSuccess = (thematicList: ThematicList): void => {
-    mixpanel.track("file_upload", {
-        status: "success",
-        error_type: null,
-        thematic_list: thematicList,
-    });
-};
+import {
+    trackFileUploadWithError,
+    trackFileUploadWithSuccess,
+} from "@/features/SearchPlus/frontend/SearchPlusPage/modules/Content/modules/InputContent/modules/DragNDropArea/modules/track";
+import * as validations from "@/features/SearchPlus/frontend/SearchPlusPage/modules/Content/modules/InputContent/modules/DragNDropArea/modules/validations";
+import type { PregnancyAndPuerperiumCareCsvRow } from "@/features/SearchPlus/frontend/SearchPlusPage/modules/common/carePathways/modules/PregnancyAndPuerperiumCare/model";
 
 const csvContent = (lines: Array<string>): string => {
     const headerIndex = lines.findIndex((line) =>
@@ -53,15 +27,8 @@ export const handleFileUpload = (
     setSuccessSnackbar: React.Dispatch<React.SetStateAction<boolean>>,
     isSearchPlusNewCarePathwayEnabled: boolean
 ): void => {
-    if (!file.name.endsWith(".csv")) {
-        errorHandler({
-            title: "Ops! Parece que esse arquivo não é compatível.",
-            message:
-                "O busca+mais funciona apenas com arquivos no formato CSV, baixados diretamente do PEC. Tente novamente.",
-        });
-        trackFileUploadWithError("invalid_file_extension");
-        return;
-    }
+    if (validations.isFileExtensionValid(file.name, errorHandler)) return;
+
     const reader = new FileReader();
     reader.onload = (): void => {
         try {
@@ -71,37 +38,20 @@ export const handleFileUpload = (
             const listRowIndex = lines.findIndex((line) =>
                 line.startsWith("Lista temática")
             );
-            const headerIndex = lines.findIndex((line) =>
-                line.startsWith("Nome;Data de nascimento;")
-            );
 
-            if (headerIndex === -1) {
-                errorHandler({
-                    title: "Ops, parece que algo não funcionou!",
-                    message:
-                        "Possivelmente esse arquivo foi modificado e possui dados inválidos. Tente novamente e selecione um arquivo CSV baixado diretamente do PEC.",
-                });
-                trackFileUploadWithError("invalid_file_header");
-                return;
-            }
+            validations.isHeaderValid(lines, errorHandler);
 
             const list = lines[listRowIndex]?.split(
                 ";"
             )[1] as ThematicList | null;
 
-            const availableLists = (
-                isSearchPlusNewCarePathwayEnabled
-                    ? Object.keys(csvListTitleToListKey)
-                    : Object.keys(csvListTitleToListKey).slice(0, 1)
-            ) as Array<ThematicList>;
-            if (!list || !availableLists.includes(list)) {
-                errorHandler({
-                    title: "Ops! Parece que essa lista temática ainda não está disponível",
-                    message: isSearchPlusNewCarePathwayEnabled
-                        ? "Por enquanto busca+mais funciona apenas com a lista temática de Cuidado da Mulher e do Homem Transgênero Na Prevenção do Câncer e Cuidado da Gestante e Puérpera."
-                        : "Por enquanto busca+mais funciona apenas com a lista temática de Cuidado da Mulher e do Homem Transgênero Na Prevenção do Câncer.",
-                });
-                trackFileUploadWithError("invalid_thematic_list");
+            if (
+                validations.isThematicListAvailable(
+                    list,
+                    isSearchPlusNewCarePathwayEnabled,
+                    errorHandler
+                )
+            ) {
                 return;
             } else {
                 setHeader((prev) => ({
@@ -110,54 +60,28 @@ export const handleFileUpload = (
                 }));
             }
 
-            const teamRowIndex = lines.findIndex((line) =>
-                line.startsWith("Equipe responsável (Nome/INE)")
-            );
-            if (teamRowIndex === -1) {
-                errorHandler({
-                    title: "Ops, parece que algo não funcionou!",
-                    message:
-                        "Possivelmente esse arquivo foi modificado e possui dados inválidos. Tente novamente e selecione um arquivo CSV baixado diretamente do PEC.",
-                });
-                trackFileUploadWithError("team_name_not_found");
-                return;
-            }
+            if (!validations.isTeamLineAvailable(lines, errorHandler)) return;
 
             const createdAtRowIndex = lines.findIndex((line) =>
                 line.startsWith("Gerado em")
             );
-            if (createdAtRowIndex === -1) {
-                errorHandler({
-                    title: "Ops, parece que algo não funcionou!",
-                    message:
-                        "Possivelmente esse arquivo foi modificado e possui dados inválidos. Tente novamente e selecione um arquivo CSV baixado diretamente do PEC.",
-                });
-                trackFileUploadWithError("creation_date_not_found");
-                return;
-            }
-            const splitCreatedAt = lines[createdAtRowIndex]?.split(";");
-            let createdAtDate;
-            let createdAtTime;
-
-            if (splitCreatedAt.length >= 4) {
-                createdAtDate = splitCreatedAt[1];
-                createdAtTime = splitCreatedAt[3];
-            }
 
             if (
-                createdAtDate &&
-                createdAtTime &&
-                (!time.isBrtDateStringValid(createdAtDate) ||
-                    !time.isBrtTimeStringValid(createdAtTime))
-            ) {
-                errorHandler({
-                    title: "Ops, parece que algo não funcionou!",
-                    message:
-                        "Possivelmente esse arquivo foi modificado e possui dados inválidos. Tente novamente e selecione um arquivo CSV baixado diretamente do PEC.",
-                });
-                trackFileUploadWithError("invalid_creation_date");
+                !validations.isCreationDateAvailable(
+                    createdAtRowIndex,
+                    errorHandler
+                )
+            )
                 return;
-            }
+
+            if (
+                !validations.isCreationDateValid(
+                    createdAtRowIndex,
+                    lines,
+                    errorHandler
+                )
+            )
+                return;
 
             const cleanedText = csvContent(lines);
             const result = parse<CsvRow>(cleanedText, {
@@ -166,28 +90,19 @@ export const handleFileUpload = (
                 delimiter: ";",
             });
 
-            const hasInvalidBirthDate = result.data.some((item) => {
-                return (
-                    item["Data de nascimento"] === null ||
-                    !time.isBrtDateStringValid(item["Data de nascimento"]) ||
-                    !item["Data de nascimento"]
-                );
-            });
+            if (validations.hasInvalidBirthDate(result, errorHandler)) return;
+
             //TODO: adicionar validacao de numero e nao deixar o csv ser valido quando o numero estiver errado (NaN) ou for um -
 
-            if (hasInvalidBirthDate) {
-                errorHandler({
-                    title: "Ops, parece que algo não funcionou!",
-                    message:
-                        "Possivelmente esse arquivo foi modificado e possui dados inválidos. Tente novamente e selecione um arquivo CSV baixado diretamente do PEC.",
-                });
-                trackFileUploadWithError("invalid_patient_birth_date");
-                return;
-            }
+            if (list === "Gestação e puerpério")
+                validations.validationsPregnancyAndPuerperium(
+                    result.data as Array<PregnancyAndPuerperiumCareCsvRow>,
+                    errorHandler
+                );
 
             setRawFileContent(file);
             setSuccessSnackbar(true);
-            trackFileUploadWithSuccess(list);
+            trackFileUploadWithSuccess(list as ThematicList);
         } catch (err) {
             if (err instanceof Error) {
                 errorHandler({
